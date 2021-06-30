b572d5cca5a64625e5a1e24ca3bb952980fd8421
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2021-06-30 16:43:55 -0400

    Create baseline terraform configuration for shared infra
    In terraform, setup a resource group and storage account.
    This is a baseline implementation, and will likely evolve as the
    servicebus and cosmosdb database are added.

---
+++ poc/shared_infrastructure/main.tf
@@ -0,0 +1,33 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Environment = "Shared Infrastructure for PoC app"
+    Team        = "ATAT Platform Team"
+  }
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
