9e71ed9706dafa5843624df7e29cdc66fed9d32d
Jeff Segal <jeff.segal-ctr@ccpo.mil>
2021-06-30 17:35:20 -0400

    AT-6316: Added boilerplate Key Vault config

---
+++ poc/shared_infrastructure/variables-network.tf
@@ -0,0 +1,9 @@
+#####################
+# Network variables #
+#####################
+
+# environment
+variable "environment" {
+  type        = string
+  description = "This variable defines the environment to be built"
+}
