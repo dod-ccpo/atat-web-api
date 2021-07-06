532d84d39344e9cce23646fe891b54c840928e9d
Anh Tran <anh.tran-ctr@ccpo.mil>
2021-07-06 11:05:25 -0400

    Updated jest.config.js to include code coverage
    AT-6317

--- poc/random_quote/jest.config.js
+++ poc/random_quote/jest.config.js
@@ -2,6 +2,24 @@ module.exports = {
   "roots": [
     "<rootDir>/src"
   ],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+  coverageProvider: "v8",
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": -10
+    }
+  },
+  testPathIgnorePatterns: [
+    "/node_modules/"
+  ],
   testEnvironment: 'node',
   testMatch: ['**/__tests__/*.test.ts'],
   "transform": {
