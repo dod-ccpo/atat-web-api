04c2a775a93b542eaa60ae12bca5e1f4bb63232f
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2021-07-06 12:13:01 -0400

    Re-organize variables to reduce confusion
    - Moved resource_group_name to variables-network.tf
    - Moved storage_account_name to variables-network.tf
    - Moved location to variables-network.tf
    - Removed default variables.tf file from tutorial.

--- poc/shared_infrastructure/variables-network.tf
+++ poc/shared_infrastructure/variables-network.tf
@@ -7,3 +7,27 @@ variable "environment" {
   type        = string
   description = "This variable defines the environment to be built"
 }
+
+##################################
+# Azure Resource Group variables #
+##################################
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Define the region the resource group will be created"
+}
+
+###################################
+# Azure Storage Account variables #
+###################################
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
