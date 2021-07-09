5c23d44f5711daa02606056fb0f01412299800ea
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-07-09 09:25:05 -0400

    add resource azurerm_cosmosdb_account

--- poc/shared_infrastructure/main.tf
+++ poc/shared_infrastructure/main.tf
@@ -31,6 +31,42 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_cosmosdb_account" "db" {
+  name                = var.cosmosdb_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = true
+
+  # capabilities {
+  #   name = "EnableAggregationPipeline"
+  # }
+
+  # capabilities {
+  #   name = "mongoEnableDocLevelTTL"
+  # }
+
+  # capabilities {
+  #   name = "MongoDBv3.4"
+  # }
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = var.failover_location
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
 module "keyvault" {
   source              = "./modules/keyvault"
   name                = "${var.environment}-keyvault"
