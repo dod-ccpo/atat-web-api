1de180ab7caccb37c4d7e29363759f01e951ab07
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-07-12 18:56:42 -0400

    add service bus namespace

--- poc/shared_infrastructure/main.tf
+++ poc/shared_infrastructure/main.tf
@@ -80,3 +80,10 @@ module "keyvault" {
   secrets    = var.kv-secrets
   customkeys = var.kv-customkeys
 }
+
+resource "azurerm_servicebus_namespace" "service_bus" {
+  name                = "${var.environment}-service-bus"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+}
