63039c29837210020282a4f00f4b476ebe85c594
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-07-12 18:07:53 -0400

    Integrate Cosmos DB; added account, database, two containers and necessary vars

--- poc/shared_infrastructure/main.tf
+++ poc/shared_infrastructure/main.tf
@@ -80,3 +80,62 @@ module "keyvault" {
   secrets    = var.kv-secrets
   customkeys = var.kv-customkeys
 }
+
+resource "azurerm_cosmosdb_account" "da" {
+  name                      = var.cosmosdb_account_name
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
+  enable_automatic_failover = true
+  consistency_policy {
+    consistency_level = "Session"
+  }
+  geo_location {
+    location          = var.failover_location
+    failover_priority = 1
+  }
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = var.cosmosdb_database_name
+  resource_group_name = data.azurerm_cosmosdb_account.da.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.da.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "users" {
+  name                  = "users"
+  resource_group_name   = azurerm_cosmosdb_account.da.resource_group_name
+  account_name          = azurerm_cosmosdb_account.da.name
+  database_name         = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path    = "/user_id"
+  partition_key_version = 1
+  throughput            = 400
+  indexing_policy {
+    indexing_mode = "Consistent"
+    included_path {
+      path = "/*"
+    }
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "portfolios" {
+  name                  = "portfolios"
+  resource_group_name   = azurerm_cosmosdb_account.da.resource_group_name
+  account_name          = azurerm_cosmosdb_account.da.name
+  database_name         = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path    = "/portfolio_id"
+  partition_key_version = 1
+  throughput            = 400
+  indexing_policy {
+    indexing_mode = "Consistent"
+    included_path {
+      path = "/*"
+    }
+  }
+}
