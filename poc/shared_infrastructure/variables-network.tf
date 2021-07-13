c5335f219467bccee948c2809478b771f00785d1
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-07-13 16:17:14 -0400

    remove now unnecessary variables

--- poc/shared_infrastructure/variables-network.tf
+++ poc/shared_infrastructure/variables-network.tf
@@ -21,29 +21,6 @@ variable "location" {
   description = "Define the region the in which resource group will be created"
 }
 
-###################################
-# Azure Storage Account variables #
-###################################
-
-variable "storage_account_name" {
-  type        = string
-  description = "Name of the storage account"
-}
-
-#####################################
-# Azure Cosmos DB Account variables #
-#####################################
-
-variable "cosmosdb_account_name" {
-  type        = string
-  description = "Name of the Cosmos DB account"
-}
-
-variable "cosmosdb_database_name" {
-  type        = string
-  description = "Name of the Cosmos DB database"
-}
-
 variable "failover_location" {
   type        = string
   description = "Define the failover Azure region; Should be geographically separate from the primary location"
