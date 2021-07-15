779f178197197832cbd7a14f4cedcd63f3b6243c
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-07-15 16:14:21 -0400

    Create jest tests for atat-web-api-aws
    Added 3 new tests to atat-web-api-aws.test.ts
    - Test for the creation of DynamoDB, ensuring partition key is set
    - Test for the creation of the API Gateway, ensuring Quotes API routes are set
    - Test for Authorizer (cognito), ensure its using JWT

--- test/atat-web-api-aws.test.ts
+++ test/atat-web-api-aws.test.ts
@@ -1,18 +1,65 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from "@aws-cdk/assert";
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
 import * as AtatWebApiAws from "../lib/atat-web-api-aws-stack";
 
-test("Empty Stack", () => {
+test('DynamoDB Resource is present, and Partition Key is set.', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new AtatWebApiAws.AtatWebApiAwsStack(app, "MyTestStack");
   // THEN
-  expectCDK(stack).to(
-    matchTemplate(
+  expectCDK(stack).to(haveResource("AWS::DynamoDB::Table", {
+    KeySchema: [
       {
-        Resources: {},
-      },
-      MatchStyle.EXACT
-    )
-  );
+        "AttributeName": "id",
+        "KeyType": "HASH"
+      }
+    ],
+    AttributeDefinitions: [
+      {
+        "AttributeName": "id",
+        "AttributeType": "S"
+      }
+    ],
+  }));
+});
+
+test('API Gateway V2 Resource is present, QuotesAPI AllowedMethods defined.', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AtatWebApiAws.AtatWebApiAwsStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::ApiGatewayV2::Api", {
+    CorsConfiguration: {
+      "AllowCredentials": false,
+      "AllowHeaders": [
+        "*"
+      ],
+      "AllowMethods": [
+        "GET",
+        "POST"
+      ],
+      "AllowOrigins": [
+        "*"
+      ],
+      "ExposeHeaders": [
+        "*"
+      ]
+    },
+    Name: "QuotesApi",
+    ProtocolType: "HTTP"
+  }));
+});
+
+test('UserPoolAuthorizer exists, using JWT', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AtatWebApiAws.AtatWebApiAwsStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::ApiGatewayV2::Authorizer", {
+    AuthorizerType: "JWT",
+    Name: "UserPoolAuthorizer",
+    IdentitySource: [
+      "$request.header.Authorization"
+    ],
+  }));
 });
