d2ab9e6a30092721dfd65c0d6933763fe0d98075
Jeff Segal <jeff.segal-ctr@ccpo.mil>
2021-07-19 21:34:00 -0400

    AT-6363: Removed some copypasta

--- provisioning/src/handlers/portfolioDrafts/portfolioDrafts.post.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from '@azure/functions'
-
-export async function getQuote (context: Context): Promise<void> {
-    context.log('getQuote function triggered from http request.')
-
-}
