b5c03479f044bb42d96a39a5f6ed9aa21e1fd2f7
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-07-28 20:27:48 -0400

    Add DynamoDB Document Client

--- applications/portfolioDrafts/post.ts
+++ applications/portfolioDrafts/post.ts
@@ -1,13 +1,14 @@
-import { AttributeValue, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { v4 as uuid } from "uuid";
 import { PortfolioSummary } from "./models/PortfolioSummary";
 import { ProvisioningStatus } from "./models/ProvisioningStatus";
-import { v4 as uuid } from "uuid";
+import { dynamodbClient } from "./utils/dynamodb";
 
-const TABLE_NAME = process.env.DYNAMODB_TABLE;
+const TABLE_NAME = process.env.ATAT_TABLE_NAME;
 // const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
-const CLIENT = new DynamoDBClient({});
+// TODO: Just use "dynamoDbClient"
+const CLIENT = dynamodbClient;
 
 /**
  * Handles requests from the API Gateway.
@@ -15,46 +16,20 @@ const CLIENT = new DynamoDBClient({});
  * @param event - The POST request from API Gateway
  */
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  if (!event.body?.trim()) {
-    return { statusCode: 400, body: "Invalid request, you are missing the parameter body" };
+  if (event.body && !JSON.parse(event.body)) {
+    return { statusCode: 400, body: "Request body must be empty" };
   }
-  // const item = typeof event.body === "object" ? event.body : JSON.parse(event.body);
-  const now = Date.prototype.toISOString();
+  const now = new Date().toISOString();
   const pf: PortfolioSummary = {
     id: uuid(),
     created_at: now,
     updated_at: now,
     status: ProvisioningStatus.NotStarted,
   };
-  const item = pf;
 
-  // item[PRIMARY_KEY] = uuid();
-  console.log(item);
-  /*
-  try {
-    JSON.parse(event.body);
-  } catch (err) {
-    return new ApiResult(400, missingQuote());
-  }
-*/
-  // const body = JSON.parse(event.body);
-  /*
+  console.log(pf);
 
-  if (!isQuote(body)) {
-    return new ApiResult(400, missingField(missingQuoteField(body)));
-  }
-  const newQuote: Quote = body as Quote;
-  const quoteId = uuid();
-
-  const pf: PortfolioSummary = {
-    id: uuid(),
-    created_at: now,
-    updated_at: now,
-    status: ProvisioningStatus.NotStarted,
-  };
-  */
-
-  const putCommand = new PutItemCommand({
+  const putCommand = new PutCommand({
     TableName: TABLE_NAME,
     Item: pf,
   });
@@ -65,5 +40,5 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     console.log(err);
     return { statusCode: 500, body: "database error" };
   }
-  return { statusCode: 201, body: "good job" };
+  return { statusCode: 201, body: JSON.stringify(pf) };
 };
