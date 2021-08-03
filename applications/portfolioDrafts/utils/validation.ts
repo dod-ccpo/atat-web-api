c511e602fbb81852c0d9986181286113b7fbb816
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-08-03 18:46:44 -0400

    Improve portfolioStep validation
    Technically this doesn't totally validate; it's just a type guard. The
    docs have been updated to clarify that. Additionally, the code is
    improved to not fail when object is null. From there, it's a trivial
    addition to introduce the check the input is an object and remove a use
    of `any`.

--- applications/portfolioDrafts/utils/validation.ts
+++ applications/portfolioDrafts/utils/validation.ts
@@ -18,9 +18,16 @@ export function isValidJson(str: string): boolean {
 /**
  * Check whether a given object is a {@link PortfolioStep}.
  *
+ * Note that this only asserts that the given object meets the interface. It does not validate
+ * that the object is a valid {@link PortfolioStep}.
+ *
  * @param object - The object to check
  * @returns true if the object has all the attributes of a {@link PortfolioStep}
  */
-export function isPortfolioStep(object: any): object is PortfolioStep {
-  return "name" in object && "description" in object && "dod_components" in object && "portfolio_managers" in object;
+export function isPortfolioStep(object: unknown): object is PortfolioStep {
+  // Ensure that the given item is a valid object prior to checks its members
+  if (typeof object !== "object" || object === null) {
+    return false;
+  }
+  return ["name", "description", "dod_components", "portfolio_managers"].every((item) => item in object);
 }
