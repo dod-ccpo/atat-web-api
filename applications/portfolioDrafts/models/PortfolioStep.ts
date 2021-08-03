4b925b3838b8acc13a88e5cb514f6978c56782f8
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-08-03 17:18:32 -0400

    Update comment on PortfolioStep to use correct object type

--- applications/portfolioDrafts/models/PortfolioStep.ts
+++ applications/portfolioDrafts/models/PortfolioStep.ts
@@ -6,10 +6,10 @@ export interface PortfolioStep {
 }
 
 /**
- * Check whether a given object is a {@link Quote}.
+ * Check whether a given object is a {@link PortfolioStep}.
  *
  * @param object - The object to check
- * @returns true if the object has all the attributes of a {@link Quote}
+ * @returns true if the object has all the attributes of a {@link PortfolioStep}
  */
 export function isPortfolioStep(object: any): object is PortfolioStep {
   if ("name" in object && "description" in object && "dod_components" in object && "portfolio_managers" in object) {
