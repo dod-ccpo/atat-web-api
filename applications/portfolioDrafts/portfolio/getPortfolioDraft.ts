660a3424a3f472012abc451028d672aabfc75748
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-08-04 18:55:37 -0400

    Update error response and validation
    Now checking for an empty portfoliodraftId, invalid portfolioDraftId,
    and missing portfolio_step attribute in the returned Item.

--- applications/portfolioDrafts/portfolio/getPortfolioDraft.ts
+++ applications/portfolioDrafts/portfolio/getPortfolioDraft.ts
@@ -2,24 +2,23 @@ import { GetCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { ErrorCodes } from "../models/Error";
 import { PortfolioStep } from "../models/PortfolioStep";
-import { dynamodbClient as client } from "../utils/dynamodb";
+import { dynamodbDocumentClient as client } from "../utils/dynamodb";
 import { ApiSuccessResponse, ErrorResponse, ErrorStatusCode, SuccessStatusCode } from "../utils/response";
-import { isPortfolioStep, isValidJson } from "../utils/validation";
 
 const TABLE_NAME = process.env.ATAT_TABLE_NAME;
-const NO_SUCH_PORTFOLIO = new ErrorResponse(
-  { code: ErrorCodes.INVALID_INPUT, message: "Portfolio Draft with the given ID does not exist" },
+const NO_SUCH_PORTFOLIO_STEP = new ErrorResponse(
+  { code: ErrorCodes.INVALID_INPUT, message: "Portfolio Step not found for this Portfolio Draft" },
   ErrorStatusCode.NOT_FOUND
 );
-const REQUEST_BODY_INVALID = new ErrorResponse(
-  { code: ErrorCodes.INVALID_INPUT, message: "A valid PortfolioStep object must be provided" },
-  ErrorStatusCode.BAD_REQUEST
+const NO_SUCH_PORTFOLIO = new ErrorResponse(
+  { code: ErrorCodes.INVALID_INPUT, message: "The given Portfolio Draft does not exist" },
+  ErrorStatusCode.NOT_FOUND
 );
 
 /**
- * Submits the Portfolio Step of the Portfolio Draft Wizard
+ * Gets the Portfolio Step of the Portfolio Draft Wizard
  *
- * @param event - The POST request from API Gateway
+ * @param event - The GET request from API Gateway
  */
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const portfolioDraftId = event.pathParameters?.portfolioDraftId;
@@ -38,21 +37,17 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
   try {
     const data = await client.send(command);
-    const responseBody = JSON.stringify(data.Item);
-    const portfolioStep: PortfolioStep = JSON.parse(responseBody);
-    console.log("Sanity check" + responseBody);
-    /*
-    if (!isPortfolioStep(responseBody)) {
-      return NO_SUCH_PORTFOLIO;
-    } */
-    return new ApiSuccessResponse<PortfolioStep>(portfolioStep, SuccessStatusCode.CREATED);
-  } catch (error) {
-    if (error.name === "ResourceNotFoundException") {
+    if (!data.Item) {
       return NO_SUCH_PORTFOLIO;
     }
-    console.log("Database error: " + error.name);
+    if (!data.Item?.portfolio_step) {
+      return NO_SUCH_PORTFOLIO_STEP;
+    }
+    return new ApiSuccessResponse<PortfolioStep>(data.Item.portfolio_step as PortfolioStep, SuccessStatusCode.OK);
+  } catch (error) {
+    console.log("Database error (" + error.name + "): " + error);
     return new ErrorResponse(
-      { code: ErrorCodes.OTHER, message: "Database error: " + error.name },
+      { code: ErrorCodes.OTHER, message: "Database error" },
       ErrorStatusCode.INTERNAL_SERVER_ERROR
     );
   }
