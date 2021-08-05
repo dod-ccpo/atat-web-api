6556e7ba244c0c07c8f4501710cb6c50929e079c
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-08-04 20:44:09 -0400

    Implement 'getPortfolioDrafts' function
    Uses expensive Scan command to return all items in table.
    Items are returned as an array of PortfolioFull objects.
    An empty table returns an empty array.
    
    Ticket: AT-6362

--- applications/portfolioDrafts/getPortfolioDrafts.ts
+++ applications/portfolioDrafts/getPortfolioDrafts.ts
@@ -1,5 +1,11 @@
+import { ScanCommand, ScanCommandInput } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyResult } from "aws-lambda";
-import { NoContentResponse } from "./utils/response";
+import { ErrorCodes } from "./models/Error";
+import { PortfolioFull } from "./models/PortfolioFull";
+import { dynamodbDocumentClient as client } from "./utils/dynamodb";
+import { ApiSuccessResponse, ErrorResponse, ErrorStatusCode, SuccessStatusCode } from "./utils/response";
+
+const TABLE_NAME = process.env.ATAT_TABLE_NAME;
 
 /**
  * Gets all Portfolio Drafts, TODO: "...to which the user has read access"
@@ -7,5 +13,23 @@ import { NoContentResponse } from "./utils/response";
  * @param event - The GET request from API Gateway
  */
 export const handler = async (): Promise<APIGatewayProxyResult> => {
-  return new NoContentResponse();
+  const params: ScanCommandInput = {
+    TableName: TABLE_NAME,
+  };
+
+  // This is an expensive command and should be replaced when
+  // portfolio draft owners and authenticated users are available
+  // so that "...to which the user has read access" can be added to the implementation.
+  const command = new ScanCommand(params);
+
+  try {
+    const data = await client.send(command);
+    return new ApiSuccessResponse<PortfolioFull[]>(data.Items as PortfolioFull[], SuccessStatusCode.OK);
+  } catch (error) {
+    console.log("Database error (" + error.name + "): " + error);
+    return new ErrorResponse(
+      { code: ErrorCodes.OTHER, message: "Database error" },
+      ErrorStatusCode.INTERNAL_SERVER_ERROR
+    );
+  }
 };
