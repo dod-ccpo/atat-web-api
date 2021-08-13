e6e9d11b545026f6d56f9d32985add6e1521b26a
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-08-13 19:47:06 -0400

    Add validation function isFundingStep().  Checks whether an object looks like model FundingStep.
    Ticket: AT-6409

--- packages/api/utils/validation.ts
+++ packages/api/utils/validation.ts
@@ -1,4 +1,5 @@
 import { PortfolioStep } from "../models/PortfolioStep";
+import { FundingStep } from "../models/FundingStep";
 
 /**
  * Check whether a given string is valid JSON.
@@ -25,9 +26,25 @@ export function isValidJson(str: string): boolean {
  * @returns true if the object has all the attributes of a {@link PortfolioStep}
  */
 export function isPortfolioStep(object: unknown): object is PortfolioStep {
-  // Ensure that the given item is a valid object prior to checks its members
+  // Ensure that the given item is a valid object prior to checking its members
   if (typeof object !== "object" || object === null) {
     return false;
   }
   return ["name", "description", "dod_components", "portfolio_managers"].every((item) => item in object);
+}
+
+/**
+ * Check whether a given object is a {@link FundingStep}.
+ *
+ * Note that this only asserts that the given object meets the interface. It does not validate
+ * that the object is a valid {@link FundingStep}.
+ *
+ * @param object - The object to check
+ * @returns true if the object has all the attributes of a {@link FundingStep}
+ */
+export function isFundingStep(object: unknown): object is FundingStep {
+  if (typeof object !== "object" || object === null) {
+    return false;
+  }
+  return ["task_order_number", "task_order_file", "csp", "clins"].every((item) => item in object);
 }
