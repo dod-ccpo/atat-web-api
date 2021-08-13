28526aa445eeb08c0cb6b986aa9ab6065f2f9626
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-08-13 10:07:53 -0400

    Update createPortfolioStep to use new validation
    now using isPathParameterPresent and isBodyPresent

--- applications/portfolioDrafts/portfolio/createPortfolioStep.ts
+++ applications/portfolioDrafts/portfolio/createPortfolioStep.ts
@@ -5,7 +5,7 @@ import { ErrorCodes } from "../models/Error";
 import { PortfolioStep } from "../models/PortfolioStep";
 import { dynamodbClient as client } from "../utils/dynamodb";
 import { ApiSuccessResponse, ErrorResponse, ErrorStatusCode, SuccessStatusCode } from "../utils/response";
-import { isPortfolioStep, isValidJson, isBodyEmpty } from "../utils/validation";
+import { isPortfolioStep, isValidJson, isBodyPresent, isPathParameterPresent } from "../utils/validation";
 
 const TABLE_NAME = process.env.ATAT_TABLE_NAME;
 const NO_SUCH_PORTFOLIO = new ErrorResponse(
@@ -16,7 +16,7 @@ const REQUEST_BODY_INVALID = new ErrorResponse(
   { code: ErrorCodes.INVALID_INPUT, message: "A valid PortfolioStep object must be provided" },
   ErrorStatusCode.BAD_REQUEST
 );
-const EMPTY_REQUEST_BODY = new ErrorResponse(
+export const EMPTY_REQUEST_BODY = new ErrorResponse(
   { code: ErrorCodes.INVALID_INPUT, message: "Request body must not be empty" },
   ErrorStatusCode.BAD_REQUEST
 );
@@ -27,17 +27,13 @@ const EMPTY_REQUEST_BODY = new ErrorResponse(
  * @param event - The POST request from API Gateway
  */
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  if (isBodyEmpty(event)) {
-    return EMPTY_REQUEST_BODY;
-  }
-
-  if (!event.body) {
+  if (!isBodyPresent(event.body)) {
     return EMPTY_REQUEST_BODY;
   }
 
   const portfolioDraftId = event.pathParameters?.portfolioDraftId;
 
-  if (!portfolioDraftId) {
+  if (!isPathParameterPresent(portfolioDraftId)) {
     return NO_SUCH_PORTFOLIO;
   }
 
