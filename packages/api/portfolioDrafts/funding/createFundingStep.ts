337be4aef7acb87813ad09bd4b42acdb27139541
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-08-26 10:49:25 -0400

    Remove excessive validation related comments used for impl process

--- packages/api/portfolioDrafts/funding/createFundingStep.ts
+++ packages/api/portfolioDrafts/funding/createFundingStep.ts
@@ -23,42 +23,29 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
     return REQUEST_BODY_EMPTY;
   }
 
-  // 1 b. The route must accept a {portfolioDraftId} that corresponds to a portfolio. This allows data for a specific portfolio draft’s funding to be stored
   const portfolioDraftId = event.pathParameters?.portfolioDraftId;
   if (!isPathParameterPresent(portfolioDraftId)) {
     return PATH_VARIABLE_REQUIRED_BUT_MISSING;
   }
 
-  // 3. When an invalid {portfolioDraftId} is provided, a 404 code is provided, along with the message “Portfolio Draft with the given ID does not exist”
   if (!portfolioDraftId) {
     return NO_SUCH_PORTFOLIO_DRAFT;
   }
 
-  // 1 c. The request body must be valid JSON
   if (!isValidJson(event.body)) {
     return REQUEST_BODY_INVALID;
   }
   const requestBody = JSON.parse(event.body);
 
-  // 2. When a valid {portfolioDraftId} is provided, and the data in the request body is invalid, a 400 code is provided, along with the message “Invalid input“
   if (!isFundingStep(requestBody)) {
     return REQUEST_BODY_INVALID;
   }
   const fundingStep: FundingStep = requestBody;
 
-  // TODO:
-  // 1 d. Input validation must take place
-  //   v. CLIN value and obligated funds must be numbers
-  //   vi. CLIN value and obligated funds should be formatted as currency (i.e. “0.00”)
-
-  // 2 b. An error map must be returned
-
   const clins = fundingStep.clins.values();
   // All validation tests below are logically negating the expected state; i.e. if (!expected) { return validation error }
   // Assertion messages are positive and describe the expected state.
   for (const clin of clins) {
-    console.debug("Processing clin_number: " + clin.clin_number + "...");
-    // 1 d. i. Any dates must be ISO 8601 compliant
     if (!isValidDate(clin.pop_start_date)) {
       console.warn("clin [" + clin.clin_number + "] - PoP start date must be a valid date");
       return createValidationErrorResponse({ pop_start_date: clin.pop_start_date });
@@ -67,7 +54,6 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
       console.warn("clin [" + clin.clin_number + "] - PoP end date must be a valid date");
       return createValidationErrorResponse({ pop_end_date: clin.pop_end_date });
     }
-    // 1 d. ii. PoP start date must occur before the PoP end date, and PoP end date cannot be in the past
     if (!(new Date(clin.pop_start_date) < new Date(clin.pop_end_date))) {
       console.warn("clin [" + clin.clin_number + "] - PoP start date must be before PoP end date");
       return createValidationErrorResponse({ pop_start_date: clin.pop_start_date, pop_end_date: clin.pop_end_date });
@@ -76,7 +62,6 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
       console.warn("clin [" + clin.clin_number + "] - PoP end date must be in the future");
       return createValidationErrorResponse({ pop_end_date: clin.pop_end_date });
     }
-    // 1 d. iii. Obligated funds must be greater than $0.00, and less than the total CLIN value
     if (!(clin.obligated_funds > 0)) {
       console.warn("clin [" + clin.clin_number + "] - Obligated funds must be greater than $0.00");
       return createValidationErrorResponse({ obligated_funds: clin.obligated_funds });
@@ -85,7 +70,6 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
       console.warn("clin [" + clin.clin_number + "] - Obligated funds must be less than the total CLIN value");
       return createValidationErrorResponse({ obligated_funds: clin.obligated_funds });
     }
-    // 1 d. iv. Total CLIN value must be greater than $0.00
     if (!(clin.total_clin_value > 0)) {
       console.warn("clin [" + clin.clin_number + "] - Total CLIN value must be greater than $0.00");
       return createValidationErrorResponse({ total_clin_value: clin.total_clin_value });
