113392b8cd83b05643e3504388c5cedfb75ac5ae
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-08-26 19:36:35 -0400

    Add isClin() to validation module including unit tests
    Ticket: AT-6409

--- packages/api/utils/validation.ts
+++ packages/api/utils/validation.ts
@@ -1,4 +1,4 @@
-import { ApplicationStep } from "../models/ApplicationStep";
+import { Clin } from "../models/Clin";
 import { FundingStep } from "../models/FundingStep";
 import { PortfolioStep } from "../models/PortfolioStep";
 import { version as uuidVersion, validate as uuidValidate } from "uuid";
@@ -117,7 +117,22 @@ export function isBodyPresent(body: string | null): body is string {
  * @param str - The string to check
  * @returns true if the string is a valid date; false otherwise
  */
- export function isValidDate(str: string): boolean {
+export function isValidDate(str: string): boolean {
   const date: Date = new Date(str);
   return date instanceof Date && !isNaN(date.getTime());
+}
+
+/**
+ * Check whether a given object is a {@link Clin}.
+ *
+ * @param object - The object to check
+ * @returns true if the object has all the attributes of a {@link Clin}
+ */
+export function isClin(object: unknown): object is Clin {
+  if (!isValidObject(object)) {
+    return false;
+  }
+  return ["clin_number", "idiq_clin", "total_clin_value", "obligated_funds", "pop_start_date", "pop_end_date"].every(
+    (item) => item in object
+  );
 }
