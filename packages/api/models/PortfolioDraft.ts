a1d02cc12ebdf50880409fd79360db7afbfd64ff
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-08-30 18:47:58 -0400

    Update existing models per API spec
    Ticket: AT-6466

--- packages/api/models/PortfolioDraft.ts
+++ packages/api/models/PortfolioDraft.ts
@@ -1,6 +1,10 @@
-import { PortfolioStep } from "./PortfolioStep";
 import { PortfolioDraftSummary } from "./PortfolioDraftSummary";
+import { PortfolioStep } from "./PortfolioStep";
+import { FundingStep } from "./FundingStep";
+import { ApplicationStep } from "./ApplicationStep";
 
 export interface PortfolioDraft extends PortfolioDraftSummary {
-  portfolio: PortfolioStep;
+  portfolio_step: PortfolioStep;
+  funding_step: FundingStep;
+  application_step: ApplicationStep;
 }
