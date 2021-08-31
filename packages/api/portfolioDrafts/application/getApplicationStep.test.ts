e565df14e4723ae012eeac50d764770ef538bbd0
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-08-31 06:47:19 -0400

    Add more unit tests.  Improve function implementation.
    Ticket: AT-6466

--- packages/api/portfolioDrafts/application/getApplicationStep.test.ts
+++ packages/api/portfolioDrafts/application/getApplicationStep.test.ts
@@ -1,21 +1,132 @@
+import { AccessLevel } from "../../models/AccessLevel";
 import { APIGatewayProxyEvent } from "aws-lambda";
-// import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { ApplicationStep } from "../../models/ApplicationStep";
+import { Clin } from "../../models/Clin";
+import { CloudServiceProvider } from "../../models/CloudServiceProvider";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { Environment } from "../../models/Environment";
 import { ErrorCodes } from "../../models/Error";
-import { ErrorStatusCode } from "../../utils/response";
-import { handler } from "./getApplicationStep";
-// import { mockClient } from "aws-sdk-client-mock";
-import { PATH_VARIABLE_REQUIRED_BUT_MISSING } from "../../utils/errors";
+import { ErrorStatusCode, SuccessStatusCode } from "../../utils/response";
+import { FileMetadata, FileScanStatus } from "../../models/FileMetadata";
+import { FundingStep } from "../../models/FundingStep";
+import { getApplicationStep, handler } from "./getApplicationStep";
+import { isApplicationStep } from "../../utils/validation";
+import { mockClient } from "aws-sdk-client-mock";
+import { Operator } from "../../models/Operator";
+import { PortfolioDraft } from "../../models/PortfolioDraft";
+import { PortfolioStep } from "../../models/PortfolioStep";
+import { ProvisioningStatus } from "../../models/ProvisioningStatus";
 import { v4 as uuid } from "uuid";
+import {
+  NO_SUCH_PORTFOLIO_DRAFT,
+  PATH_VARIABLE_REQUIRED_BUT_MISSING,
+  DATABASE_ERROR,
+  PATH_VARIABLE_INVALID,
+} from "../../utils/errors";
 
+const ddbMock = mockClient(DynamoDBDocumentClient);
+beforeEach(() => {
+  ddbMock.reset();
+});
+
+// Implementation of PortfolioDraftSummaryEx from API spec
+const mockOperatorDarthVader: Operator = {
+  first_name: "Darth",
+  last_name: "Vader",
+  email: "iam@yourfather.com",
+  access: AccessLevel.ADMINISTRATOR,
+};
+const mockOperatorHanSolo: Operator = {
+  first_name: "Han",
+  last_name: "Solo",
+  email: "frozen@carbonite.com",
+  access: AccessLevel.READ_ONLY,
+};
+const mockOperatorLukeSkywalker: Operator = {
+  first_name: "Luke",
+  last_name: "Skywalker",
+  email: "lostmy@hand.com",
+  access: AccessLevel.READ_ONLY,
+};
+const mockOperatorSalaciousCrumb: Operator = {
+  first_name: "Salacious",
+  last_name: "Crumb",
+  email: "monkey@lizard.com",
+  access: AccessLevel.ADMINISTRATOR,
+};
+const mockOperatorBobaFett: Operator = {
+  first_name: "Boba",
+  last_name: "Fett",
+  email: "original@mandalorian.com",
+  access: AccessLevel.READ_ONLY,
+};
+const mockEnvironmentCloudCity: Environment = {
+  name: "Cloud City",
+  operators: [mockOperatorDarthVader, mockOperatorHanSolo, mockOperatorLukeSkywalker],
+};
+const mockEnvironmentJabbasPalace: Environment = {
+  name: "Jabba's Palace",
+  operators: [mockOperatorSalaciousCrumb, mockOperatorHanSolo, mockOperatorBobaFett],
+};
+// const mockPortfolioStep: PortfolioStep = {
+//   name: "Mock Portfolio",
+//   description: "Mock portfolio description",
+//   dod_components: ["air_force", "army", "marine_corps", "navy", "space_force"],
+//   portfolio_managers: ["joe.manager@example.com", "jane.manager@example.com"],
+// };
+// const mockFileMetadata: FileMetadata = {
+//   id: "1234",
+//   created_at: "2021-08-03T16:21:07.978Z",
+//   updated_at: "2021-08-03T16:21:07.978Z",
+//   size: 100,
+//   name: "Mock task order file",
+//   status: FileScanStatus.PENDING,
+// };
+// const mockClin: Clin = {
+//   clin_number: "0001",
+//   idiq_clin: "1234",
+//   total_clin_value: 200000,
+//   obligated_funds: 10000,
+//   pop_start_date: "2021-09-01",
+//   pop_end_date: "2022-09-01",
+// };
+// const mockFundingStep: FundingStep = {
+//   task_order_number: "12345678910",
+//   task_order_file: mockFileMetadata,
+//   csp: CloudServiceProvider.AWS,
+//   clins: [mockClin],
+// };
+const mockApplicationStep: ApplicationStep = {
+  name: "Mock Application",
+  description: "The description of the Mock Application",
+  environments: [mockEnvironmentCloudCity, mockEnvironmentJabbasPalace],
+};
+// const now = new Date();
 const mockPortfolioDraftId = uuid();
+// const mockPortfolioDraft: PortfolioDraft = {
+//   updated_at: now.toISOString(),
+//   created_at: now.toISOString(),
+//   portfolio_step: mockPortfolioStep,
+//   funding_step: mockFundingStep,
+//   application_step: mockApplicationStep,
+//   num_portfolio_managers: 1,
+//   status: ProvisioningStatus.NOT_STARTED,
+//   id: mockPortfolioDraftId,
+// };
+
 const normalRequest: APIGatewayProxyEvent = {
   body: "",
   pathParameters: { portfolioDraftId: mockPortfolioDraftId },
 } as any;
 
 const missingPathVariableRequest: APIGatewayProxyEvent = {
-  body: "",
+  ...normalRequest,
   pathParameters: {}, // missing portfolioDraftId
+} as any;
+
+const invalidPathVariableRequest: APIGatewayProxyEvent = {
+  ...normalRequest,
+  pathParameters: { portfolioDraftId: "invalid_portfolio_draft_id" }, // invalid
 } as any;
 
 describe("TODO list", function () {
@@ -40,5 +151,50 @@ describe("when handler() does not receive required parameter 'portfolioDraftId'"
   it("should return a response body containing message 'Required path variable is missing'", async () => {
     const response = await handler(missingPathVariableRequest);
     expect(JSON.parse(response.body).message).toEqual("Required path variable is missing");
+  });
+});
+
+describe("when handler() receives an invalid 'portfolioDraftId'", function () {
+  it("should return error response PATH_VARIABLE_INVALID", async () => {
+    const response = await handler(invalidPathVariableRequest);
+    expect(response).toEqual(PATH_VARIABLE_INVALID);
+  });
+  it("should return HTTP response status code 400 Bad Request", async () => {
+    const response = await handler(invalidPathVariableRequest);
+    expect(response.statusCode).toEqual(ErrorStatusCode.BAD_REQUEST);
+  });
+  it("should return a response body containing code 'INVALID_INPUT'", async () => {
+    const response = await handler(invalidPathVariableRequest);
+    expect(JSON.parse(response.body).code).toEqual(ErrorCodes.INVALID_INPUT);
+  });
+  it("should return a response body containing message 'Invalid path variable'", async () => {
+    const response = await handler(invalidPathVariableRequest);
+    expect(JSON.parse(response.body).message).toEqual("Invalid path variable");
+  });
+});
+
+describe("getApplicationStep()", function () {
+  it("should return an object that looks like an Application Step given a valid portfolioDraftId", async () => {
+    ddbMock.on(GetCommand).resolves({
+      Item: mockApplicationStep,
+    });
+    const data = await getApplicationStep(mockPortfolioDraftId);
+    expect(data.Item).toStrictEqual(mockApplicationStep);
+  });
+});
+
+describe("when handler() receives a valid 'portfolioDraftId' but the portfolio draft does not exist", function () {
+  it("should return an error response can not find that portfolio draft", async () => {
+    ddbMock.on(GetCommand).rejects({ name: "ResourceNotFoundException" });
+    const data = await handler(normalRequest);
+    expect(data).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
+  });
+});
+
+describe("when handler() fails to service the request", function () {
+  it("should return database error when unknown internal problem occurs", async () => {
+    ddbMock.on(GetCommand).rejects({ name: "InternalServiceError" });
+    const data = await handler(normalRequest);
+    expect(data).toEqual(DATABASE_ERROR);
   });
 });
--- packages/api/portfolioDrafts/application/getApplicationStep.ts
+++ packages/api/portfolioDrafts/application/getApplicationStep.ts
@@ -1,15 +1,17 @@
-import { GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
+import { APPLICATION_STEP } from "../../models/PortfolioDraft";
 import { ApplicationStep } from "../../models/ApplicationStep";
 import { dynamodbDocumentClient as client } from "../../utils/dynamodb";
-import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
+import { GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
+import { isPathParameterPresent, isValidUuidV4 } from "../../utils/validation";
 import {
   DATABASE_ERROR,
   NO_SUCH_PORTFOLIO_DRAFT,
   NO_SUCH_APPLICATION_STEP,
   PATH_VARIABLE_REQUIRED_BUT_MISSING,
+  PATH_VARIABLE_INVALID,
 } from "../../utils/errors";
-import { APPLICATION_STEP } from "../../models/PortfolioDraft";
 
 export async function getApplicationStep(portfolioDraftId: string): Promise<GetCommandOutput> {
   return client.send(
@@ -30,19 +32,22 @@ export async function getApplicationStep(portfolioDraftId: string): Promise<GetC
  */
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   const portfolioDraftId = event.pathParameters?.portfolioDraftId;
-  if (!portfolioDraftId) {
+  if (!isPathParameterPresent(portfolioDraftId)) {
     return PATH_VARIABLE_REQUIRED_BUT_MISSING;
+  }
+  if (!isValidUuidV4(portfolioDraftId)) {
+    return PATH_VARIABLE_INVALID;
   }
   try {
     const data = await getApplicationStep(portfolioDraftId);
-    if (!data.Item) {
-      return NO_SUCH_PORTFOLIO_DRAFT;
-    }
     if (!data.Item?.application_step) {
       return NO_SUCH_APPLICATION_STEP;
     }
     return new ApiSuccessResponse<ApplicationStep>(data.Item.application_step as ApplicationStep, SuccessStatusCode.OK);
   } catch (error) {
+    if (error.name === "ResourceNotFoundException") {
+      return NO_SUCH_PORTFOLIO_DRAFT;
+    }
     console.error("Database error: " + error);
     return DATABASE_ERROR;
   }
