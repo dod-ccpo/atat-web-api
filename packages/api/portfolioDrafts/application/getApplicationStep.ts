bd7761ef9e48665dee502bb301b70a3e50201a65
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-09-01 16:43:36 -0400

    Refactor: Return NO_SUCH_PORTFOLIO_DRAFT when portfolioDraftId is not valid UUIDv4
    Collapsed function 'getApplicationStep' into handler
    Renamed 'data' to 'result' for output from DynamoDB
    
    Ticket: AT-6466

--- packages/api/portfolioDrafts/application/getApplicationStep.ts
+++ packages/api/portfolioDrafts/application/getApplicationStep.ts
@@ -3,27 +3,14 @@ import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
 import { APPLICATION_STEP } from "../../models/PortfolioDraft";
 import { ApplicationStep } from "../../models/ApplicationStep";
 import { dynamodbDocumentClient as client } from "../../utils/dynamodb";
-import { GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 import { isPathParameterPresent, isValidUuidV4 } from "../../utils/validation";
 import {
   DATABASE_ERROR,
   NO_SUCH_PORTFOLIO_DRAFT,
   NO_SUCH_APPLICATION_STEP,
   PATH_VARIABLE_REQUIRED_BUT_MISSING,
-  PATH_VARIABLE_INVALID,
 } from "../../utils/errors";
-
-export async function getApplicationStep(portfolioDraftId: string): Promise<GetCommandOutput> {
-  return client.send(
-    new GetCommand({
-      TableName: process.env.ATAT_TABLE_NAME ?? "",
-      Key: {
-        id: portfolioDraftId,
-      },
-      ProjectionExpression: APPLICATION_STEP,
-    })
-  );
-}
 
 /**
  * Gets the Application Step of the specified Portfolio Draft if it exists
@@ -36,17 +23,28 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
     return PATH_VARIABLE_REQUIRED_BUT_MISSING;
   }
   if (!isValidUuidV4(portfolioDraftId)) {
-    return PATH_VARIABLE_INVALID;
+    return NO_SUCH_PORTFOLIO_DRAFT;
   }
   try {
-    const data = await getApplicationStep(portfolioDraftId);
-    if (!data.Item) {
+    const result = await client.send(
+      new GetCommand({
+        TableName: process.env.ATAT_TABLE_NAME ?? "",
+        Key: {
+          id: portfolioDraftId,
+        },
+        ProjectionExpression: APPLICATION_STEP,
+      })
+    );
+    if (!result.Item) {
       return NO_SUCH_PORTFOLIO_DRAFT;
     }
-    if (!data.Item?.application_step) {
+    if (!result.Item?.application_step) {
       return NO_SUCH_APPLICATION_STEP;
     }
-    return new ApiSuccessResponse<ApplicationStep>(data.Item.application_step as ApplicationStep, SuccessStatusCode.OK);
+    return new ApiSuccessResponse<ApplicationStep>(
+      result.Item.application_step as ApplicationStep,
+      SuccessStatusCode.OK
+    );
   } catch (error) {
     console.error("Database error: " + error);
     return DATABASE_ERROR;
