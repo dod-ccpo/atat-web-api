201f0da9a80ed6c7ac2d7ebd56d48c5d0c20b0e8
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-09-08 15:40:21 -0400

    Update submitPortfolioDraft fn
    In order to return separate error codes (400 and 404),
    we must make a separate query to check if a portfolioDraftId exists.
    This commit will throw an error if the portfolioDraft has already been
    submitted

--- packages/api/portfolioDrafts/submit/submitPortfolioDraft.ts
+++ packages/api/portfolioDrafts/submit/submitPortfolioDraft.ts
@@ -1,11 +1,23 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, UpdateCommand, UpdateCommandOutput } from "@aws-sdk/lib-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  UpdateCommand,
+  UpdateCommandOutput,
+  GetCommand,
+  GetCommandOutput,
+} from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
 import { isBodyPresent, isPathParameterPresent } from "../../utils/validation";
-import { DATABASE_ERROR, NO_SUCH_PORTFOLIO_DRAFT, REQUEST_BODY_NOT_EMPTY } from "../../utils/errors";
+import {
+  DATABASE_ERROR,
+  NO_SUCH_PORTFOLIO_DRAFT,
+  REQUEST_BODY_NOT_EMPTY,
+  PORTFOLIO_ALREADY_SUBMITTED,
+} from "../../utils/errors";
 import { PortfolioDraft } from "../../models/PortfolioDraft";
 import { v4 as uuidv4 } from "uuid";
+import { ProvisioningStatus } from "../../models/ProvisioningStatus";
 const TABLE_NAME = process.env.ATAT_TABLE_NAME ?? "";
 
 /**
@@ -21,18 +33,24 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
   const portfolioDraftId = event.pathParameters?.portfolioDraftId;
 
   if (!isPathParameterPresent(portfolioDraftId)) {
-    return NO_SUCH_PORTFOLIO_DRAFT;
+    return PORTFOLIO_ALREADY_SUBMITTED;
   }
 
   try {
     const result = await submitPortfolioDraftCommand(TABLE_NAME, portfolioDraftId);
-
     return new ApiSuccessResponse(result.Attributes as PortfolioDraft, SuccessStatusCode.ACCEPTED);
   } catch (error) {
+    console.log("Error: " + error);
+    // Check if the portfolioDraft exists
     if (error.name === "ConditionalCheckFailedException") {
-      return NO_SUCH_PORTFOLIO_DRAFT;
+      const getResult = await doesPortfolioDraftExistCommand(TABLE_NAME, portfolioDraftId);
+      console.log("getResult: " + JSON.stringify(getResult));
+      if (!getResult.Item) {
+        return NO_SUCH_PORTFOLIO_DRAFT;
+      }
+      return PORTFOLIO_ALREADY_SUBMITTED;
     }
-    console.log("Database error: " + error.name);
+    console.log("Database error: " + error);
     return DATABASE_ERROR;
   }
 }
@@ -43,23 +61,43 @@ export async function submitPortfolioDraftCommand(
   const dynamodb = new DynamoDBClient({});
   const ddb = DynamoDBDocumentClient.from(dynamodb);
   const now = new Date().toISOString();
-  const submitId = uuidv4();
+  // const submitId = uuidv4();
   const result = await ddb.send(
     new UpdateCommand({
       TableName: table,
       Key: {
         id: portfolioDraftId,
       },
-      UpdateExpression: "set #submitId = :submitIdValue, updated_at = :now",
+      UpdateExpression: "set #submitId = :submitIdValue, updated_at = :now, #status = :statusUpdate",
       ExpressionAttributeNames: {
         "#submitId": "submit_id",
+        "#status": "status",
       },
       ExpressionAttributeValues: {
         ":now": now,
-        ":submitIdValue": submitId,
+        ":submitIdValue": uuidv4(),
+        ":statusUpdate": ProvisioningStatus.IN_PROGRESS,
       },
-      ConditionExpression: "attribute_exists(created_at)",
+      ConditionExpression: "attribute_exists(created_at) AND attribute_not_exists(submit_id)",
       ReturnValues: "ALL_NEW",
+    })
+  );
+  return result;
+}
+// We cannot use ConditionExpression to differentiate between a 404 and 400 response, so we need
+// to make an additional query to check if the portfolioDraft exists.
+export async function doesPortfolioDraftExistCommand(
+  table: string,
+  portfolioDraftId: string
+): Promise<GetCommandOutput> {
+  const dynamodb = new DynamoDBClient({});
+  const ddb = DynamoDBDocumentClient.from(dynamodb);
+  const result = await ddb.send(
+    new GetCommand({
+      TableName: table,
+      Key: {
+        id: portfolioDraftId,
+      },
     })
   );
   return result;
