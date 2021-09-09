56455f852249b089e24b1e5257cdf13e39a0e575
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-09-09 12:01:13 -0400

    Undo capitalization fix for yaml spec and provisioning status

--- packages/api/portfolioDrafts/portfolio/createPortfolioStep.test.ts
+++ packages/api/portfolioDrafts/portfolio/createPortfolioStep.test.ts
@@ -27,7 +27,7 @@ describe("Dynamodb mock validation", function () {
         dod_components: [Array],
       },
       num_portfolio_managers: 0,
-      status: "NOT_STARTED",
+      status: "not_started",
       id: "595c31d3-190c-42c3-a9b6-77325fa5ed38",
     };
 
