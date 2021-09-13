57a8f5efa23c08df05c545c6df83062aa0bc7b26
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-09-13 12:14:09 -0400

    Correction to test descriptions
    Ticket: AT-6409

--- packages/api/utils/validation.test.ts
+++ packages/api/utils/validation.test.ts
@@ -237,11 +237,11 @@ describe("isClin()", function () {
 
 describe("isClinNumber()", function () {
   const goodClinNumbers = ["0001", "0010", "0500", "5000", "9999"];
-  it.each(goodClinNumbers)("should reject a clin number with incorrect length or not in valid range", (num) => {
+  it.each(goodClinNumbers)("should accept a clin number with expected length and within accepted range", (num) => {
     expect(isClinNumber(num)).toEqual(true);
   });
   const badClinNumbers = ["", "0", "1", "02", "111", "0000", "10000", "99999"];
-  it.each(badClinNumbers)("should reject a clin number with incorrect length or not in valid range", (num) => {
+  it.each(badClinNumbers)("should reject a clin number with unexpected length or not within accepted range", (num) => {
     expect(isClinNumber(num)).toEqual(false);
   });
 });
