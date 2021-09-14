0557007631dca860287182e6670016b1c94bb6c9
Julius Fitzhugh (Hunter Strategy) <julius.fitzhugh-ctr@ccpo.mil>
2021-09-14 14:43:54 -0400

    Update API spec YAML and interface model
    Update "name" to "portfolio_name" to make the Portfolio
    Draft Summary schema clear.
    
    Include additional attributes as optional for the PortfolioDraftSumary
    interface to account for the before and after creation
    in the Portflio Summary. The attributes are added based on the
    step (portoflio, funding, application) that is updated.
    
    Ticket: AT-6575

--- packages/api/models/PortfolioDraftSummary.ts
+++ packages/api/models/PortfolioDraftSummary.ts
@@ -2,10 +2,10 @@ import { BaseDocument } from "./BaseDocument";
 import { ProvisioningStatus } from "./ProvisioningStatus";
 
 export interface PortfolioDraftSummary extends BaseDocument {
-  name: string;
+  name?: string;
   status: ProvisioningStatus;
-  num_portfolio_managers: number;
-  num_applications: number;
-  num_environments: number;
-  num_task_orders: number;
+  num_portfolio_managers?: number;
+  num_applications?: number;
+  num_environments?: number;
+  num_task_orders?: number;
 }
