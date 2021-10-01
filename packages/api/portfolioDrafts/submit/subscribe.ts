fb656a8fc2357c36bd6bf07415bccda78664a3d1
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-01 17:40:46 -0400

    Use SqsEvent type instead of any for subscribe.ts handler fn

--- packages/api/portfolioDrafts/submit/subscribe.ts
+++ packages/api/portfolioDrafts/submit/subscribe.ts
@@ -1,29 +1,11 @@
-import { UpdateCommand, UpdateCommandOutput, GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { ApiSuccessResponse, NoContentResponse, SuccessStatusCode } from "../../utils/response";
-import { isBodyPresent, isPathParameterPresent } from "../../utils/validation";
-import {
-  DATABASE_ERROR,
-  NO_SUCH_PORTFOLIO_DRAFT,
-  REQUEST_BODY_NOT_EMPTY,
-  PORTFOLIO_ALREADY_SUBMITTED,
-  PATH_PARAMETER_REQUIRED_BUT_MISSING,
-} from "../../utils/errors";
-import { PortfolioDraft } from "../../models/PortfolioDraft";
-import { v4 as uuidv4 } from "uuid";
-import { ProvisioningStatus } from "../../models/ProvisioningStatus";
-import { dynamodbDocumentClient as client } from "../../utils/dynamodb";
-import { SendMessageCommand } from "@aws-sdk/client-sqs";
-import { sqsClient } from "../../utils/sqs";
-const TABLE_NAME = process.env.ATAT_TABLE_NAME ?? "";
-const QUEUE_URL = process.env.ATAT_QUEUE_URL ?? "";
+import { SQSEvent } from "aws-lambda";
 
 /**
- * Submits all progress from the Portfolio Provisioning Wizard
+ * Recieves SQS Events and logs the MessageBody
  *
- * @param event - The POST request from API Gateway
+ * @param event - The SQS Event, triggered when a message is sent to the queue
  */
-export async function handler(event: any) {
+export async function handler(event: SQSEvent) {
   event.Records.forEach((record: any) => {
     const { body } = record;
     console.log(body);
