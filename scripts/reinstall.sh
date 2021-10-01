d763425bc7c1221e89d88059aeb4e05f4790d14d
Jason Burkert (Hunter Strategy) <77642966+jasonburkert-ccpo@users.noreply.github.com>
2021-10-01 13:28:00 -0400

    Don't remove node versions installed under nvm; just instgall preferred version

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -7,7 +7,6 @@ echo " - \$ATAT_WEB_API_DIR ($ATAT_WEB_API_DIR) is defined"
 echo " - nvm is installed and \$NVM_DIR ($NVM_DIR) is defined"
 echo "This will do the following:"
 echo " - operate from the currently selected git branch and state"
-echo " - remove ALL installed node versions under nvm"
 echo " - install the preferred node version specified in .nvmrc"
 echo " - install the latest AWS CLI"
 echo " - install the latest AWS CDK Toolkit"
@@ -19,7 +18,6 @@ read -rp "Press enter to continue or Ctrl+C to cancel"
 . $NVM_DIR/nvm.sh
 . $HOME/.profile
 . $HOME/.bashrc
-echo; echo "[Remove node versions]" && rm -rf $NVM_DIR/versions/node/ && mkdir $NVM_DIR/versions/node/
 echo; echo "[Install AWS CLI]" && sudo apt install awscli
 echo; echo "[Install node]" && cd $ATAT_WEB_API_DIR && nvm install
 echo; echo "[Install TypeScript and AWS CDK Toolkit]" && npm install -g typescript aws-cdk
