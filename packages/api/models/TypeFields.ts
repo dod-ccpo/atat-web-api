b341dae0e91a4a3c469a3d6a86a34d6f68e2a8dc
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-10-01 10:28:24 -0400

    Add input validation example

--- packages/api/models/TypeFields.ts
+++ packages/api/models/TypeFields.ts
@@ -11,6 +11,19 @@ function pick<T>(obj: T, ...keys: Array<keyof T>): T {
   return Object.fromEntries(entries);
 }
 
+function allFields<T>(map: ExhaustiveAttributeMap<T>): Array<keyof T> {
+  return Object.keys(map) as Array<keyof T>;
+}
+
+export function containsExactlyFields<T>(obj: unknown, map: ExhaustiveAttributeMap<T>): obj is T {
+  if (obj === undefined || typeof obj !== "object" || obj == null) {
+    return false;
+  }
+  const requiredKeys = allFields(map) as string[];
+  const actualKeys = Object.keys(obj);
+  return requiredKeys.length === actualKeys.length && requiredKeys.every((required) => actualKeys.includes(required));
+}
+
 export function exhaustivePick<T>(obj: T, map: ExhaustiveAttributeMap<T>): T {
-  return pick(obj, ...(Object.keys(map) as Array<keyof T>));
+  return pick(obj, ...allFields(map));
 }
