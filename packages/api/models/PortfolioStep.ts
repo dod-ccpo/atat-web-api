085693c649645b778a71878c5e36684a8b85ac6d
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-01 11:33:50 -0400

    Update models TaskOrder and PortfolioStep Update unit tests
    Ticket: AT-6677

--- packages/api/models/PortfolioStep.ts
+++ packages/api/models/PortfolioStep.ts
@@ -1,5 +1,7 @@
+import { CloudServiceProvider } from "./CloudServiceProvider";
 export interface PortfolioStep {
   name: string;
+  csp: CloudServiceProvider;
   description: string;
   dod_components: Array<string>;
   portfolio_managers: Array<string>;
--- packages/api/models/TaskOrder.ts
+++ packages/api/models/TaskOrder.ts
@@ -1,12 +1,10 @@
 import { Clin } from "./Clin";
-import { CloudServiceProvider } from "./CloudServiceProvider";
 import { FileMetadataSummary } from "./FileMetadataSummary";
 import { ExhaustiveAttributeMap } from "./TypeFields";
 
 export interface TaskOrder {
   task_order_number: string;
   task_order_file: FileMetadataSummary;
-  csp: CloudServiceProvider;
   clins: Array<Clin>;
 }
 
