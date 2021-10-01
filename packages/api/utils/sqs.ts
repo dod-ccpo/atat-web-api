fb165105134fd0cf793786751e858c49f77e0dcb
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-01 17:37:12 -0400

    Update sqs.ts to remove REGION option

--- packages/api/utils/sqs.ts
+++ packages/api/utils/sqs.ts
@@ -1,6 +1,5 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
-// Set the AWS Region.
-const REGION = "us-gov-west-1"; // e.g. "us-east-1"
-// Create SNS service object.
-const sqsClient = new SQSClient({ region: REGION });
+
+// Create SQS service object.
+const sqsClient = new SQSClient({});
 export { sqsClient };
