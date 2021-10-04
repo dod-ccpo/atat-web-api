890601975e1cbded324655e42a25ccebf815c07b
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-04 11:56:20 -0400

    Change record var data type from any to -->  SQSRecord

--- packages/api/portfolioDrafts/submit/subscribe.ts
+++ packages/api/portfolioDrafts/submit/subscribe.ts
@@ -1,4 +1,4 @@
-import { SQSEvent } from "aws-lambda";
+import { SQSEvent, SQSRecord } from "aws-lambda";
 
 /**
  * Recieves SQS Events and logs the MessageBody
@@ -6,7 +6,7 @@ import { SQSEvent } from "aws-lambda";
  * @param event - The SQS Event, triggered when a message is sent to the queue
  */
 export async function handler(event: SQSEvent) {
-  event.Records.forEach((record: any) => {
+  event.Records.forEach((record: SQSRecord) => {
     const { body } = record;
     console.log(body);
   });
