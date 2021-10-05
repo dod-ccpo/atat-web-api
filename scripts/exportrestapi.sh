a1aba444fabc4cb6083b1317ae9ce25a21535364
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-05 14:20:00 -0400

    Improve script 'exportrestapi.sh' - use existing env var AWS_PROFILE while providing default of 'atat-sandbox-dev' - display visible restapi ids to the user when no args - improve description of output - improve output filename

--- scripts/exportrestapi.sh
+++ scripts/exportrestapi.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+AWS_PROFILE="${AWS_PROFILE:-atat-sandbox-dev}"
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied."
     echo "Expected Parameters:"
-    echo "  1. The Physical ID of the AWS::ApiGateway::RestApi, for example 'd82arw4bnk'"
+    echo "  1. The ID of a AWS::ApiGateway::RestApi, for example 'd82arw4bnk'"
+    echo
+    echo "RestApis visible to AWS profile ${AWS_PROFILE}:"
+    aws apigateway get-rest-apis --profile ${AWS_PROFILE} | jq -c '.items[] | (.createdDate |= todate) | {rest_api_id: .id, stack_name: .tags."aws:cloudformation:stack-name", created_date: .createdDate}'
+    echo
+    echo "To switch AWS profiles, set an environment variable, for example 'export AWS_PROFILE=atat-sandbox-dev'"
     exit 1
 fi
 today=$(date +"%Y-%m-%d")
-aws apigateway get-export --export-type swagger --parameters extensions='postman' --profile atat-sandbox-dev --stage-name prod --rest-api-id $1 ~/AtatWebApi-restapi-$1-postman-export-${today}.json
-echo "Exported to ~/AtatWebApi-restapi-$1-postman-export-${today}.json"
+output=~/${AWS_PROFILE}-restapi-$1-${today}.json
+aws apigateway get-export --export-type swagger --parameters extensions='postman' --profile ${AWS_PROFILE} --stage-name prod --rest-api-id $1 ${output}
+echo
+echo "Swagger-type export with Postman extensions written to ${output}"
