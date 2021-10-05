db668a3aba43aef84786a95cb13d304c6f38a527
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-05 17:13:42 -0400

    Replace jq query with aws cli --query

--- scripts/exportrestapi.sh
+++ scripts/exportrestapi.sh
@@ -7,13 +7,14 @@ if [ $# -eq 0 ]
     echo "  1. The ID of a AWS::ApiGateway::RestApi, for example 'd82arw4bnk'"
     echo
     echo "RestApis visible to AWS profile ${AWS_PROFILE}:"
-    aws apigateway get-rest-apis --profile ${AWS_PROFILE} | jq -c '.items[] | (.createdDate |= todate) | {rest_api_id: .id, stack_name: .tags."aws:cloudformation:stack-name", created_date: .createdDate}'
+    aws apigateway get-rest-apis --profile ${AWS_PROFILE} --query 'items[*].[id, tags."aws:cloudformation:stack-name"]' --output text
     echo
     echo "To switch AWS profiles, set environment variable 'AWS_PROFILE"
     exit 1
 fi
 today=$(date +"%Y-%m-%d")
-output=~/${AWS_PROFILE}-restapi-$1-${today}.json
+output=$HOME/${AWS_PROFILE}-restapi-$1-${today}.json
 aws apigateway get-export --export-type oas30 --parameters extensions='postman' --profile ${AWS_PROFILE} --stage-name prod --rest-api-id $1 ${output}
 echo
-echo "OpenAPI 3.0.x-type export with Postman extensions written to ${output}"
+echo "OpenAPI 3.0.x-type export with Postman extensions written to"
+echo ${output}
