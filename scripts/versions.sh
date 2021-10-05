0b72dae18a65a5f220d6b80b8d1164fff765c380
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-05 15:05:53 -0400

    Tighten up script 'versions.sh' - one line per output section

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-DIVIDER="----------------------------------------------------------------------"
-echo "$DIVIDER"; echo "-- Software Tool Versions"; echo "$DIVIDER"
-echo "[user]"
-whoami
-echo
-echo "[os]"
-PRETTY_NAME=$(grep -oP '(?<=^PRETTY_NAME=).+' /etc/os-release | tr -d '"')
-echo "$PRETTY_NAME"
-uname -a
-echo
-echo "[Node.js]"
-echo "node $(node --version)"
-echo "npm $(npm --version)"
-echo
-echo "[TypeScript]"
-echo "tsc $(tsc --version)"
-echo
-echo "[AWS CLI]"
-echo "aws $(aws --version)"
-echo
-echo "[AWS CDK Toolkit]"
-echo "cdk $(cdk --version)"
-echo
+echo "[user]"; whoami; echo
+echo "[os]"; PRETTY_NAME=$(grep -oP '(?<=^PRETTY_NAME=).+' /etc/os-release | tr -d '"'); echo "$PRETTY_NAME"; uname -a; echo
+echo "[Node.js]"; echo "node $(node --version)"; echo "npm $(npm --version)"; echo
+echo "[TypeScript]"; echo "tsc $(tsc --version)"; echo
+echo "[AWS CLI]"; echo "aws $(aws --version)"; echo
+echo "[AWS CDK Toolkit]"; echo "cdk $(cdk --version)"; echo
