35e1d70697517e76cd7cc51492b9bc77e5c05218
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-08 16:15:00 -0400

    Add validation schema as temporary solution to yaml schema import

--- packages/api/models/PortfolioStep.ts
+++ packages/api/models/PortfolioStep.ts
@@ -6,3 +6,48 @@ export interface PortfolioStep {
   dod_components: Array<string>;
   portfolio_managers: Array<string>;
 }
+
+export const schema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        name: { type: "string" },
+        csp: {
+          type: "string",
+          enum: ["aws", "azure"],
+        },
+        portfolio_managers: {
+          type: "array",
+          items: {
+            type: "string",
+            format: "email",
+          },
+        },
+        dod_components: {
+          type: "array",
+          items: {
+            type: "string",
+            enum: [
+              "air_force",
+              "army",
+              "marine_corps",
+              "navy",
+              "space_force",
+              "combatant_command",
+              "joint_staff",
+              "dafa",
+              "osd_psas",
+              "nsa",
+            ],
+          },
+        },
+        description: {
+          type: "string",
+        },
+      },
+      required: ["name", "portfolio_managers", "description", "csp"], // Insert here all required event properties
+    },
+  },
+};
--- packages/api/portfolioDrafts/middyHandler.ts
+++ packages/api/portfolioDrafts/middyHandler.ts
@@ -1,13 +1,13 @@
 import middy from "@middy/core";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 // import middleware from "../utils/middleware";
-// import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
-// import jsonBodyParser from "@middy/http-json-body-parser";
-// import httpErrorHandler from "@middy/http-error-handler";
-// import validator from "@middy/validator";
+import jsonBodyParser from "@middy/http-json-body-parser";
+import httpErrorHandler from "@middy/http-error-handler";
+import validator from "@middy/validator";
+import { schema } from "../models/PortfolioStep";
 
+// example baseHandler function
 async function baseHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  // the returned response will be checked against the type `APIGatewayProxyResult`
   console.log(event.body);
   return {
     statusCode: 200,
@@ -16,52 +16,95 @@ async function baseHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxy
 }
 
 const handler = middy(baseHandler);
-
-// This is the model output for the schema of the portfoliostep
-// working on a way to generate this from the yaml file
-const portfolioSchema = {
-  required: ["name"],
-  type: "object",
-  properties: {
-    csp: {
-      type: "string",
-      enum: ["aws", "azure"],
-    },
-    portfolio_managers: {
-      type: "array",
-      items: {
-        type: "string",
-        format: "email",
-      },
-    },
-    dod_components: {
-      type: "array",
-      items: {
-        type: "string",
-        enum: [
-          "air_force",
-          "army",
-          "marine_corps",
-          "navy",
-          "space_force",
-          "combatant_command",
-          "joint_staff",
-          "dafa",
-          "osd_psas",
-          "nsa",
-        ],
-      },
-    },
-    name: {
-      type: "string",
-    },
-    description: {
-      type: "string",
-    },
-  },
-  description: "Represents step 1 of the Portfolio Draft Wizard",
-};
-
-// handler.use(validator({ inputSchema: portfolioSchema })).use(httpErrorHandler());
+handler
+  .use(jsonBodyParser()) // parses request body into JSON object
+  .use(validator({ inputSchema: schema })) // validates request body to see if it matches schema
+  .use(httpErrorHandler()); // http error handlers and responses
 
 export { handler };
+
+// This is the model output for the schema of the portfoliostep, which will be taken from our api yaml as a TODO
+// Currently it is being exported from the PortfolioStep Model, but it needs to be taken right from the spec.
+// A problem in retrieving the model is that you need to wrap the model in a body object for it to validate properly
+// Below is the schema used in validation:
+/*
+const schema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        name: { type: "string" },
+        csp: {
+          type: "string",
+          enum: ["aws", "azure"],
+        },
+        portfolio_managers: {
+          type: "array",
+          items: {
+            type: "string",
+            format: "email",
+          },
+        },
+        dod_components: {
+          type: "array",
+          items: {
+            type: "string",
+            enum: [
+              "air_force",
+              "army",
+              "marine_corps",
+              "navy",
+              "space_force",
+              "combatant_command",
+              "joint_staff",
+              "dafa",
+              "osd_psas",
+              "nsa",
+            ],
+          },
+        },
+        description: {
+          type: "string",
+        },
+      },
+      required: ["name", "portfolio_managers", "description", "csp"], // Insert here all required event properties
+    },
+  },
+}; */
+// This is the schema output from our API yaml spec:
+/*
+{
+  "required" : [ "csp", "dod_components", "name", "portfolio_managers" ],
+  "type" : "object",
+  "properties" : {
+    "csp" : {
+      "type" : "string",
+      "enum" : [ "aws", "azure" ]
+    },
+    "portfolio_managers" : {
+      "type" : "array",
+      "items" : {
+        "type" : "string",
+        "format" : "email"
+      }
+    },
+    "dod_components" : {
+      "type" : "array",
+      "items" : {
+        "type" : "string",
+        "enum" : [ "air_force", "army", "marine_corps", "navy", "space_force", "combatant_command", "joint_staff", "dafa", "osd_psas", "nsa" ]
+      }
+    },
+    "name" : {
+      "type" : "string"
+    },
+    "description" : {
+      "type" : "string"
+    }
+  },
+  "additionalProperties" : false,
+  "description" : "Represents step 1 of the Portfolio Draft Wizard"
+}
+*/
+// Notice how properties is wrapped in a body object, which is required to validate the event.body of the APIGatewayProxyEvent
