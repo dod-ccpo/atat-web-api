39f94a97fffa1b74f21c34abbee336c0f3aa4a11
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-11 11:36:47 -0400

    Edit schema to include wrapper for event.body (APIGatewayProxyEvent)
    When using schema to validate the request body, we need to wrap all schema
    inside an APIGatewayProxyEvent event.body format so it can be used with
    middy validation.

--- packages/api/models/PortfolioStep.ts
+++ packages/api/models/PortfolioStep.ts
@@ -8,46 +8,53 @@ export interface PortfolioStep {
 }
 
 export const schema = {
+  required: ["csp", "dod_components", "name", "portfolio_managers"],
   type: "object",
   properties: {
-    body: {
-      type: "object",
-      properties: {
-        name: { type: "string" },
-        csp: {
-          type: "string",
-          enum: ["aws", "azure"],
-        },
-        portfolio_managers: {
-          type: "array",
-          items: {
-            type: "string",
-            format: "email",
-          },
-        },
-        dod_components: {
-          type: "array",
-          items: {
-            type: "string",
-            enum: [
-              "air_force",
-              "army",
-              "marine_corps",
-              "navy",
-              "space_force",
-              "combatant_command",
-              "joint_staff",
-              "dafa",
-              "osd_psas",
-              "nsa",
-            ],
-          },
-        },
-        description: {
-          type: "string",
-        },
-      },
-      required: ["name", "portfolio_managers", "description", "csp"], // Insert here all required event properties
+    csp: {
+      type: "string",
+      enum: ["aws", "azure"],
     },
+    portfolio_managers: {
+      type: "array",
+      items: {
+        type: "string",
+        format: "email",
+      },
+    },
+    dod_components: {
+      type: "array",
+      items: {
+        type: "string",
+        enum: [
+          "air_force",
+          "army",
+          "marine_corps",
+          "navy",
+          "space_force",
+          "combatant_command",
+          "joint_staff",
+          "dafa",
+          "osd_psas",
+          "nsa",
+        ],
+      },
+    },
+    name: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  additionalProperties: false,
+  description: "Represents step 1 of the Portfolio Draft Wizard",
+};
+// Because the validator checks the ApiGatewayProxyEvent event.body, we need to wrap it in a body.
+export const schemaWrapper = {
+  type: "object",
+  required: ["body"],
+  properties: {
+    body: schema,
   },
 };
