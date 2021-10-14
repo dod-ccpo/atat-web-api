78759dbffab979148f236f81ec06272ab7d809f0
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-14 04:18:52 -0400

    Remove excess expect statements
    Ticket: AT-6615

--- packages/api/portfolioDrafts/xssSanitizer.test.ts
+++ packages/api/portfolioDrafts/xssSanitizer.test.ts
@@ -8,8 +8,5 @@ describe("Useable with middy", function () {
     expect(handler.__middlewares.before[0]).not.toBeDefined();
     handler.use(middleware);
     expect(handler.__middlewares.before[0]).toBeDefined();
-    // didn't add these
-    expect(handler.__middlewares.after[0]).not.toBeDefined();
-    expect(handler.__middlewares.onError[0]).not.toBeDefined();
   });
 });
