573c7fae7a312421b0571f4c19c1cd69cc19edd3
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-14 01:05:30 -0400

    Add demonstrative unit test for XSS sanitization Work-in-progress
    Ticket: AT-6615

--- packages/api/portfolioDrafts/portfolio/createPortfolioStep.test.ts
+++ packages/api/portfolioDrafts/portfolio/createPortfolioStep.test.ts
@@ -1,14 +1,15 @@
-import { APIGatewayProxyEvent, Callback, Context } from "aws-lambda";
+import { Callback, Context } from "aws-lambda";
 import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
 import { CloudServiceProvider } from "../../models/CloudServiceProvider";
 import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { handler } from "./createPortfolioStep";
 import { mockClient } from "aws-sdk-client-mock";
-import { NO_SUCH_PORTFOLIO_DRAFT, REQUEST_BODY_INVALID } from "../../utils/errors";
+import { NO_SUCH_PORTFOLIO_DRAFT } from "../../utils/errors";
 import { PortfolioStep } from "../../models/PortfolioStep";
 import { ProvisioningStatus } from "../../models/ProvisioningStatus";
 import { v4 as uuidv4 } from "uuid";
 import { ApiGatewayEventParsed } from "../../utils/eventHandlingTool";
+import xss from "xss";
 
 const ddbMock = mockClient(DynamoDBDocumentClient);
 beforeEach(() => {
@@ -94,5 +95,31 @@ describe.each(mockValidPortfolioSteps)("Handler response with mock dynamodb", (m
     ddbMock.on(UpdateCommand).rejects({ name: "ConditionalCheckFailedException" });
     const data = await handler(request, {} as Context, null as unknown as Callback)!;
     expect(data).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
+  });
+});
+
+describe("Cross-Site Scripting (XSS) tests", () => {
+  // example attacks from ASD STIG: V-222602 text
+  const attacks = ["<script>alert('attack!')</script>", "<img src=x onerror='alert(document.cookie);'"];
+  it.each(attacks)("should sanitize input for XSS attacks", async (attackCode) => {
+    const xssAttackPortfolioStep = {
+      ...mockValidPortfolioSteps[0],
+      description: attackCode,
+      name: attackCode,
+    };
+    // TODO: Place this in impl, put impl here
+    const sanitizedAttackCode = xss(attackCode);
+    // expect inputs to contain an XSS attack
+    expect(xssAttackPortfolioStep.description).toEqual(attackCode);
+    expect(xssAttackPortfolioStep.name).toEqual(attackCode);
+    // confirm attack is present in the input
+    const input = JSON.stringify(xssAttackPortfolioStep);
+    expect(input).toContain(attackCode);
+    expect(input).not.toContain(sanitizedAttackCode);
+    // scrub the output
+    const output = xss(JSON.stringify(xssAttackPortfolioStep));
+    // confirm attack is not present in the output
+    expect(output).not.toContain(attackCode);
+    expect(output).toContain(sanitizedAttackCode);
   });
 });
