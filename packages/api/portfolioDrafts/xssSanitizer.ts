feb3110d82be0f4a80a84445efc519489d39b34a
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-10-14 02:48:25 -0400

    Add options to for xss
    Ticket: AT-6615

--- packages/api/portfolioDrafts/xssSanitizer.ts
+++ packages/api/portfolioDrafts/xssSanitizer.ts
@@ -2,9 +2,15 @@ import middy from "@middy/core";
 import xss from "xss";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
+// keep plain text only
+const xssOptions = {
+  whiteList: {}, // empty means remove all tags
+  stripIgnoreTag: true, // remove all tags not in whitelist
+  stripIgnoreTagBody: ["script"], // remove script tag content
+};
 const xssSanitizer = (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
   const before: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async (request): Promise<void> => {
-    request.event.body = xss(request.event.body ?? "");
+    request.event.body = xss(request.event.body ?? "", xssOptions);
   };
   return {
     before,
