f84b77a2d6a20e288910a284671951284b7728ac
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-25 16:28:23 -0400

    Update load-schema.ts to properly generate a schema.json file

--- infrastructure/lib/load-schema.ts
+++ infrastructure/lib/load-schema.ts
@@ -4,13 +4,15 @@ import toJsonSchema from "@openapi-contrib/openapi-schema-to-json-schema";
 import * as utils from "./util";
 export function convertSchema() {
   try {
-    const schema = utils.packageRoot() + "../../atat_provisioning_wizard_api.yaml";
-    const fileContents = fs.readFileSync(schema, "utf8");
-    const data = load(fileContents);
-    console.log(data);
+    const schema = utils.packageRoot() + "/../atat_provisioning_wizard_api.yaml";
+    const fileContents = fs.readFileSync(schema, { encoding: "utf8" });
+    const data = load(fileContents, {
+      onWarning: console.log,
+    });
+    // console.log(data);
     // data is now loaded, we can now convert it to json schema with toJsonSchema
     const convertedSchema = toJsonSchema(data as any);
-    fs.writeFileSync("schema.json", convertedSchema as any);
+    fs.writeFileSync("schema.json", JSON.stringify(convertedSchema));
   } catch (e) {
     console.log(e);
   }
