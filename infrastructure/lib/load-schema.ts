70dc400e9d0a250fd498e7065f838b543fedf68d
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-26 15:30:10 -0400

    Remove comments, change import to * as yaml in response to PR review

--- infrastructure/lib/load-schema.ts
+++ infrastructure/lib/load-schema.ts
@@ -1,15 +1,14 @@
 import * as fs from "fs";
-import { load } from "js-yaml";
+import * as yaml from "js-yaml";
 import toJsonSchema from "@openapi-contrib/openapi-schema-to-json-schema";
 import * as utils from "./util";
 export function convertSchema(): void {
   try {
     const schema = utils.packageRoot() + "/../atat_provisioning_wizard_api.yaml";
     const fileContents = fs.readFileSync(schema, { encoding: "utf8" });
-    const data = load(fileContents, {
+    const data = yaml.load(fileContents, {
       onWarning: console.log,
     });
-    // console.log(data);
     // data is now loaded, we can now convert it to json schema with toJsonSchema
     const convertedSchema = toJsonSchema(data as any);
     const convertedSchemaDestination = utils.packageRoot() + "/api/models/schema.json";
