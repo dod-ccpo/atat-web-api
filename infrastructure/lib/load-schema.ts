882e98b74e4bb7e5525f0ddb14a4d50cfc039a95
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-10-27 17:39:51 -0400

    Update load-schema to properly handle $refs...
    - add a $RefParser to resolve $refs in our new JSON schema (our middleware validators
    don't handle this)
    - instead of writing the entire API, now we are only writing the schema as JSON objects
    - updated createPortfolioStep to use new path

--- infrastructure/lib/load-schema.ts
+++ infrastructure/lib/load-schema.ts
@@ -2,8 +2,11 @@ import * as fs from "fs";
 import * as yaml from "js-yaml";
 import toJsonSchema from "@openapi-contrib/openapi-schema-to-json-schema";
 import * as utils from "./util";
-export function convertSchema(): void {
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+
+export async function convertSchema(): Promise<void> {
   try {
+    const parser = new $RefParser();
     const schema = utils.packageRoot() + "/../atat_provisioning_wizard_api.yaml";
     const fileContents = fs.readFileSync(schema, { encoding: "utf8" });
     const data = yaml.load(fileContents, {
@@ -11,8 +14,13 @@ export function convertSchema(): void {
     });
     // data is now loaded, we can now convert it to json schema with toJsonSchema
     const convertedSchema = toJsonSchema(data as any);
-    const convertedSchemaDestination = utils.packageRoot() + "/api/models/schema.json";
-    fs.writeFileSync(convertedSchemaDestination, JSON.stringify(convertedSchema));
+    // now we need to resolve any $refs so our middleware schema validator can properly parse the schema
+    const resolvedSchema = await parser.dereference(convertedSchema);
+    // due to a typescript quirk, we can't directly reference the resolvedSchema.components.schemas, this is the work around
+    const schemaToExport = resolvedSchema as any;
+    const exportedSchemaDestination = utils.packageRoot() + "/api/models/schema.json";
+    // create a schema.json file, only containing the schemas
+    fs.writeFileSync(exportedSchemaDestination, JSON.stringify(schemaToExport.components.schemas));
   } catch (e) {
     console.log(e);
   }
