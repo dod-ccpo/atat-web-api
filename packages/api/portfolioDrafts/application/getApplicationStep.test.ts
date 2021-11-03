2d7f63454b0f805f687b47c547b43e636cf6fad9
Julius Fitzhugh (Hunter Strategy) <julius.fitzhugh-ctr@ccpo.mil>
2021-11-02 23:52:58 -0400

    Update getApplicationStep to use middy
    Wrap getApplicationStep with middy so that additional middleware
    can be added in the future. Update unit tests.
    
    Ticket: AT-6723

--- packages/api/portfolioDrafts/application/getApplicationStep.test.ts
+++ packages/api/portfolioDrafts/application/getApplicationStep.test.ts
@@ -1,71 +1,78 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { Context } from "aws-lambda";
 import { ApiSuccessResponse, ErrorStatusCode, OtherErrorResponse, SuccessStatusCode } from "../../utils/response";
-import { DATABASE_ERROR, NO_SUCH_APPLICATION_STEP, PATH_PARAMETER_REQUIRED_BUT_MISSING } from "../../utils/errors";
+import { DATABASE_ERROR, NO_SUCH_APPLICATION_STEP, NO_SUCH_PORTFOLIO_DRAFT } from "../../utils/errors";
 import { DynamoDBDocumentClient, GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
-import { handler, NO_SUCH_PORTFOLIO_DRAFT } from "./getApplicationStep";
 import { mockApplicationStep } from "./commonApplicationMockData";
 import { mockClient } from "aws-sdk-client-mock";
-import { validRequest } from "../commonPortfolioDraftMockData";
+import { handler, NO_SUCH_PORTFOLIO_DRAFT_FOUND } from "./getApplicationStep";
+import { v4 as uuidv4 } from "uuid";
+import { ApiGatewayEventParsed } from "../../utils/eventHandlingTool";
+import { ApplicationStep } from "../../models/ApplicationStep";
 
 const ddbMock = mockClient(DynamoDBDocumentClient);
 beforeEach(() => {
   ddbMock.reset();
 });
 
+const validRequest: ApiGatewayEventParsed<ApplicationStep> = {
+  body: {},
+  pathParameters: { portfolioDraftId: uuidv4() },
+} as any;
+
 it("should return generic Error if exception caught", async () => {
   jest.spyOn(console, "error").mockImplementation(() => jest.fn()); // suppress output
   ddbMock.on(GetCommand).rejects("Some error occurred");
-  const result = await handler(validRequest);
+  const result = await handler(validRequest, {} as Context, () => null);
   expect(result).toBeInstanceOf(OtherErrorResponse);
   expect(result).toEqual(DATABASE_ERROR);
-  expect(result.statusCode).toEqual(ErrorStatusCode.INTERNAL_SERVER_ERROR);
+  expect(result?.statusCode).toEqual(ErrorStatusCode.INTERNAL_SERVER_ERROR);
 });
 it("should require path param", async () => {
-  const emptyRequest: APIGatewayProxyEvent = {} as any;
-  const result = await handler(emptyRequest);
-  expect(result).toBeInstanceOf(OtherErrorResponse);
-  expect(result).toEqual(PATH_PARAMETER_REQUIRED_BUT_MISSING);
-  expect(result.statusCode).toEqual(ErrorStatusCode.BAD_REQUEST);
-});
-it("should return error when path param not UUIDv4 (to avoid performing query)", async () => {
-  const invalidRequest: APIGatewayProxyEvent = {
-    pathParameters: { portfolioDraftId: "invalid" },
-  } as any;
-  const result = await handler(invalidRequest);
+  const emptyRequest: ApiGatewayEventParsed<ApplicationStep> = {} as any;
+  const result = await handler(emptyRequest, {} as Context, () => null);
   expect(result).toBeInstanceOf(OtherErrorResponse);
   expect(result).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
-  expect(result.statusCode).toEqual(ErrorStatusCode.BAD_REQUEST);
-  expect(JSON.parse(result.body).message).toMatch(/The given Portfolio Draft does not exist/);
+  expect(result?.statusCode).toEqual(ErrorStatusCode.NOT_FOUND);
+});
+it("should return error when path param not UUIDv4 (to avoid performing query)", async () => {
+  const invalidRequest: ApiGatewayEventParsed<ApplicationStep> = {
+    pathParameters: { portfolioDraftId: "invalid" },
+  } as any;
+  const result = await handler(invalidRequest, {} as Context, () => null);
+  expect(result).toBeInstanceOf(OtherErrorResponse);
+  expect(result).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
+  expect(result?.statusCode).toEqual(ErrorStatusCode.NOT_FOUND);
+  expect(JSON.parse(result?.body ?? "").message).toMatch(/Portfolio Draft with the given ID does not exist/);
 });
 it("should return error if portfolio draft does not exist", async () => {
   const emptyOutput: GetCommandOutput = {} as any;
   ddbMock.on(GetCommand).resolves(emptyOutput);
-  const result = await handler(validRequest);
+  const result = await handler(validRequest, {} as Context, () => null);
   expect(result).toBeInstanceOf(OtherErrorResponse);
-  expect(result).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
-  expect(result.statusCode).toEqual(ErrorStatusCode.BAD_REQUEST);
-  expect(JSON.parse(result.body).message).toMatch(/The given Portfolio Draft does not exist/);
+  expect(result).toEqual(NO_SUCH_PORTFOLIO_DRAFT_FOUND);
+  expect(result?.statusCode).toEqual(ErrorStatusCode.BAD_REQUEST);
+  expect(JSON.parse(result?.body ?? "").message).toMatch(/The given Portfolio Draft does not exist/);
 });
 it("should return error if application step does not exist in portfolio draft", async () => {
   const itemOutput: GetCommandOutput = { Item: {} } as any;
   ddbMock.on(GetCommand).resolves(itemOutput);
-  const result = await handler(validRequest);
+  const result = await handler(validRequest, {} as Context, () => null);
   expect(result).toBeInstanceOf(OtherErrorResponse);
   expect(result).toEqual(NO_SUCH_APPLICATION_STEP);
-  expect(result.statusCode).toEqual(ErrorStatusCode.NOT_FOUND);
-  expect(JSON.parse(result.body).message).toMatch(/Application Step not found for this Portfolio Draft/);
+  expect(result?.statusCode).toEqual(ErrorStatusCode.NOT_FOUND);
+  expect(JSON.parse(result?.body ?? "").message).toMatch(/Application Step not found for this Portfolio Draft/);
 });
 it("should return application step", async () => {
   const appStepOutput: GetCommandOutput = { Item: { application_step: {} } } as any;
   ddbMock.on(GetCommand).resolves(appStepOutput);
-  const result = await handler(validRequest);
+  const result = await handler(validRequest, {} as Context, () => null);
   expect(result).toBeInstanceOf(ApiSuccessResponse);
-  expect(result.statusCode).toEqual(SuccessStatusCode.OK);
-  expect(result.body).toEqual(JSON.stringify({}));
+  expect(result?.statusCode).toEqual(SuccessStatusCode.OK);
+  expect(result?.body ?? "").toEqual(JSON.stringify({}));
 });
 it("should return application step with sample data", async () => {
   const fullAppStepOutput: GetCommandOutput = { Item: { application_step: mockApplicationStep } } as any;
   ddbMock.on(GetCommand).resolves(fullAppStepOutput);
-  const result = await handler(validRequest);
-  expect(result.body).toStrictEqual(JSON.stringify(mockApplicationStep));
+  const result = await handler(validRequest, {} as Context, () => null);
+  expect(result?.body ?? "").toStrictEqual(JSON.stringify(mockApplicationStep));
 });
--- packages/api/portfolioDrafts/application/getApplicationStep.ts
+++ packages/api/portfolioDrafts/application/getApplicationStep.ts
@@ -1,14 +1,23 @@
 import { GetCommand } from "@aws-sdk/lib-dynamodb";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyResult, Context } from "aws-lambda";
 import { ApplicationStep } from "../../models/ApplicationStep";
 import { APPLICATION_STEP } from "../../models/PortfolioDraft";
 import { dynamodbDocumentClient as client } from "../../utils/aws-sdk/dynamodb";
-import { DATABASE_ERROR, NO_SUCH_APPLICATION_STEP, PATH_PARAMETER_REQUIRED_BUT_MISSING } from "../../utils/errors";
-import { ApiSuccessResponse, ErrorStatusCode, OtherErrorResponse, SuccessStatusCode } from "../../utils/response";
-import { isPathParameterPresent, isValidUuidV4 } from "../../utils/validation";
+import { DATABASE_ERROR, NO_SUCH_APPLICATION_STEP } from "../../utils/errors";
+import {
+  ApiSuccessResponse,
+  ErrorStatusCode,
+  OtherErrorResponse,
+  SetupError,
+  SuccessStatusCode,
+} from "../../utils/response";
+import middy from "@middy/core";
+import cors from "@middy/http-cors";
+import { ApiGatewayEventParsed } from "../../utils/eventHandlingTool";
+import { shapeValidationForPostRequest } from "../../utils/requestValidation";
 
 // Note that API spec calls for 400 and not 404
-export const NO_SUCH_PORTFOLIO_DRAFT = new OtherErrorResponse(
+export const NO_SUCH_PORTFOLIO_DRAFT_FOUND = new OtherErrorResponse(
   "The given Portfolio Draft does not exist",
   ErrorStatusCode.BAD_REQUEST
 );
@@ -18,14 +27,16 @@ export const NO_SUCH_PORTFOLIO_DRAFT = new OtherErrorResponse(
  *
  * @param event - The GET request from API Gateway
  */
-export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  const portfolioDraftId = event.pathParameters?.portfolioDraftId;
-  if (!isPathParameterPresent(portfolioDraftId)) {
-    return PATH_PARAMETER_REQUIRED_BUT_MISSING;
+export async function baseHandler(
+  event: ApiGatewayEventParsed<ApplicationStep>,
+  context?: Context
+): Promise<APIGatewayProxyResult> {
+  const setupResult = shapeValidationForPostRequest<ApplicationStep>(event);
+  if (setupResult instanceof SetupError) {
+    return setupResult.errorResponse;
   }
-  if (!isValidUuidV4(portfolioDraftId)) {
-    return NO_SUCH_PORTFOLIO_DRAFT;
-  }
+  const portfolioDraftId = setupResult.path.portfolioDraftId;
+
   try {
     const result = await client.send(
       new GetCommand({
@@ -37,7 +48,7 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
       })
     );
     if (!result.Item) {
-      return NO_SUCH_PORTFOLIO_DRAFT;
+      return NO_SUCH_PORTFOLIO_DRAFT_FOUND;
     }
     if (!result.Item?.application_step) {
       return NO_SUCH_APPLICATION_STEP;
@@ -51,3 +62,5 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
     return DATABASE_ERROR;
   }
 }
+
+export const handler = middy(baseHandler).use(cors({ headers: "*", methods: "*" }));
