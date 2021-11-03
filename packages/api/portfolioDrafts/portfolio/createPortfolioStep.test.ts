1f62134345b77d0a477f4056b407f9a1e1194b1b
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-11-03 17:13:08 -0400

    Refactor tests
    Ticket: AT-6739

--- packages/api/portfolioDrafts/portfolio/createPortfolioStep.test.ts
+++ packages/api/portfolioDrafts/portfolio/createPortfolioStep.test.ts
@@ -5,44 +5,35 @@ import { handler } from "./createPortfolioStep";
 import { mockClient } from "aws-sdk-client-mock";
 import { NO_SUCH_PORTFOLIO_DRAFT } from "../../utils/errors";
 import { PortfolioStep } from "../../models/PortfolioStep";
-import { ProvisioningStatus } from "../../models/ProvisioningStatus";
-import { v4 as uuidv4 } from "uuid";
 import { ApiGatewayEventParsed } from "../../utils/eventHandlingTool";
 import { mockPortfolioStep, mockValidPortfolioSteps } from "./commonPortfolioMockData";
+import { validRequest } from "../commonPortfolioDraftMockData";
 
 const ddbMock = mockClient(DynamoDBDocumentClient);
 beforeEach(() => {
   ddbMock.reset();
 });
 
-const now = new Date().toISOString();
-
 describe("Successful operations test", () => {
-  const mockResponse = {
-    updated_at: now,
-    created_at: now,
-    status: ProvisioningStatus.NOT_STARTED,
-    id: uuidv4(),
-  };
-  it.each(mockValidPortfolioSteps)("should return portfolio step and http status code 201", async (requestBody) => {
-    ddbMock.on(UpdateCommand).resolves({
-      Attributes: mockResponse,
-    });
-
-    const validRequest: ApiGatewayEventParsed<PortfolioStep> = {
-      body: requestBody,
-      pathParameters: { portfolioDraftId: mockResponse.id },
-    } as any;
-    const response = await handler(validRequest, {} as Context, null as unknown as Callback)!;
-    expect(response).toBeInstanceOf(ApiSuccessResponse);
-    expect(response.statusCode).toEqual(SuccessStatusCode.CREATED);
-  });
+  it.each(mockValidPortfolioSteps)(
+    "should return portfolio step and http status code 201",
+    async (mockPortfolioStep) => {
+      const request: ApiGatewayEventParsed<PortfolioStep> = {
+        ...validRequest,
+        body: mockPortfolioStep,
+      } as any;
+      const response = await handler(request, {} as Context, null as unknown as Callback)!;
+      expect(response).toBeInstanceOf(ApiSuccessResponse);
+      expect(response.statusCode).toEqual(SuccessStatusCode.CREATED);
+    }
+  );
 });
 
 describe("Validation of handler", () => {
-  it("should return NO_SUCH_PORTFOLIO_DRAFT when no portfolioId specified", async () => {
+  it("should return NO_SUCH_PORTFOLIO_DRAFT when no portfolioDraftId specified", async () => {
     const request = {
       body: mockPortfolioStep,
+      // no pathParameter portfolioDraftId
     } as ApiGatewayEventParsed<PortfolioStep>;
     const response = await handler(request, {} as Context, null as unknown as Callback)!;
     expect(response).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
@@ -51,15 +42,14 @@ describe("Validation of handler", () => {
 
 describe.each(mockValidPortfolioSteps)("Handler response with mock dynamodb", (mockPortfolioStep) => {
   const request: ApiGatewayEventParsed<PortfolioStep> = {
+    ...validRequest,
     body: mockPortfolioStep,
-    pathParameters: { portfolioDraftId: uuidv4() },
   } as any;
 
   it("should return error when the portfolioDraft doesn't exist", async () => {
     ddbMock.on(UpdateCommand).rejects({ name: "ConditionalCheckFailedException" });
-    const data = await handler(request, {} as Context, null as unknown as Callback)!;
-    console.log(data);
-    expect(data).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
+    const response = await handler(request, {} as Context, null as unknown as Callback)!;
+    expect(response).toEqual(NO_SUCH_PORTFOLIO_DRAFT);
   });
 });
 
@@ -67,14 +57,14 @@ describe("Cross-Site Scripting (XSS) tests", () => {
   // example attacks from ASD STIG: V-222602 text
   const attacks = ["<script>alert('attack!')</script>", "<img src=x onerror='alert(document.cookie);'"];
   it.each(attacks)("should sanitize input for XSS attacks", async (attackCode) => {
-    const requestBody: PortfolioStep = {
+    const mockPortfolioStep: PortfolioStep = {
       ...mockValidPortfolioSteps[0],
       description: attackCode,
       name: attackCode,
     };
     const request: ApiGatewayEventParsed<PortfolioStep> = {
-      body: requestBody,
-      pathParameters: { portfolioDraftId: uuidv4() },
+      ...validRequest,
+      body: mockPortfolioStep,
     } as any;
     // expect request to contain an XSS attack
     expect(request.body.description).toEqual(attackCode);
