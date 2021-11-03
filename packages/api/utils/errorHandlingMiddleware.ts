14f4b86dfaa037fb5f121c5d8efb6560c037f6e9
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-11-03 18:06:00 -0400

    Created errorHandlingMiddleware.ts to wrap validation errors
    Created a new middleware with onError to catch middy errors and
    business rules validation errors. It wraps the error in a new
    ValidationErrorResponse, returning a code, message, and error_map.
    
    Decided to implement this now so I wouldn't have to rewrite as many
    unit tests in AT-6734.

---
+++ packages/api/utils/errorHandlingMiddleware.ts
@@ -0,0 +1,30 @@
+import middy from "@middy/core";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { serializeError } from "serialize-error";
+import { ValidationErrorResponse } from "./response";
+
+const errorHandlingMiddleware = (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
+  const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async (
+    request
+  ): Promise<ValidationErrorResponse | void> => {
+    const error = serializeError(request.error)!;
+    // Catch middy validation error, wrap it in ValidationErrorResponse
+    if (error.message === "Event object failed validation") {
+      return new ValidationErrorResponse("Request failed validation", error.details as Record<string, unknown>);
+    }
+    if (error.message === "Business rules validation failed") {
+      const errorMap = error.details as Record<string, unknown>;
+      const businessRules = errorMap.input_validation_errors;
+      // should we keep this inside of input_validation_errors?
+      return new ValidationErrorResponse(
+        "Request failed validation (business rules)",
+        businessRules as Record<string, unknown>
+      );
+    }
+  };
+  return {
+    onError,
+  };
+};
+
+export default errorHandlingMiddleware;
