ace634262ecda2bde781529fa12b51218f8e9892
Matthew D'vertola (Hunter Strategy) <92047706+matthewdvertola-ccpo@users.noreply.github.com>
2021-11-05 08:32:39 -0700

    Delete index.ts

--- packages/cognito/postAuthentication/index.ts
+++
@@ -1,8 +0,0 @@
-import { PostAuthenticationTriggerEvent } from "aws-lambda";
-
-export async function handler(event: PostAuthenticationTriggerEvent): Promise<PostAuthenticationTriggerEvent> {
-  // currently returning the event passed to lambda on trigger
-  // should serve as a baseline for log details (may need ajustments/ parsing to clean up)
-  console.log(JSON.stringify(event));
-  return event;
-}
