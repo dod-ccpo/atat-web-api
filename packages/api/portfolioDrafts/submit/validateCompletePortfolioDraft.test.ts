8a99aad6f22eb9bb5f0b859818015ca020ae0327
Julius Fitzhugh (Hunter Strategy) <julius.fitzhugh-ctr@ccpo.mil>
2021-11-09 10:29:43 -0500

    Add sfnClient mock and remove console logs from tests
    Ticket: AT-6743

--- packages/api/portfolioDrafts/submit/validateCompletePortfolioDraft.test.ts
+++ packages/api/portfolioDrafts/submit/validateCompletePortfolioDraft.test.ts
@@ -4,7 +4,6 @@ import { v4 as uuidv4 } from "uuid";
 import { APPLICATION_STEP, FUNDING_STEP, PORTFOLIO_STEP } from "../../models/PortfolioDraft";
 import { ProvisioningStatus } from "../../models/ProvisioningStatus";
 import { Context } from "aws-lambda";
-import util from "util";
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -16,20 +15,8 @@ describe("validate portfolio draft submission", () => {
     const completedPortfolioDraft: any = {
       body: { ...mockPortfolioDraft, submit_id: uuidv4(), status: ProvisioningStatus.IN_PROGRESS },
     };
-    const modifiedPortfolioDraft: any = {
-      body: {
-        portfolio_step: mockPortfolioDraft[PORTFOLIO_STEP],
-        funding_step: mockPortfolioDraft[FUNDING_STEP],
-        application_step: mockPortfolioDraft[APPLICATION_STEP],
-        id: uuidv4(),
-        submit_id: uuidv4(),
-        status: ProvisioningStatus.IN_PROGRESS,
-      },
-    };
 
     const response = await handler(completedPortfolioDraft, {} as Context, () => null);
-    // const response = await handler(modifiedPortfolioDraft, {} as Context, () => null);
-    console.log("SUCCESS validation Response: " + util.inspect(response, false, null, true));
     expect(response?.validationResult).toEqual(ValidationResult.SUCCESS);
   });
   it("should return an error with validating the portfolio draft", async () => {
@@ -44,8 +31,6 @@ describe("validate portfolio draft submission", () => {
       },
     };
     const response = await handler(modifiedBadPortfolioDraft, {} as Context, () => null);
-    console.log("FAILED validation Response: " + util.inspect(response, false, null, true));
-    console.log("EVENT: " + util.inspect(response, false, null, true));
     expect(response?.validationResult).toEqual(ValidationResult.FAILED);
   });
 });
