0df7f8eedea0d04f5b180b7e710de8921e7b19b7
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-11-10 10:53:50 -0500

    Update errorHandlingMiddleware...
    Changed from if statement block to switch statement with each error.message
    evaluated as the case. This will increase performance and looks much cleaner

--- packages/api/utils/errorHandlingMiddleware.ts
+++ packages/api/utils/errorHandlingMiddleware.ts
@@ -2,16 +2,37 @@ import middy from "@middy/core";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { serializeError } from "serialize-error";
 import { ValidationErrorResponse } from "./response";
-import { REQUEST_BODY_INVALID } from "./errors";
+import { NO_SUCH_PORTFOLIO_DRAFT_404, REQUEST_BODY_INVALID } from "./errors";
 
 export const errorHandlingMiddleware = (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
   const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async (
     request
   ): Promise<ValidationErrorResponse | void> => {
     const error = serializeError(request.error)!;
+    const errorMessage = error?.message as string;
+    // const errorMap = error?.details as Record<string, unknown>;
+    // const businessRules = errorMap.input_validation_errors;
+    switch (errorMessage) {
+      case "Event object failed validation":
+        return new ValidationErrorResponse("Request failed validation", error.details as Record<string, unknown>);
+      case "Business rules validation failed":
+        // const errorMap = error.details as Record<string, unknown>;
+        // const businessRules = errorMap.input_validation_errors;
+        // should we keep this inside of input_validation_errors?
+        return new ValidationErrorResponse(
+          "Request failed validation (business rules)",
+          error.error_map as Record<string, unknown>
+        );
+      case "Shape validation failed, invalid UUIDv4":
+        return NO_SUCH_PORTFOLIO_DRAFT_404;
+      default:
+        console.log(error.message);
+    }
+
     // AT-6734 catch setup error
     // AT-6734 catch DB error
     // Catch middy validation error, wrap it in ValidationErrorResponse
+    /*
     if (error.message === "Event object failed validation") {
       return new ValidationErrorResponse("Request failed validation", error.details as Record<string, unknown>);
     }
@@ -24,9 +45,20 @@ export const errorHandlingMiddleware = (): middy.MiddlewareObj<APIGatewayProxyEv
         businessRules as Record<string, unknown>
       );
     }
-    if (error.name === "UnprocessableEntityError") {
+    if (error.message === "Shape validation failed, invalid UUIDv4") {
+      return NO_SUCH_PORTFOLIO_DRAFT_404;
+    }
+    if (
+      error.message === "Shape validation failed, invalid request body" ||
+      error.name === "UnprocessableEntityError"
+    ) {
       return REQUEST_BODY_INVALID;
     }
+    */
+    /*
+    if (error.name === "UnprocessableEntityError") {
+      return REQUEST_BODY_INVALID;
+    } */
   };
   return {
     onError,
