88401686577340dfbeda4bb7a42eafc76a1df06d
Zachary Clark <zach.clark-ctr@ccpo.mil>
2021-11-10 13:08:03 -0500

    Clean-up comments on errorHandlingMiddleware

--- packages/api/utils/errorHandlingMiddleware.ts
+++ packages/api/utils/errorHandlingMiddleware.ts
@@ -10,55 +10,21 @@ export const errorHandlingMiddleware = (): middy.MiddlewareObj<APIGatewayProxyEv
   ): Promise<ValidationErrorResponse | void> => {
     const error = serializeError(request.error)!;
     const errorMessage = error?.message as string;
-    // const errorMap = error?.details as Record<string, unknown>;
-    // const businessRules = errorMap.input_validation_errors;
     switch (errorMessage) {
       case "Event object failed validation":
         return new ValidationErrorResponse("Request failed validation", error.details as Record<string, unknown>);
       case "Business rules validation failed":
-        // const errorMap = error.details as Record<string, unknown>;
-        // const businessRules = errorMap.input_validation_errors;
-        // should we keep this inside of input_validation_errors?
         return new ValidationErrorResponse(
           "Request failed validation (business rules)",
           error.error_map as Record<string, unknown>
         );
       case "Shape validation failed, invalid UUIDv4":
         return NO_SUCH_PORTFOLIO_DRAFT_404;
-      default:
-        console.log(error.message);
+      case "Shape validation failed, invalid request body":
+        return REQUEST_BODY_INVALID;
+      case "Content type defined as JSON but an invalid JSON was provided":
+        return REQUEST_BODY_INVALID;
     }
-
-    // AT-6734 catch setup error
-    // AT-6734 catch DB error
-    // Catch middy validation error, wrap it in ValidationErrorResponse
-    /*
-    if (error.message === "Event object failed validation") {
-      return new ValidationErrorResponse("Request failed validation", error.details as Record<string, unknown>);
-    }
-    if (error.message === "Business rules validation failed") {
-      const errorMap = error.details as Record<string, unknown>;
-      const businessRules = errorMap.input_validation_errors;
-      // should we keep this inside of input_validation_errors?
-      return new ValidationErrorResponse(
-        "Request failed validation (business rules)",
-        businessRules as Record<string, unknown>
-      );
-    }
-    if (error.message === "Shape validation failed, invalid UUIDv4") {
-      return NO_SUCH_PORTFOLIO_DRAFT_404;
-    }
-    if (
-      error.message === "Shape validation failed, invalid request body" ||
-      error.name === "UnprocessableEntityError"
-    ) {
-      return REQUEST_BODY_INVALID;
-    }
-    */
-    /*
-    if (error.name === "UnprocessableEntityError") {
-      return REQUEST_BODY_INVALID;
-    } */
   };
   return {
     onError,
