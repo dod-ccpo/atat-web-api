da7b8390ad046468b0574155be6cbeb56509c332
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-11-19 13:30:33 -0500

    Improve portfolio entity
    Ticket: AT-6836

--- packages/api/orm/entity/portfolio.ts
+++ packages/api/orm/entity/portfolio.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+import { CloudServiceProvider } from "../../models/CloudServiceProvider";
 import { ProvisioningStatus } from "../../models/ProvisioningStatus";
 
 @Entity()
@@ -12,6 +13,40 @@ export class Portfolio {
     default: ProvisioningStatus.NOT_STARTED,
   })
   status: string;
+
+  @Column({
+    type: String,
+    nullable: false,
+    length: 100,
+  })
+  name: string;
+
+  @Column({
+    type: String,
+    nullable: true,
+    length: 300,
+  })
+  description: string;
+
+  @Column({
+    type: "enum",
+    enum: CloudServiceProvider,
+  })
+  csp: CloudServiceProvider;
+
+  @Column({
+    type: String,
+    nullable: false,
+    array: true,
+  })
+  dodComponents: Array<string>;
+
+  @Column({
+    type: String,
+    nullable: false,
+    array: true,
+  })
+  portfolioManagers: Array<string>;
 
   @CreateDateColumn()
   createdDate: Date;
