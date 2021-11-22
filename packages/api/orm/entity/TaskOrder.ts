c349778564ed0036d8645737b555c00f5cbdba86
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-11-22 14:43:57 -0500

    Improve task order entity
    Ticket: AT-6836

--- packages/api/orm/entity/TaskOrder.ts
+++ packages/api/orm/entity/TaskOrder.ts
@@ -12,16 +12,16 @@ export class TaskOrder extends BaseEntity {
   })
   taskOrderNumber: string;
 
-  @Column({ type: "uuid", nullable: true, comment: "S3 object key of task order pdf" })
+  @Column({ type: "uuid", nullable: false, comment: "S3 object key of task order pdf" })
   fileId: string;
 
-  @Column({ type: String, nullable: true, length: 256, comment: "name of pdf file when uploaded" })
+  @Column({ type: String, nullable: false, length: 256, comment: "name of pdf file when uploaded" })
   fileName: string;
 
   @Column({ nullable: true, comment: "pdf file size in bytes" })
   fileSize: number;
 
-  @Column({ type: "enum", nullable: true, enum: FileScanStatus })
+  @Column({ type: "enum", nullable: false, enum: FileScanStatus, default: FileScanStatus.PENDING })
   fileScanStatus: FileScanStatus;
 
   // TODO clins: Array<Clin>;
