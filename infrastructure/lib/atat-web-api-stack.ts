b7fe35d9dfd93e5c004cf3f6e3a154d2bd6836a2
Jeff Segal (Hunter Strategy) <75797900+jeffsegal-ccpo@users.noreply.github.com>
2021-11-23 14:28:46 -0500

    Update infrastructure/lib/atat-web-api-stack.ts
    Good call
    
    Co-authored-by: Kyle Laker (Hunter Strategy) <kyle.laker-ctr@ccpo.mil>

--- infrastructure/lib/atat-web-api-stack.ts
+++ infrastructure/lib/atat-web-api-stack.ts
@@ -50,6 +50,9 @@ export interface AtatWebApiStackProps extends cdk.StackProps {
 
 export class AtatWebApiStack extends cdk.Stack {
   private readonly environmentId: string;
+  /**
+   * @deprecated Use {@link internaApi} instead
+   */
   public readonly draftApi: apigw.IRestApi;
   public readonly internalApi: apigw.IRestApi;
   public readonly submitQueue: sqs.IQueue;
