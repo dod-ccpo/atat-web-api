01032c8ce773ad4b94d0689ad9828591b17872f9
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2021-11-24 16:07:31 -0500

    Lambda construct function improvements...
    - Added comments explaining all if statements
    - Added comments explaining readonly class variables
    - Re-ordered optional methods inside the constructor
    - Moved all switch statements into their own functions
    - Removed abstract class declaration

--- infrastructure/lib/constructs/lambda-fn.ts
+++ infrastructure/lib/constructs/lambda-fn.ts
@@ -12,13 +12,14 @@ import { SecureBucket } from "./compliant-resources";
 import * as sqs from "@aws-cdk/aws-sqs";
 import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
 import * as sfn from "@aws-cdk/aws-stepfunctions";
+
 /**
  * An IAM service principal for the API Gateway service, used to grant Lambda
  * invocation permissions.
  */
 const APIGW_SERVICE_PRINCIPAL = new iam.ServicePrincipal("apigateway.amazonaws.com");
 
-export interface ApiFunctionProps {
+export interface ApiFunctionPropstest {
   /**
    * The HTTP method this route applies to.
    */
@@ -97,6 +98,7 @@ export interface ApiFunctionProps {
    * from the SQS event source
    */
   readonly batchSize?: number;
+
   /**
    * The State Machine that will start execution when this function
    * is called.
@@ -104,112 +106,114 @@ export interface ApiFunctionProps {
   readonly stateMachine?: sfn.IStateMachine;
 }
 
-export abstract class ApiFunction extends cdk.Construct {
+export class ApiFunctiontest extends cdk.Construct {
   /**
-   * The underlying Lambda function for the API call.
+   * Required resources
+   * @param fn - the NodejsFunction, named with the @id passed to the function
    */
   public readonly fn: lambda.Function;
+  /**
+   * Optional resources to add to function:
+   * @param method - HTTP Method, indicates that the function uses API Gateway
+   * @param table - DynamoDB table connection
+   * @param bucket - S3 bucket connection
+   * @param queue - SQS queue connection
+   * @param stateMachine - State Machine connection
+   */
+  public readonly method: HttpMethod;
+  public readonly table: dynamodb.ITable;
+  public readonly bucket: s3.IBucket;
+  public readonly queue: sqs.IQueue;
+  public readonly stateMachine?: sfn.IStateMachine;
 
-  /**
-   * The HTTP method that a route should be created for.
-   */
-  // public readonly method?: HttpMethod;
-
-  /**
-   * The API gateway Method resource that gets created.
-   */
-  public readonly route: apigw.Method;
-  /**
-   * The DynamoDB table.
-   */
-  public readonly table?: dynamodb.ITable;
-  public readonly bucket?: s3.IBucket;
-  readonly queue?: sqs.IQueue;
-  /**
-   * A function with the permissions to start execution of a State Machine
-   */
-  readonly stateMachine?: sfn.IStateMachine;
-
-  protected constructor(scope: cdk.Construct, id: string, props: ApiFunctionProps) {
+  constructor(scope: cdk.Construct, id: string, props: ApiFunctionPropstest) {
     super(scope, id);
-    // this.method = props.method;
-
     // Create the lambda fn
     this.fn = new lambdaNodeJs.NodejsFunction(this, "PackagedFunction", {
       entry: props.handlerPath,
       vpc: props.lambdaVpc,
       ...props.functionPropsOverride,
     });
-    // Optional - create table
-    if (props.table) {
-      this.table = props.table;
-      this.fn.addEnvironment("ATAT_TABLE_NAME", props.table.tableName);
-      // assign permissions
-      switch (props.tablePermissions) {
-        case "ALL":
-          this.table.grantFullAccess(this.fn);
-          break;
-        case "READ":
-          this.table.grantReadData(this.fn);
-          break;
-        case "READ_WRITE":
-          this.table.grantReadWriteData(this.fn);
-          break;
-        case "WRITE":
-          this.table.grantWriteData(this.fn);
-          break;
-        default:
-          this.table.grantReadWriteData(this.fn);
-      }
+    // Optional - create API Gateway connection and grant permissions
+    if (props.method) {
+      this.fn.addPermission("AllowApiGatewayInvoke", { principal: APIGW_SERVICE_PRINCIPAL });
+      // editing name from API spec
+      (this.fn.node.defaultChild as lambda.CfnFunction).overrideLogicalId(id + "Function");
     }
-    // if it is using API Gateway it needs a principal
-    this.fn.addPermission("AllowApiGatewayInvoke", { principal: APIGW_SERVICE_PRINCIPAL });
-    // editing name from API spec
-    (this.fn.node.defaultChild as lambda.CfnFunction).overrideLogicalId(id + "Function");
 
-    // Optional - secrets manager
+    // Optional - create DynamoDB connection and grant permissions
+    if (props.table && props.tablePermissions) {
+      this.table = props.table;
+      this.grantRequiredTablePermissions(props.tablePermissions);
+    }
+
+    // Optional - grant permissions for secrets manager
     if (props.smtpSecrets) {
       props.smtpSecrets.grantRead(this.fn);
     }
-    // Optional - s3 bucket
-    if (props.bucket) {
+
+    // Optional - create S3 bucket connection and grant permissions
+    if (props.bucket && props.bucketPermissions) {
       this.bucket = props.bucket.bucket;
       this.fn.addEnvironment("DATA_BUCKET", this.bucket.bucketName);
-      switch (props.bucketPermissions) {
-        case "READ":
-          this.bucket.grantRead(this.fn);
-          break;
-        case "PUT":
-          this.bucket.grantPut(this.fn);
-          break;
-        case "DELETE":
-          this.bucket.grantDelete(this.fn);
-          break;
-        default:
-          this.bucket.grantRead(this.fn);
-      }
+      this.grantRequiredBucketPermissions(props.bucketPermissions);
     }
-    // Optional - SQS
-    if (props.queue) {
+
+    // Optional - create SQS connection and grant permissions
+    if (props.queue && props.queuePermissions) {
       this.queue = props.queue;
       this.fn.addEnvironment("ATAT_QUEUE_URL", props.queue.queueUrl);
-      switch (props.queuePermissions) {
-        case "SEND":
-          this.queue.grantSendMessages(this.fn);
-          break;
-        case "CONSUME":
-          this.queue.grantConsumeMessages(this.fn);
-      }
+      this.grantRequiredSQSPermissions(props.queuePermissions);
       // allows the fn to subscribe to the queue using an EventSource
       if (props.createEventSource) {
         this.fn.addEventSource(new SqsEventSource(this.queue, props.batchSize ? { batchSize: props.batchSize } : {}));
       }
     }
-    // Optional - State Machine
+
+    // Optional - create State Machine and grant permissions
     if (props.stateMachine) {
       this.stateMachine = props.stateMachine;
       this.fn.addEnvironment("SFN_ARN", props.stateMachine.stateMachineArn);
       this.stateMachine.grantStartExecution(this.fn);
+    }
+  }
+
+  private grantRequiredTablePermissions(tablePermissions: string | undefined): iam.Grant | undefined {
+    switch (tablePermissions) {
+      case "ALL":
+        return this.table.grantFullAccess(this.fn);
+      case "READ":
+        return this.table.grantReadData(this.fn);
+      case "READ_WRITE":
+        return this.table.grantReadWriteData(this.fn);
+      case "WRITE":
+        return this.table.grantWriteData(this.fn);
+      default:
+        return this.table.grantReadWriteData(this.fn);
+    }
+  }
+
+  private grantRequiredBucketPermissions(bucketPermissions: string | undefined): iam.Grant | undefined {
+    switch (bucketPermissions) {
+      case "READ":
+        return this.bucket.grantRead(this.fn);
+      case "PUT":
+        return this.bucket.grantPut(this.fn);
+      case "DELETE":
+        return this.bucket.grantDelete(this.fn);
+      default:
+        return this.bucket.grantRead(this.fn);
+    }
+  }
+
+  private grantRequiredSQSPermissions(queuePermissions: string | undefined): iam.Grant | undefined {
+    switch (queuePermissions) {
+      case "SEND":
+        return this.queue.grantSendMessages(this.fn);
+      case "CONSUME":
+        return this.queue.grantConsumeMessages(this.fn);
+      default:
+        return this.queue.grantSendMessages(this.fn);
     }
   }
 }
