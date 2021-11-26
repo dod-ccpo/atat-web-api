b2311787ac91e4329c80e9cb7b746a542f8fb2e2
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-11-26 15:25:08 -0500

    Refactor DB permission grants

--- packages/rds/initial-bootstrap.ts
+++ packages/rds/initial-bootstrap.ts
@@ -9,36 +9,48 @@ import * as fs from "fs";
 type User = {
   name: string;
   dbPrivileges: string[];
+  tablePrivileges: string[];
+};
+
+type Config = {
+  databaseName: string;
+  secretName: string;
+  databaseHost: string;
+  caBundleFile: string;
 };
 
 const USERS: User[] = [
   {
     name: "atat_api_read",
-    dbPrivileges: ["CONNECT", "SELECT"],
+    dbPrivileges: ["CONNECT"],
+    tablePrivileges: ["SELECT"],
   },
   {
     name: "atat_api_write",
-    dbPrivileges: ["CONNECT", "SELECT", "INSERT", "UPDATE", "DELETE"],
+    dbPrivileges: ["CONNECT"],
+    tablePrivileges: ["SELECT", "INSERT", "UPDATE", "DELETE"],
   },
   {
     name: "atat_api_admin",
     dbPrivileges: ["ALL PRIVILEGES"],
+    tablePrivileges: ["ALL PRIVILEGES"],
   },
 ];
 
-async function connectWithoutDatabase(secretName: string, host: string, caBundle: string): Promise<Connection> {
-  const dbAuth = await getDatabaseCredentials(secretName);
+async function connect(connectConfig: Config, useDatabase = true): Promise<Connection> {
+  const dbAuth = await getDatabaseCredentials(connectConfig.secretName);
   return await createConnection({
     type: "postgres",
-    host: host,
+    host: connectConfig.databaseHost,
     port: 5432,
     username: dbAuth.username,
     password: dbAuth.password,
     logging: "all",
     ssl: {
       minVersion: "TLSv1.2",
-      ca: fs.readFileSync(path.join(__dirname, caBundle)),
+      ca: fs.readFileSync(path.join(__dirname, connectConfig.caBundleFile)),
     },
+    database: useDatabase ? connectConfig.databaseName : "postgres",
   });
 }
 
@@ -47,26 +59,41 @@ async function dbExists(connection: Connection, dbName: string): Promise<boolean
 }
 
 async function userExists(connection: Connection, userName: string): Promise<boolean> {
-  return !!(await connection.query(`SELECT * FROM pg_roles WHERE rolname='${userName}';`)).length;
+  const result = await connection.query(`SELECT * FROM pg_roles WHERE rolname='${userName}';`);
+  return !!result.length;
 }
 
-async function handleCreate(connection: Connection, databaseName: string): Promise<void> {
-  if (!dbExists(connection, databaseName)) {
-    await connection.query(`CREATE DATABASE ${databaseName} WITH ENCODING 'UTF8'`);
+async function handleCreate(connectConfig: Config): Promise<void> {
+  // Create the underlying database if it doesn't exist
+  let connection = await connect(connectConfig, false);
+  if (!(await dbExists(connection, connectConfig.databaseName))) {
+    await connection.query(`CREATE DATABASE ${connectConfig.databaseName} WITH ENCODING 'UTF8'`);
   }
+  await connection.close();
+
+  // Establish a new connection to the newly-created database, which is required for
+  // ALTER DEFAULT PRIVILEGES. `\connect` is not supported.
+  connection = await connect(connectConfig);
   for (const user of USERS) {
-    if (!userExists(connection, user.name)) {
+    if (!(await userExists(connection, user.name))) {
       await connection.query(`CREATE ROLE ${user.name} WITH LOGIN IN ROLE rds_iam`);
     }
-    await connection.query(`GRANT ${user.dbPrivileges.join(", ")} ON DATABASE ${databaseName} TO ${user.name}`);
+    await connection.query(
+      `GRANT ${user.dbPrivileges.join(", ")} ON DATABASE ${connectConfig.databaseName} TO ${user.name}`
+    );
+    await connection.query(
+      `ALTER DEFAULT PRIVILEGES GRANT ${user.tablePrivileges.join(", ")} ON TABLES TO ${user.name}`
+    );
   }
 }
 
-async function handleDelete(connection: Connection, databaseName: string): Promise<void> {
+async function handleDelete(connectConfig: Config): Promise<void> {
+  const connection = await connect(connectConfig);
+
   for (const user of USERS) {
     await connection.query(`DROP ROLE IF EXISTS ${user.name}`);
   }
-  await connection.query(`DROP DATABASE IF EXISTS ${databaseName} WITH FORCE`);
+  await connection.query(`DROP DATABASE IF EXISTS ${connectConfig.databaseName} WITH FORCE`);
 }
 
 async function sendResponse(
@@ -87,7 +114,6 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
   const databaseName = event.ResourceProperties.DatabaseName;
   const secretName = event.ResourceProperties.DatabaseSecretName;
   const databaseHost = event.ResourceProperties.DatabaseHost;
-  const certBundleName = "rds-ca-2017.pem";
 
   const resultBase = {
     RequestId: event.RequestId,
@@ -107,16 +133,21 @@ export async function handler(event: CloudFormationCustomResourceEvent): Promise
     return;
   }
 
-  try {
-    const connection = await connectWithoutDatabase(secretName, databaseHost, certBundleName);
+  const connectConfig: Config = {
+    databaseName,
+    databaseHost,
+    secretName,
+    caBundleFile: "rds-ca-2017.pem",
+  };
 
+  try {
     switch (event.RequestType) {
       case "Create":
       case "Update":
-        await handleCreate(connection, databaseName);
+        await handleCreate(connectConfig);
         break;
       case "Delete":
-        await handleDelete(connection, databaseName);
+        await handleDelete(connectConfig);
         break;
     }
 
