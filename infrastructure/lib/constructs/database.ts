99cebb02132bd0c8d7138032cd2c0a4b9d980126
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-11-26 15:25:25 -0500

    Make it easier to grant DB access in CDK

--- infrastructure/lib/constructs/database.ts
+++ infrastructure/lib/constructs/database.ts
@@ -2,12 +2,13 @@ import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as lambdaNodeJs from "@aws-cdk/aws-lambda-nodejs";
-import * as customResources from "@aws-cdk/custom-resources";
 import * as rds from "@aws-cdk/aws-rds";
+import * as iam from "@aws-cdk/aws-iam";
 import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
 
 import { packageRoot } from "../util";
 import { CfnDBInstance } from "@aws-cdk/aws-rds";
+import { ConcreteDependable } from "@aws-cdk/core";
 
 export interface DatabaseProps {
   vpc: ec2.IVpc;
@@ -67,8 +68,25 @@ class DatabaseBootstrapper extends cdk.Construct {
  * automatic bootstrapping of an underlying database.
  */
 export class Database extends cdk.Construct {
-  public readonly cluster: rds.IDatabaseCluster;
+  /**
+   * The underlying Aurora cluster.
+   */
+  public readonly cluster: rds.DatabaseCluster;
+  /**
+   * The Secret resource for the Aurora cluster's master user.
+   */
   public readonly adminSecret: secretsmanager.ISecret;
+  /**
+   * A way to identify whether the database has actually been created on the Aurora host.
+   *
+   * Any resource that needs to connect to the database for any operation should depend
+   * directly on this. It is not guaranteed that the database or users will be created and
+   * properly configured prior to this resource being complete.
+   */
+  // Using a ConcreteDependable means that we can change the underlying definition of what it
+  // means for this to be "ready" without having to update the API. We just need to add/change
+  // resources in the ConcreteDependable.
+  public readonly databaseReady: cdk.ConcreteDependable;
 
   constructor(scope: cdk.Construct, id: string, props: DatabaseProps) {
     super(scope, id);
@@ -107,6 +125,7 @@ export class Database extends cdk.Construct {
       cloudwatchLogsExports: ["postgresql"],
     });
     this.cluster = cluster;
+
     // cluster.addRotationSingleUser({
     //   // Default value is 30 days, so this makes the secret much shorter-lived.
     //   // The primary means for access will be via IAM; however, we will use this
@@ -117,6 +136,7 @@ export class Database extends cdk.Construct {
     // is in fact a secret for the cluster.
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     this.adminSecret = cluster.secret!;
+
     const bootstrapper = new DatabaseBootstrapper(this, "DatabaseBootstrapper", {
       ...props,
       secretName: this.adminSecret.secretName,
@@ -124,8 +144,72 @@ export class Database extends cdk.Construct {
     });
     this.adminSecret.grantRead(bootstrapper.backingLambda);
     cluster.connections.allowDefaultPortFrom(bootstrapper.backingLambda);
-    cluster.node.children
-      .filter((child) => child instanceof CfnDBInstance)
-      .forEach((dbInstance) => bootstrapper.bootstrapResource.node.addDependency(dbInstance));
+
+    // Ensure the DB is not created until all instances in the cluster are ready
+    const instances = cluster.node.children.filter((child) => child instanceof CfnDBInstance);
+    const instancesReady = new ConcreteDependable();
+    instances.forEach((dbInstance) => instancesReady.add(dbInstance));
+    bootstrapper.bootstrapResource.node.addDependency(instancesReady);
+
+    // Provide a resource to determine whether the database has been created.
+    this.databaseReady = new ConcreteDependable();
+    this.databaseReady.add(bootstrapper.bootstrapResource);
+  }
+
+  private clusterUserArn(user: string): string {
+    return cdk.Stack.of(this).formatArn({
+      arnFormat: cdk.ArnFormat.COLON_RESOURCE_NAME,
+      service: "rds-db",
+      resource: "dbuser",
+      resourceName: `${this.cluster.clusterIdentifier}/${user}`,
+    });
+  }
+
+  /**
+   * Grant network and IAM permissions necessary to read from the database.
+   *
+   * @param resource The resource to grant access to
+   */
+  public grantRead(resource: ec2.IConnectable & iam.IGrantable): void {
+    this.cluster.connections.allowDefaultPortFrom(resource);
+    resource.grantPrincipal.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["rds-db:connect"],
+        resources: [this.clusterUserArn("atat_api_read")],
+      })
+    );
+  }
+
+  /**
+   * Grant network and IAM permissions necessary to write to the database.
+   *
+   * @param resource The resource to grant access to
+   */
+  public grantWrite(resource: ec2.IConnectable & iam.IGrantable): void {
+    this.cluster.connections.allowDefaultPortFrom(resource);
+    resource.grantPrincipal.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["rds-db:connect"],
+        resources: [this.clusterUserArn("atat_api_write")],
+      })
+    );
+  }
+
+  /**
+   * Grant network and IAM permissions necessary to administer the database.
+   *
+   * @param resource The resource to grant access to
+   */
+  public grantAdmin(resource: ec2.IConnectable & iam.IGrantable): void {
+    this.cluster.connections.allowDefaultPortFrom(resource);
+    resource.grantPrincipal.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["rds-db:connect"],
+        resources: [this.clusterUserArn("atat_api_admin")],
+      })
+    );
   }
 }
