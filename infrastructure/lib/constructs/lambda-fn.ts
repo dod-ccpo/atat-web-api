5c9d6d5ecad36f42e54f9ddbeb7d96b3f3b00889
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-11-29 10:02:16 -0500

    Fix bundling with pg-native

--- infrastructure/lib/constructs/lambda-fn.ts
+++ infrastructure/lib/constructs/lambda-fn.ts
@@ -132,6 +132,24 @@ export class ApiFlexFunction extends cdk.Construct {
       entry: props.handlerPath,
       vpc: props.lambdaVpc,
       ...props.functionPropsOverride,
+      bundling: {
+        // forceDockerBundling: true,
+        externalModules: ["pg-native"],
+        commandHooks: {
+          beforeBundling() {
+            return [];
+          },
+          afterBundling(inputDir: string, outputDir: string): string[] {
+            return [
+              `curl -sL -o /tmp/rds-ca-2017.pem https://truststore.pki.us-gov-west-1.rds.amazonaws.com/global/global-bundle.pem`,
+              `cp /tmp/rds-ca-2017.pem ${outputDir}/rds-ca-2017.pem`,
+            ];
+          },
+          beforeInstall() {
+            return [];
+          },
+        },
+      },
     });
     // Optional - create API Gateway connection and grant permissions
     if (props.method) {
