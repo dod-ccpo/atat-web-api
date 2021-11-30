0350cd874cc3900ef46f69ed0f1dee0b8014b40e
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-11-30 17:46:45 -0500

    Add initial (empty) typeorm migration file
    Created with
    npx typeorm migration:create -n initial

---
+++ packages/api/orm/migration/1638312144983-initial.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class initial1638312144983 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
