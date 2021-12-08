a5f67e307a304714d27d9a8caf66139c6bc9d27a
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2021-12-08 14:20:44 -0500

    Move repository location to be consistent with existing repositories
    Due to a folder restructuring, moved repository to packages/api/repository
    to be consistent with other implementations.
    
    Ticet: AT-6885

--- packages/orm/entity/Environment.ts
+++ packages/orm/entity/Environment.ts
@@ -2,6 +2,18 @@ import { Application } from "./Application";
 import { Column, Entity, ManyToOne } from "typeorm";
 import { ProvisionableEntity } from "./ProvisionableEntity";
 
+export interface IEnvironment {
+  name: string;
+}
+
+export interface IEnvironmentCreate extends IEnvironment {
+  application: Application;
+}
+export interface IEnvironmentUpdate extends IEnvironment {
+  administrators: string[];
+  contributors: string[];
+  readOnlyOperators: string[];
+}
 @Entity("environment")
 export class Environment extends ProvisionableEntity {
   @Column({ length: 100 })
