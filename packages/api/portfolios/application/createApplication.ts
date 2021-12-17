6cdfc5bfd0cc43b7b33ff8ed641caab7cd82474f
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2021-12-17 14:56:46 -0500

    Update createApplication...
    - Remove unused imports
    - remove catch from main fn, this behaviour is handled by errorhandlingmiddleware
    - clean up comments

--- packages/api/portfolios/application/createApplication.ts
+++ packages/api/portfolios/application/createApplication.ts
@@ -1,9 +1,6 @@
 import "reflect-metadata";
-import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyResult, Context } from "aws-lambda";
-// import { Application } from "../../models/Application";
-import { DATABASE_ERROR, NO_SUCH_PORTFOLIO_DRAFT_404 } from "../../utils/errors";
-import { ApiSuccessResponse, OtherErrorResponse, SetupError, SuccessStatusCode } from "../../utils/response";
+import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
 import schema = require("../../models/internalSchema.json");
 import middy from "@middy/core";
 import xssSanitizer from "../../utils/xssSanitizer";
@@ -16,11 +13,11 @@ import { validateRequestShape } from "../../utils/shapeValidator";
 import { CORS_CONFIGURATION } from "../../utils/corsConfig";
 import { wrapSchema } from "../../utils/schemaWrapper";
 import { errorHandlingMiddleware } from "../../utils/errorHandlingMiddleware";
-// import { createConnection } from "typeorm";
-
 import { Application, IApplicationCreate } from "../../../orm/entity/Application";
 import { Portfolio } from "../../../orm/entity/Portfolio";
 import { createConnection } from "../../utils/database";
+import { ApplicationRepository } from "../../repository/ApplicationRepository";
+import createError from "http-errors";
 
 /**
  * Create an Application
@@ -35,49 +32,65 @@ export async function baseHandler(
   const portfolioId = event.pathParameters?.portfolioId as string;
   console.log(portfolioId);
   let response;
-  const thebody = event.body as Application;
+  const applicationBody = event.body as Application;
   // set up database connection
   const connection = await createConnection();
   try {
     // Ensure portfolio exists
     const portfolioRepository = connection.getRepository(Portfolio);
     const portfolio = await portfolioRepository.findOneOrFail({ id: portfolioId });
-    console.log(portfolio);
-    console.log("The Portfolio should appear above this message");
-    thebody.portfolio = portfolio;
+
+    // Add the fetched portfolio to the applicationBody
+    applicationBody.portfolio = portfolio;
+
+    // Business rules validation
+    // 3.1 unique applicaiton name
+    // Search for all applications in the portfolio where the name matches the incoming name
+    const existingApplications = await connection
+      .getCustomRepository(ApplicationRepository)
+      .getAllMatchingApplicationNames(portfolioId, applicationBody.name);
+    console.log("existing applications below");
+    console.log(existingApplications);
+
+    // existing applications is either an empty array or contains a matching application.name
+    // if it contains any matches, throw an error
+    if (existingApplications.length > 0) {
+      throw createError(400, "Duplicate application name in portfolio", {
+        errorName: "DuplicateApplicationName",
+        applicationName: applicationBody.name,
+      });
+    }
+
+    // 3.3 unique environment name
+    const uniqueValues = new Set(applicationBody.environments.map((v) => v.name));
+    if (uniqueValues.size < applicationBody.environments.length) {
+      console.log("Duplicates found");
+      throw createError(400, "Duplicate environment name in application", {
+        errorName: "DuplicateEnvironmentName",
+        environmentName: applicationBody.environments,
+      });
+    }
 
     // Create a new application
-    const newApp = await connection.getRepository(Application).save(thebody);
+    const newApp = await connection.getRepository(Application).save(applicationBody);
     console.log("Successfully created new application" + JSON.stringify(newApp));
-
     console.log("ID of just created App:" + JSON.stringify(newApp.id));
 
     // Formatting response (remove the portfolio)
     response = newApp as IApplicationCreate;
     if (response.portfolio) {
       delete response.portfolio;
-      console.log("Deleted the portfolio form the");
+      console.log("Deleted the portfolio from application response");
     }
-    console.log(JSON.stringify(response));
-
-    /*
-    // Don't need to do this query because .save returns the full object with relations
-    response = await connection.getCustomRepository(ApplicationRepository).getApplication(newApp.id);
-
-    console.log("Inserted new application " + JSON.stringify(response)); */
-  } catch (error) {
-    console.log(error);
-    if (error.name === "EntityNotFoundError") {
-      console.log("Invalid parameter entered: " + error);
-      return NO_SUCH_PORTFOLIO_DRAFT_404;
-    }
+    console.log("Response:" + JSON.stringify(response));
   } finally {
     connection.close();
+    // console.log("finished everything");
   }
 
-  return new ApiSuccessResponse<any>(response, SuccessStatusCode.CREATED);
+  return new ApiSuccessResponse<IApplicationCreate>(response, SuccessStatusCode.CREATED);
 }
-// middy without validation
+
 export const handler = middy(baseHandler)
   .use(xssSanitizer())
   .use(jsonBodyParser())
