6a11504906b2f4aec39242e5a9befb9ff7aca50d
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2021-12-17 19:55:57 -0500

    Created additional unit tests for validation

--- packages/api/portfolios/application/createApplication.test.ts
+++ packages/api/portfolios/application/createApplication.test.ts
@@ -2,21 +2,86 @@ import { handler } from "../application/createApplication";
 import { Context } from "aws-lambda";
 import { ApiGatewayEventParsed } from "../../utils/eventHandlingTool";
 import { ApiSuccessResponse, SuccessStatusCode } from "../../utils/response";
+import { ErrorStatusCode } from "../../../email/utils/statusCodesAndErrors";
 
 const validRequest: ApiGatewayEventParsed<any> = {
   body: {
-    name: "newnenwenew",
-    description: "meep",
-    environments: [{ name: "frog frog frog" }],
+    name: "Zach's test app",
+    description: "Description",
+    environments: [{ name: "Environment name" }],
   },
-  pathParameters: { portfolioId: "88fc1582-896e-471f-9189-c1ce1d45e4c7" },
+  pathParameters: { portfolioId: "8e935e1c-cbc9-4db5-81dd-d811652d2b8f" },
 } as any;
-// 49626f72-2220-42b3-84f0-d921b31b79a7
-describe("Successful operation tests", () => {
-  it.skip("should return funding step and http status code 201", async () => {
+// These local tests only work the proper local db environment
+describe("Local createApplication tests", () => {
+  it.skip("should return valid Application", async () => {
     const result = await handler(validRequest, {} as Context, () => null);
     console.log(result);
     expect(result).toBeInstanceOf(ApiSuccessResponse);
     expect(result?.statusCode).toEqual(SuccessStatusCode.CREATED);
+  });
+});
+// Validation tests
+describe("Validation tests", () => {
+  it("should fail due to having no request body", async () => {
+    const validRequest: ApiGatewayEventParsed<any> = {
+      // body: { name: "coolest app" },
+      pathParameters: {
+        portfolioId: "91c6bd1f-5ed8-413b-8f85-55a62dd50ad3",
+      },
+      requestContext: { identity: { sourceIp: "7.7.7.7" } },
+    } as any;
+
+    const result = await handler(validRequest, {} as Context, () => null);
+    console.log(result?.body);
+    expect(result?.statusCode).toBe(ErrorStatusCode.BAD_REQUEST);
+  });
+  it("should fail due to having an incorrect path parameter", async () => {
+    const validRequest: ApiGatewayEventParsed<any> = {
+      body: { name: "coolest app", description: "Description", environments: [{ name: "Environment name" }] },
+      pathParameters: {
+        portfolioId: "wrong",
+      },
+      requestContext: { identity: { sourceIp: "7.7.7.7" } },
+    } as any;
+
+    const result = await handler(validRequest, {} as Context, () => null);
+    console.log(result?.body);
+    expect(result?.statusCode).toBe(ErrorStatusCode.NOT_FOUND);
+  });
+  it("should fail validation due to having additional properties at application level", async () => {
+    const validRequest: ApiGatewayEventParsed<any> = {
+      body: {
+        name: "Zach's test app",
+        createdAt: "2021-12-17T19:37:18.210Z",
+        description: "Description",
+        environments: [{ name: "Environment name" }],
+      },
+      pathParameters: {
+        portfolioId: "91c6bd1f-5ed8-413b-8f85-55a62dd50ad3",
+      },
+      requestContext: { identity: { sourceIp: "7.7.7.7" } },
+    } as any;
+
+    const result = await handler(validRequest, {} as Context, () => null);
+    console.log(result?.body);
+    expect(result?.statusCode).toBe(ErrorStatusCode.BAD_REQUEST);
+  });
+  it("should fail validation due to having additional properties at environment level", async () => {
+    const validRequest: ApiGatewayEventParsed<any> = {
+      body: {
+        name: "Zach's test app",
+        description: "Description",
+        environments: [{ name: "Environment name", createdAt: "2021-12-17T19:37:18.210Z" }],
+      },
+      pathParameters: {
+        portfolioId: "91c6bd1f-5ed8-413b-8f85-55a62dd50ad3",
+      },
+      requestContext: { identity: { sourceIp: "7.7.7.7" } },
+    } as any;
+
+    const result = await handler(validRequest, {} as Context, () => null);
+    console.log(result?.body);
+    expect(result?.statusCode).toBe(ErrorStatusCode.BAD_REQUEST);
   });
 });
