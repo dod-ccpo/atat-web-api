470bcc5fb451ce66d921348e2691406e1334ee96
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2021-12-20 09:45:44 -0500

    Improve unit test for requests with unexpected properties
    Ticket: AT-6878

--- packages/api/portfolios/createPortfolio.test.ts
+++ packages/api/portfolios/createPortfolio.test.ts
@@ -1,9 +1,10 @@
-import { handler } from "./createPortfolio";
 import { ApiGatewayEventParsed } from "../utils/eventHandlingTool";
 import { CloudServiceProvider, DodComponent, IPortfolioCreate } from "../../orm/entity/Portfolio";
-import { ErrorStatusCode, SuccessStatusCode } from "../utils/response";
 import { Context } from "aws-lambda";
+import { ErrorCode } from "../models/Error";
+import { handler } from "./createPortfolio";
 import { ProvisioningStatus } from "../../orm/entity/ProvisionableEntity";
+import { SuccessStatusCode, ValidationErrorResponse } from "../utils/response";
 
 describe("createPortfolio", () => {
   it.skip("successful operation testing locally only", async () => {
@@ -22,17 +23,33 @@ describe("createPortfolio", () => {
     const result = await handler(validRequest, {} as Context, () => null);
     expect(result?.statusCode).toBe(SuccessStatusCode.CREATED);
   });
-  it("requests with audit properties should cause 400 BAD REQUEST", async () => {
-    const validRequest: ApiGatewayEventParsed<IPortfolioCreate> = {
-      body: {
-        id: "",
-        createdAt: "",
-        updatedAt: "",
-        archivedAt: "",
-      },
-      requestContext: { identity: { sourceIp: "7.7.7.7" } },
-    } as any;
-    const result = await handler(validRequest, {} as Context, () => null);
-    expect(result?.statusCode).toBe(ErrorStatusCode.BAD_REQUEST);
+  it("should reject requests that include audit properties, disallowing overwrite", async () => {
+    function makeRequestWithPropertyNamed(propertyName: string): ApiGatewayEventParsed<IPortfolioCreate> {
+      return {
+        body: {
+          [propertyName]: "",
+          // otherwise valid request body below
+          portfolioManagers: ["joe.manager@example.mil", "jane.manager@example.mil"],
+          csp: CloudServiceProvider.CSP_A,
+        },
+        requestContext: { identity: { sourceIp: "7.7.7.7" } },
+      } as any;
+    }
+    const badRequests: Array<ApiGatewayEventParsed<IPortfolioCreate>> = [
+      makeRequestWithPropertyNamed("id"),
+      makeRequestWithPropertyNamed("createdAt"),
+      makeRequestWithPropertyNamed("updatedAt"),
+      makeRequestWithPropertyNamed("archivedAt"),
+      makeRequestWithPropertyNamed("propertyNotExpected"),
+    ];
+    badRequests.forEach(async (badRequest) => {
+      const result = await handler(badRequest, {} as Context, () => null);
+      expect(result).toBeInstanceOf(ValidationErrorResponse);
+      const body = JSON.parse(result?.body ?? "");
+      expect(body.code).toBe(ErrorCode.INVALID_INPUT);
+      expect(body.message).toBe("Request failed validation");
+      expect(body.error_map[0].keyword).toBe("additionalProperties");
+      expect(body.error_map[0].message).toBe("must NOT have additional properties");
+    });
   });
 });
