5f0b41d8102df7428d8c4d9e9c3c0b4406af9945
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-02-15 13:18:07 -0500

    Fix synthesis

--- lib/constructs/apigateway.ts
+++ lib/constructs/apigateway.ts
@@ -1,4 +1,4 @@
-import * as cdk from "aws-cdk-lib/core";
+import * as cdk from "aws-cdk-lib";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as logs from "aws-cdk-lib/aws-logs";
