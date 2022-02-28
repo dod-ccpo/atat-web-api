058e35c29e0d6a73411f9f0b7137e3e1fd771948
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2022-02-28 16:51:55 -0500

    Create assertion tests for all generated IAM policies
    Wrote fine-grained assertions for the following IAM Managed policies:
    -generalReadAccess
    -auditorAccess
    -developerRwAccess
    -baseDenies
    
    Ticket: AT-7074

--- test/atat-iam.test.ts
+++ test/atat-iam.test.ts
@@ -25,15 +25,17 @@ describe("ATAT network creation", () => {
 });
 
 describe("ATAT IAM Policy creation", () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
+  let template: Template;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new AtatIam.AtatIamStack(app, "TestIamStack", {});
+    template = Template.fromStack(stack);
+  });
+
   test("Fully assert generalReadAccess IAM managed policy with matchers", async () => {
-    // GIVEN
-    const app = new cdk.App();
-
-    // WHEN
-    const stack = new AtatIam.AtatIamStack(app, "TestIamStack", {});
-    const template = Template.fromStack(stack);
-
-    // THEN
     template.hasResourceProperties(
       "AWS::IAM::ManagedPolicy",
       Match.objectEquals({
@@ -108,6 +110,123 @@ describe("ATAT IAM Policy creation", () => {
           Version: "2012-10-17",
         },
         Description: "Grants read access to specific resources not in ViewOnlyAccess",
+        Path: "/",
+      })
+    );
+  });
+  test("Fully assert auditorAccess IAM managed policy with matchers", async () => {
+    template.hasResourceProperties(
+      "AWS::IAM::ManagedPolicy",
+      Match.objectEquals({
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: "artifact:Get",
+              Effect: "Allow",
+              Resource: {
+                "Fn::Join": [
+                  "",
+                  [
+                    "arn:",
+                    {
+                      Ref: "AWS::Partition",
+                    },
+                    ":artifact:::report-package/*",
+                  ],
+                ],
+              },
+              Sid: "AllowArtifactReportPackageAccess",
+            },
+            {
+              Action: "artifact:DownloadAgreement",
+              Effect: "Allow",
+              Resource: "*",
+              Sid: "AllowArtifactAgreementDownload",
+            },
+          ],
+          Version: "2012-10-17",
+        },
+        Description: "Grants additional security auditor access beyond SecurityAudit",
+        Path: "/",
+      })
+    );
+  });
+  test("Fully assert developerRwAccess IAM managed policy with matchers", async () => {
+    template.hasResourceProperties(
+      "AWS::IAM::ManagedPolicy",
+      Match.objectEquals({
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: "s3:*",
+              Effect: "Allow",
+              Resource: [
+                {
+                  "Fn::Join": [
+                    "",
+                    [
+                      "arn:",
+                      {
+                        Ref: "AWS::Partition",
+                      },
+                      ":s3:::cdk-*-assets-",
+                      {
+                        Ref: "AWS::AccountId",
+                      },
+                      "-*",
+                    ],
+                  ],
+                },
+                {
+                  "Fn::Join": [
+                    "",
+                    [
+                      "arn:",
+                      {
+                        Ref: "AWS::Partition",
+                      },
+                      ":s3:::cdktoolkit-stagingbucket-*",
+                    ],
+                  ],
+                },
+              ],
+              Sid: "AllowModifyingCdkToolBuckets",
+            },
+            {
+              Action: [
+                "dynamodb:RestoreTable*",
+                "dynamodb:ExportTableToPointInTime",
+                "dynamodb:ListBackups",
+                "dynamodb:Describe*Backup*",
+              ],
+              Effect: "Allow",
+              Resource: "*",
+              Sid: "AllowDynamoDbBackupRestore",
+            },
+          ],
+          Version: "2012-10-17",
+        },
+        Description: "Grants read/write access to developer-specific actions and resources",
+        Path: "/",
+      })
+    );
+  });
+  test("Fully assert baseDenies IAM managed policy with matchers", async () => {
+    template.hasResourceProperties(
+      "AWS::IAM::ManagedPolicy",
+      Match.objectEquals({
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: "organizations:*",
+              Effect: "Deny",
+              Resource: "*",
+              Sid: "DenyAllOrganizations",
+            },
+          ],
+          Version: "2012-10-17",
+        },
+        Description: "",
         Path: "/",
       })
     );
