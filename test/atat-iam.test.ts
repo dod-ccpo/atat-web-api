9cb752537cc28bb043d79f5f83275dea85e887ee
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2022-02-28 16:00:57 -0500

    Create ATAT IAM Policy test for fine-grained assertions
    Tests the generalReadAccess IAM managed policy from atat-iam test
    to ensure it is correctly generated with with an AWS Cloudformation template.
    
    This test is useful to detect regressions and for developing new features
    with test driven development.
    
    Ticket: AT-7074

--- test/atat-iam.test.ts
+++ test/atat-iam.test.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 import * as AtatIam from "../lib/atat-iam-stack";
 
 describe("ATAT network creation", () => {
@@ -21,5 +21,95 @@ describe("ATAT network creation", () => {
     for (const output of expectedOutputNames) {
       template.hasOutput("*", { Export: { Name: output } });
     }
+  });
+});
+
+describe("ATAT IAM Policy creation", () => {
+  test("Fully assert generalReadAccess IAM managed policy with matchers", async () => {
+    // GIVEN
+    const app = new cdk.App();
+
+    // WHEN
+    const stack = new AtatIam.AtatIamStack(app, "TestIamStack", {});
+    const template = Template.fromStack(stack);
+
+    // THEN
+    template.hasResourceProperties(
+      "AWS::IAM::ManagedPolicy",
+      Match.objectEquals({
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: ["dynamodb:*GetItem", "dynamodb:PartiQLSelect", "dynamodb:Scan", "dynamodb:Query"],
+              Effect: "Allow",
+              Resource: {
+                "Fn::Join": [
+                  "",
+                  [
+                    "arn:",
+                    {
+                      Ref: "AWS::Partition",
+                    },
+                    ":dynamodb:*:",
+                    {
+                      Ref: "AWS::AccountId",
+                    },
+                    ":table/*",
+                  ],
+                ],
+              },
+              Sid: "DynamoDBItemAccess",
+            },
+            {
+              Action: "apigateway:GET",
+              Effect: "Allow",
+              Resource: {
+                "Fn::Join": [
+                  "",
+                  [
+                    "arn:",
+                    {
+                      Ref: "AWS::Partition",
+                    },
+                    ":apigateway:*::/restapis*",
+                  ],
+                ],
+              },
+              Sid: "APIGatewayRestApiReadAccess",
+            },
+            {
+              Action: ["states:List*", "states:Describe*", "states:GetExecutionHistory"],
+              Effect: "Allow",
+              Resource: {
+                "Fn::Join": [
+                  "",
+                  [
+                    "arn:",
+                    {
+                      Ref: "AWS::Partition",
+                    },
+                    ":states:*:",
+                    {
+                      Ref: "AWS::AccountId",
+                    },
+                    ":stateMachine:*",
+                  ],
+                ],
+              },
+              Sid: "StepFunctionsReadAccess",
+            },
+            {
+              Action: ["xray:BatchGetTraces", "xray:Get*", "xray:List*"],
+              Effect: "Allow",
+              Resource: "*",
+              Sid: "XRayReadAccess",
+            },
+          ],
+          Version: "2012-10-17",
+        },
+        Description: "Grants read access to specific resources not in ViewOnlyAccess",
+        Path: "/",
+      })
+    );
   });
 });
