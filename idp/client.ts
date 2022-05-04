5a04daa0ee5771c2448e40528b503153031a3e34
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-05-04 14:06:16 -0400

    Request scopes and dump token contents
    This still does not log the raw response (which contains the signature).
    This strips the signature, ignores it, and logs the contents.

--- idp/client.ts
+++ idp/client.ts
@@ -1,4 +1,5 @@
 import { Context } from "aws-lambda";
+import { GetSecretValueCommandInput } from "@aws-sdk/client-secrets-manager";
 import axios from "axios";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { secretsClient } from "../utils/aws-sdk/secrets-manager";
@@ -24,35 +25,62 @@ interface TokenResponse {
  * Build a client_credentials flow request body
  *
  * This does not support requesting any scopes.
+ *
+ * @param clientId The OAuth Client ID
+ * @param scopes A list of scopes to request
  */
-function buildTokenRequest(clientId: string): URLSearchParams {
+function buildTokenRequest(clientId: string, scopes?: string[]): URLSearchParams {
   const form = new URLSearchParams();
   form.set("grant_type", "client_credentials");
   form.set("client_id", clientId);
+  if (scopes?.length) {
+    form.set("scope", scopes.join(" "));
+  }
   return form;
 }
 
 /**
  * Get the actual client secret value from AWS Systems Manager
+ *
+ * @param secretId the name/ARN of the Secrets Manager secret
  */
 async function fetchClientSecret(secretId: string): Promise<string> {
-  logger.info("Retriving client secret from Secrets Manager", { SecretId: secretId });
-  return (await secretsClient.getSecretValue({ SecretId: secretId })).SecretString!;
+  const request: GetSecretValueCommandInput = { SecretId: secretId };
+  logger.info("Retriving client secret from Secrets Manager", { request: { ...request } });
+  return (await secretsClient.getSecretValue(request)).SecretString!;
+}
+
+/**
+ * Decode a JWT without performing any verification of the token.
+ *
+ * @param payload The raw reponse from the TOKEN endpoint
+ */
+function unsafelyDecodeJwt(payload: string): { [key: string]: any } {
+  const base64Data = payload.split(".")[1];
+  const decoded = Buffer.from(base64Data, "base64");
+  return JSON.parse(decoded.toString());
 }
 
 /**
  * Perform the request to get the token from the TOKEN endpoint.
  *
  * Throws an error if the response is not a 200
+ *
+ * @param clientId The OAuth client ID
+ * @param clientSecret The secret to use to authenticate to the server
+ * @param baseUrl The base URL of the authentication server
+ * @param timeout The timeout to use when making the token request
+ * @param scopes The scopes to request in the token
  */
 async function getToken(
   clientId: string,
   clientSecret: string,
   baseUrl: string,
-  timeout?: number
+  timeout?: number,
+  scopes?: string[]
 ): Promise<TokenResponse> {
   logger.info("Fetching a token from the TOKEN endpoint", { clientId });
-  const response = await axios.post(`${baseUrl}/oauth2/token`, buildTokenRequest(clientId), {
+  const response = await axios.post(`${baseUrl}/oauth2/token`, buildTokenRequest(clientId, scopes), {
     timeout,
     headers: {
       "content-type": "application/x-www-form-urlencoded",
@@ -88,12 +116,15 @@ export async function handler(_event: Record<string, never>, context: Context): 
     const response = await getToken(
       IDP_CLIENT_ID,
       await fetchClientSecret(IDP_CLIENT_SECRET_NAME),
-      `https://${IDP_DOMAIN}`
+      `https://${IDP_DOMAIN}`,
+      context.getRemainingTimeInMillis() - 500,
+      ["atat/read-cost"]
     );
     const body = { type: response.token_type, expires_in: response.expires_in };
     logger.info("Received token from client credentials", {
       response: { body },
     });
+    logger.info("Decoded the received token", { token: unsafelyDecodeJwt(response.access_token) });
   } catch (error) {
     if (axios.isAxiosError(error)) {
       logger.error("The request to get an OAUTH 2.0 token failed", error as Error);
