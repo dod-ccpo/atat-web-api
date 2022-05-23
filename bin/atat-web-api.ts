2f08cc4ec387563210d1e723daa42b13f152a480
Jeff Segal <jeff.segal-ctr@ccpo.mil>
2022-05-23 16:38:46 -0400

    Removed explicit cdk.synth() invocation
    Ticket: AT-7448

--- bin/atat-web-api.ts
+++ bin/atat-web-api.ts
@@ -3,5 +3,4 @@ import "source-map-support/register";
 import { createApp } from "./app";
 
 // Externalized the logic to set up our App so that we can perform proper unit tests on it
-const app = createApp();
-app.synth();
+createApp();
