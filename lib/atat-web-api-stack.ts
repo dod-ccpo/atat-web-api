2ff92b4076d323fb0874113a0bc03266c3183b3f
Julius Fitzhugh (Hunter Strategy) <julius.fitzhugh-ctr@ccpo.mil>
2022-06-07 01:02:13 -0400

    Add lambda layer for template files
    Add layer that contains the templating files used for generating
    the document in the lambda.
    
    Ticket: AT-7345

--- lib/atat-web-api-stack.ts
+++ lib/atat-web-api-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as statement from "cdk-iam-floyd";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { AtatNetStack } from "./atat-net-stack";
@@ -103,8 +104,20 @@ export class AtatWebApiStack extends cdk.Stack {
 
     // APIGW Document Generation Resource
     const generateDocumentResource = api.restApi.root.addResource("generate-document");
+    const documentGenerationLayer = new lambda.LayerVersion(this, "GenerateDocumentSupportLayer", {
+      compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
+      code: lambda.Code.fromAsset("document-generation/templates", {}),
+    });
+    // const puppeteerLayer = new lambda.LayerVersion(this, "PuppeteerSupportLayer", {
+    //   compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
+    //   code: lambda.Code.fromAsset("node_modules/chrome-aws-lambda/build", {}),
+    // });
     const generateDocumentFn = new nodejs.NodejsFunction(this, "GenerateDocumentFunction", {
       entry: "document-generation/generate-document.ts",
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 512,
+      layers: [documentGenerationLayer /* , puppeteerLayer */],
+      timeout: cdk.Duration.seconds(120),
     });
     generateDocumentResource.addMethod(HttpMethod.POST, new apigw.LambdaIntegration(generateDocumentFn));
 
