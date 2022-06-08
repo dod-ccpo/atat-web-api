6c416984bb323f02a8677c1bbc0f9eab74f0ae06
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-06-08 16:12:48 -0400

    Add a script to remediate various STIG items
    This ensures the password policy is set and that we have blocked public
    access to S3 buckets. This can be run by installing `ts-node` globally
    and running `./manual-steps/stig-remediation.ts`.

---
+++ manual-steps/stig-remediation.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env ts-node
+
+import { IAM } from "@aws-sdk/client-iam";
+import { S3Control } from "@aws-sdk/client-s3-control";
+import { STS } from "@aws-sdk/client-sts";
+
+const iam = new IAM({ useFipsEndpoint: true });
+const s3Control = new S3Control({ useFipsEndpoint: true });
+const sts = new STS({ useFipsEndpoint: true });
+
+async function getAccountId(): Promise<string> {
+  return (await sts.getCallerIdentity({})).Account!;
+}
+
+async function blockS3PublicAccess() {
+  s3Control.putPublicAccessBlock({
+    AccountId: await getAccountId(),
+    PublicAccessBlockConfiguration: {
+      BlockPublicAcls: true,
+      BlockPublicPolicy: true,
+      RestrictPublicBuckets: true,
+    },
+  });
+}
+
+function setIamPasswordPolicy() {
+  iam.updateAccountPasswordPolicy({
+    AllowUsersToChangePassword: true,
+    HardExpiry: true,
+    // V-222545
+    MaxPasswordAge: 60,
+    // V-222536
+    MinimumPasswordLength: 15,
+    PasswordReusePrevention: 24,
+    RequireLowercaseCharacters: true,
+    RequireNumbers: true,
+    RequireSymbols: true,
+    RequireUppercaseCharacters: true,
+  });
+}
+
+blockS3PublicAccess();
+setIamPasswordPolicy();
