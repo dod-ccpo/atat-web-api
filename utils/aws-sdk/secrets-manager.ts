2ac729abc321ca2dbb365dca7676c69db3e2987f
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-06-17 09:34:17 -0400

    Add logger to AWS SDK clients

--- utils/aws-sdk/secrets-manager.ts
+++ utils/aws-sdk/secrets-manager.ts
@@ -1,3 +1,4 @@
 import { SecretsManager } from "@aws-sdk/client-secrets-manager";
+import { logger } from "../logging";
 
-export const secretsClient = new SecretsManager({ useFipsEndpoint: true });
+export const secretsClient = new SecretsManager({ useFipsEndpoint: true, logger });
--- utils/aws-sdk/step-functions.ts
+++ utils/aws-sdk/step-functions.ts
@@ -1,3 +1,4 @@
 import { SFN } from "@aws-sdk/client-sfn";
+import { logger } from "../logging";
 
-export const sfnClient = new SFN({ useFipsEndpoint: true });
+export const sfnClient = new SFN({ useFipsEndpoint: true, logger });
