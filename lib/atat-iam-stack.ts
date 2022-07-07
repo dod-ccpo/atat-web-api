f77478c7ab73814a472512af05b488e908b01e0d
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-07-07 18:26:08 -0400

    Allow developers to purge SQS queue
    This will prevent an out-of-control Lambda function from being
    re-invoked over and over and over again without developers individually
    having a means to resolve that problem.

--- lib/atat-iam-stack.ts
+++ lib/atat-iam-stack.ts
@@ -117,7 +117,7 @@ export class AtatIamStack extends cdk.Stack {
         // troubleshoot
         new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
-          actions: ["sqs:Get*", "sqs:List*", "sqs:ReceiveMessage"],
+          actions: ["sqs:Get*", "sqs:List*", "sqs:ReceiveMessage", "sqs:PurgeQueue"],
           resources: ["*"],
         }),
         // Grant access to read the pipeline state
