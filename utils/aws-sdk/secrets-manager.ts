972f6f7a28b062f7919d8ca6ff304fc006a56a5f
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-07-07 15:29:06 -0400

    Fix networking issues with AWS Secrets Manager
    The bulk of the issue is described in the code comment. Putting this
    explanation inline makes it more immediately clear to future
    maintainers, reviewers, or auditors why we've omitted the flag in this
    particular case.
    
    All other services we invoke from within our VPC use the same hostname
    for FIPS and non-FIPS so keeping `useFipsEndpoint` is fine for those
    (SQS and Step Functions).

--- utils/aws-sdk/secrets-manager.ts
+++ utils/aws-sdk/secrets-manager.ts
@@ -1,4 +1,9 @@
 import { SecretsManager } from "@aws-sdk/client-secrets-manager";
 import { logger } from "../logging";
 
-export const secretsClient = new SecretsManager({ useFipsEndpoint: true, logger });
+// For this client, we do not use the `useFipsEndpoint` parameter. In the end, this will use
+// our private endpoint, which is FIPS-compliant; however, the DNS query that results will be
+// for `secretsmanager-fips.<region>.amazonaws.com. The private endpoint will only "register"
+// as `secretsmanager.<region>.amazonaws.com. This results in failed queries to the endpoint
+// with our networking configuration.
+export const secretsClient = new SecretsManager({ logger });
