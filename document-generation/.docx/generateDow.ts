d4df2277cc5f42d880536369d4c3011a0db663c2
zachclark-ccpo <zach.clark-ctr@ccpo.mil>
2022-11-30 13:33:53 -0500

    Update generateDow to properly utilize getDocxTemplatePath...
    This solves the issue of searching for _dirname in the Lambda fn
    Ticket:At-8281

--- document-generation/.docx/generateDow.ts
+++ document-generation/.docx/generateDow.ts
@@ -4,30 +4,32 @@ import * as fs from "fs";
 import * as path from "path";
 import PizZip from "pizzip";
 import Docxtemplater from "docxtemplater";
+import { getDocxTemplatePath } from "../utils/utils";
 
 export async function generateDow(event: RequestEvent<GenerateDocumentRequest>): Promise<ApiBase64SuccessResponse> {
   // Extract payload
-  const payload = event.body.templatePayload;
+  const { documentType, templatePayload } = event.body;
 
   // Load the docx file as binary content
-  const content = fs.readFileSync(path.resolve(__dirname, "dow.docx"), "binary");
-  const zip = new PizZip(content);
+  const template = getDocxTemplatePath(documentType);
+
+  const zip = new PizZip(template);
   const document = new Docxtemplater(zip, {
     paragraphLoop: true,
     linebreaks: true,
   });
 
-  document.render(payload);
+  document.render(templatePayload);
 
   // Create Node JS Buffer to send as response
-  const report = document.getZip().generate({
+  const report = Buffer.from(document.getZip().generate({
     type: "nodebuffer",
     compression: "DEFLATE",
-  });
+  }));
 
   const headers = {
     "Content-Type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     "Content-Disposition": `attachment; filename=DescriptionOfWork.docx`,
   };
-  return new ApiBase64SuccessResponse(report, SuccessStatusCode.OK, headers);
+  return new ApiBase64SuccessResponse(report.toString("base64"), SuccessStatusCode.OK, headers);
 }
