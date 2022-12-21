402f3bc449cddeb780765aa982d76b9bea77ec79
Julius Fitzhugh (Hunter Strategy) <julius.fitzhugh-ctr@ccpo.mil>
2022-12-21 18:28:57 -0500

    Update dow-documnt fn

--- document-generation/dow-document.ts
+++ document-generation/dow-document.ts
@@ -2,26 +2,257 @@ import { logger } from "../utils/logging";
 import createReport from "docx-templates";
 import { DescriptionOfWork } from "../models/document-generation";
 import { ApiBase64SuccessResponse, SuccessStatusCode } from "../utils/response";
+import { getCDRLs, getTaskPeriods } from "./utils/utils";
 
 export async function generateDowDocument(
   template: Buffer,
   payload: DescriptionOfWork
 ): Promise<ApiBase64SuccessResponse> {
+  const popTasks = getTaskPeriods(payload);
+  const entirePeriodTasks = popTasks.entireDurationTasks.map((taskNumber: any) => taskNumber);
+  const selectedPeriodTask = popTasks.taskNumberGroups.flatMap((group: any) => group.dowTaskNumbers);
+  const allPopTasks = entirePeriodTasks.concat(selectedPeriodTask);
+  const cdrls = getCDRLs(allPopTasks, payload.contractType);
+
   const report = Buffer.from(
     await createReport({
       template,
       data: {
         ...payload,
-        // update amounts to have commas
-        // TODO see if there is anything we need to manipulate, if so do that here
-        // estimatedTaskOrderValue: payload.estimatedTaskOrderValue.toLocaleString("en-US"),
-        // initialAmount: payload.initialAmount.toLocaleString("en-US"),
-        // remainingAmount: payload.remainingAmount.toLocaleString("en-US"),
-        
-        // construct Funding Document information to be used in template
-        //Additional data needed to pull in to complete the document
-        //fundingDocInfo: getWordTemplate(payload.),
-        
+        pop: popTasks,
+        cdrls,
+        // TODO: match up with the data in sections 1-4 of DOW
+        // checkXaaSClassifications: () => {},
+        // sumTotalInstances: () => {
+        //   let number = 0;
+        //   for (let i = 0; i < payload.currentEnvironment.envInstances.length; i++) {
+        //     number += payload.currentEnvironment.envInstances[i].currentEnvironmentInstance.numberOfInstances;
+        //   }
+        //   return number;
+        // },
+        // filterCloudSupport: (cloudSupportPackage: any) => {
+        //   const impactLevel2: any = [];
+        //   const impactLevel4: any = [];
+        //   const impactLevel5: any = [];
+        //   const impactLevel6: any = [];
+        //   // iterate over Cloud Support Package
+        //   cloudSupportPackage.instanceConfigurations.forEach((plan: any) => {
+        //     plan.portabilityPlan.forEach((item: any) => {
+        //       switch (item.classificationLevel.impactLevel.impactLevel) {
+        //         case dow.ImpactLevel.IL2:
+        //           impactLevel2.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL4:
+        //           impactLevel4.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL5:
+        //           impactLevel5.push(item);
+        //           break;
+        //         default:
+        //           impactLevel6.push(item);
+        //       }
+        //     });
+        //     plan.advisoryAndAssistance.forEach((item: any) => {
+        //       switch (item.classificationLevel.impactLevel.impactLevel) {
+        //         case dow.ImpactLevel.IL2:
+        //           impactLevel2.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL4:
+        //           impactLevel4.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL5:
+        //           impactLevel5.push(item);
+        //           break;
+        //         default:
+        //           impactLevel6.push(item);
+        //       }
+        //     });
+        //     plan.helpDesk.forEach((item: any) => {
+        //       switch (item.classificationLevel.impactLevel.impactLevel) {
+        //         case dow.ImpactLevel.IL2:
+        //           impactLevel2.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL4:
+        //           impactLevel4.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL5:
+        //           impactLevel5.push(item);
+        //           break;
+        //         default:
+        //           impactLevel6.push(item);
+        //       }
+        //     });
+        //     plan.training.forEach((item: any) => {
+        //       switch (item.classificationLevel.impactLevel.impactLevel) {
+        //         case dow.ImpactLevel.IL2:
+        //           impactLevel2.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL4:
+        //           impactLevel4.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL5:
+        //           impactLevel5.push(item);
+        //           break;
+        //         default:
+        //           impactLevel6.push(item);
+        //       }
+        //     });
+        //     plan.docSupport.forEach((item: any) => {
+        //       switch (item.classificationLevel.impactLevel.impactLevel) {
+        //         case dow.ImpactLevel.IL2:
+        //           impactLevel2.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL4:
+        //           impactLevel4.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL5:
+        //           impactLevel5.push(item);
+        //           break;
+        //         default:
+        //           impactLevel6.push(item);
+        //       }
+        //     });
+        //     plan.generalXaaS.forEach((item: any) => {
+        //       switch (item.classificationLevel.impactLevel.impactLevel) {
+        //         case dow.ImpactLevel.IL2:
+        //           impactLevel2.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL4:
+        //           impactLevel4.push(item);
+        //           break;
+        //         case dow.ImpactLevel.IL5:
+        //           impactLevel5.push(item);
+        //           break;
+        //         default:
+        //           impactLevel6.push(item);
+        //       }
+        //     });
+        //   });
+        //   const data = {
+        //     il2: impactLevel2,
+        //     il4: impactLevel4,
+        //     il5: impactLevel5,
+        //     il6: impactLevel6,
+        //   };
+        //   console.log(data);
+        //   return data;
+        // },
+        // // fully implemented
+        // checkForCloudSupport: () => {
+        //   if (payload.cloudSupportPackage.length > 0) {
+        //     return "Yes";
+        //   } else {
+        //     return "No";
+        //   }
+        // },
+        // getComputeInstances: (impactLevel: any) => {
+        //   const numInstances = {
+        //     dev: 0,
+        //     preProd: 0,
+        //     prod: 0,
+        //     coop: 0,
+        //   };
+        //   // need to filter on IL level...
+        //   payload.xaasOfferings.instanceConfigurations.computeInstances.forEach((instance: any) => {
+        //     if (instance.classificationLevel.impactLevel === impactLevel) {
+        //       switch (instance.environmentType) {
+        //         case dow.EnvironmentType.DEV_TEST:
+        //           numInstances.dev += instance.numberOfInstances;
+        //           break;
+        //         case dow.EnvironmentType.PRE_PROD:
+        //           numInstances.preProd += instance.numberOfInstances;
+        //           break;
+        //         case dow.EnvironmentType.COOP_DIASTER_RECOVERY:
+        //           numInstances.coop += instance.numberOfInstances;
+        //           break;
+        //         default:
+        //           numInstances.prod += instance.numberOfInstances;
+        //       }
+        //     }
+        //   });
+        //   return numInstances;
+        // },
+        // getDatabaseInstances: () => {
+        //   const numInstances = {
+        //     IL2: 0,
+        //     IL4: 0,
+        //     IL5: 0,
+        //     IL6: 0,
+        //   };
+        //   // let base = "instance.classificationLevel." + impactLevel
+        //   payload.xaasOfferings.instanceConfigurations.databaseInstances.forEach((instance: any) => {
+        //     switch (instance.classificationLevel.impactLevel) {
+        //       case dow.ImpactLevel.IL2:
+        //         numInstances.IL2 += instance.numberOfInstances;
+        //         break;
+        //       case dow.ImpactLevel.IL4:
+        //         numInstances.IL4 += instance.numberOfInstances;
+        //         break;
+        //       case dow.ImpactLevel.IL5:
+        //         numInstances.IL5 += instance.numberOfInstances;
+        //         break;
+        //       default:
+        //         numInstances.IL6 += instance.numberOfInstances;
+        //     }
+        //   });
+        //   return numInstances;
+        // },
+        // calcAvgDataEgress: () => {
+        //   let gbEgress = 0;
+        //   let pbEgress = 0;
+        //   let tbEgress = 0;
+        //   const response = {
+        //     dataEgressAverage: 0,
+        //     dataEgressMonthlyUnit: dow.StorageUnit.TB,
+        //   };
+        //   payload.xaasOfferings.instanceConfigurations.computeInstances.forEach((instance: any) => {
+        //     switch (instance.dataEgressMonthlyUnit) {
+        //       case dow.StorageUnit.GB:
+        //         gbEgress += instance.dataEgressMonthlyAmount * instance.numberOfInstances;
+        //         break;
+        //       case dow.StorageUnit.TB:
+        //         tbEgress += instance.dataEgressMonthlyAmount * instance.numberOfInstances;
+        //         break;
+        //       case dow.StorageUnit.PB:
+        //         pbEgress += instance.dataEgressMonthlyAmount * instance.numberOfInstances;
+        //         break;
+        //       default:
+        //         console.error();
+        //     }
+        //   });
+        //   let pbToGB;
+        //   let tbToGB;
+        //   let gbDataEgress;
+        //   if (isNaN(gbEgress)) {
+        //     gbEgress = 0;
+        //   }
+        //   if (isNaN(pbEgress)) {
+        //     pbEgress = 0;
+        //   }
+        //   if (isNaN(tbEgress)) {
+        //     tbEgress = 0;
+        //   }
+        //   pbToGB = pbEgress * 1000000;
+        //   tbToGB = tbEgress * 1000;
+        //   gbDataEgress = gbEgress + pbToGB + tbToGB;
+        //   if (gbDataEgress / 1000000 > 1) {
+        //     response.dataEgressAverage = gbDataEgress / 1000000;
+        //     response.dataEgressMonthlyUnit = dow.StorageUnit.PB;
+        //     return response;
+        //   } else if (gbDataEgress / 1000 > 1) {
+        //     response.dataEgressAverage = gbDataEgress / 1000;
+        //     response.dataEgressMonthlyUnit = dow.StorageUnit.TB;
+        //     return response;
+        //   } else {
+        //     response.dataEgressAverage = gbDataEgress;
+        //     response.dataEgressMonthlyUnit = dow.StorageUnit.GB;
+        //     return response;
+        //   }
+        // },
+        // calcDevTestInstances: () => {
+        //   /* for(let i = 0; i < dow.XaasServiceOfferingGroup.length; i ++){
+        //   } */
+        // },
       },
       cmdDelimiter: ["{", "}"],
     })
--- document-generation/utils/utils.ts
+++ document-generation/utils/utils.ts
@@ -5,8 +5,15 @@ import {
   TemplatePaths,
   IFundingDocument,
   FundingType,
+  Classification,
+  PeriodType,
 } from "../../models/document-generation";
 import * as fs from "fs";
+import {
+  IXaaSOfferingGroup,
+  IXaaSOfferings,
+  XaasServiceOfferingGroup,
+} from "../../models/document-generation/description-of-work";
 
 export const capitalize = (string: string) => {
   if (typeof string !== "string") {
@@ -69,6 +76,362 @@ export const getFundingDocInfo = (fundingDoc: IFundingDocument): string => {
 interface PDFTemplateFiles {
   html: string;
   css: string;
+}
+
+const xaasServiceGroupsOrder = [
+  "COMPUTE",
+  "DEVELOPER_TOOLS",
+  "APPLICATIONS",
+  "MACHINE_LEARNING",
+  "NETWORKING",
+  "SECURITY",
+  "DATABASE",
+  "STORAGE",
+  "EDGE_COMPUTING",
+  "IOT",
+  "GENERAL_XAAS",
+];
+
+const cloudSupportGroupsOrder = [
+  "ADVISORY_ASSISTANCE",
+  "HELP_DESK_SERVICES",
+  "TRAINING",
+  "DOCUMENTATION_SUPPORT",
+  "GENERAL_CLOUD_SUPPORT",
+  "PORTABILITY_PLAN",
+];
+const classificationLevelsOrder = ["IL2", "IL4", "IL5", "IL6", "TS"];
+
+export interface ITaskGrouping {
+  dowTaskNumbers: string[];
+  taskPeriods: string[];
+}
+
+const hasSameSelectedPeriods = (unique: ITaskGrouping, check: string[]) => {
+  if (unique.taskPeriods.length !== check.length) {
+    return false;
+  }
+  const hasAllPeriods = check.every((period: any) => unique.taskPeriods.includes(period));
+  return hasAllPeriods;
+}
+
+export const getTaskPeriods = (payload: any) => {
+  const { xaasOfferings, crossDomainSolutions, cloudSupportPackages, periodOfPerformance } = payload;
+  const { basePeriod, optionPeriods } = periodOfPerformance;
+  const uniqueSelectedPeriodPairs: ITaskGrouping[] = [];
+  const entireDurationTasks: string[] = [];
+  
+  // group the periods based on array like ["B", "OP1", "OP2"]
+  const popPeriods: string[] = [];
+  if (basePeriod) {
+    popPeriods.push("B")
+  }
+  if (optionPeriods) {
+    optionPeriods
+      .sort((a: IPeriod, b: IPeriod) => a.optionOrder - b.optionOrder)
+      .forEach((period: IPeriod) => popPeriods.push(`OP${period.optionOrder}`))
+  }
+
+  // 4.2.x.x
+  xaasOfferings.forEach((offering: any) => {
+    const groupName: any = offering.serviceOffering.serviceOffering.serviceOfferingGroup;
+    const instances = offering.serviceOffering.classificationInstances;
+
+    instances.forEach((classInst: any) => {
+      const { classificationLevel, needForEntireTaskOrderDuration, selectedPeriods } = classInst;
+      let dowTaskNumber: string;
+      let levelNumber: number;
+      let groupNumber: number;
+      let taskPeriods: string[] = [];
+      let uniqueGroupingIndex = -1;
+
+      // NOTE: TS level does not have an impact level to use
+      if (classificationLevel.classification === Classification.TS) {
+        // account for zero index
+        levelNumber = classificationLevelsOrder.indexOf(classificationLevel.classification) + 1;
+        groupNumber = xaasServiceGroupsOrder.indexOf(groupName) + 1; 
+        dowTaskNumber = `4.2.${levelNumber}.${groupNumber}`;
+      } else {
+        levelNumber = classificationLevelsOrder.indexOf(classificationLevel.impactLevel) + 1; 
+        groupNumber = xaasServiceGroupsOrder.indexOf(groupName) + 1; 
+        dowTaskNumber = `4.2.${levelNumber}.${groupNumber}`;
+      }
+
+      if (needForEntireTaskOrderDuration) {
+        entireDurationTasks.push(dowTaskNumber);
+      }
+
+      // for all selected Pop periods (not entire pop duration)
+      if (selectedPeriods && selectedPeriods.length >= 1) {
+        taskPeriods = selectedPeriods
+          .sort((a: IPeriod, b: IPeriod) => a.optionOrder - b.optionOrder)
+          .map((period: IPeriod) => period.periodType === PeriodType.BASE ? "B" : `OP${period.optionOrder}`)
+
+        // see if any previous period pairings that the task number can be added to
+        const matchingPeriodPair = uniqueSelectedPeriodPairs.filter((pair, index) =>  {
+          const hasAllPeriods = hasSameSelectedPeriods(pair, taskPeriods)
+          uniqueGroupingIndex = hasAllPeriods ? index : -1;
+          return hasAllPeriods
+        });
+
+        // add to a period paring if one found
+        if (matchingPeriodPair.length >= 1 && uniqueGroupingIndex > -1) {
+          uniqueSelectedPeriodPairs[uniqueGroupingIndex].dowTaskNumbers.push(dowTaskNumber)
+        }
+
+        // create a new period pairing for the task number (and future numbers)
+        if (matchingPeriodPair.length < 1) {
+          uniqueSelectedPeriodPairs.push({
+            dowTaskNumbers: [dowTaskNumber],
+            taskPeriods: taskPeriods
+          })
+        }
+      }
+    });
+
+
+  });
+
+  // 4.2.6
+  if (crossDomainSolutions.crossDomainSolutionRequired) {
+    if (crossDomainSolutions.needForEntireTaskOrderDuration) {
+      entireDurationTasks.push(`4.2.6`);
+    }
+    // which periods is it needed for
+    if (crossDomainSolutions.selectedPeriods) {
+      // TODO: populate selected periods
+
+    }
+  }
+
+  // cloud support 4.3.x.x
+  cloudSupportPackages.forEach((offering: any) => {
+    const groupName: any = offering.serviceOffering.serviceOffering.serviceOfferingGroup;
+    const instances = offering.serviceOffering.classificationInstances;
+
+    // each instances will be a different subtask 4.3.x.1
+    // x is the classification level
+    // groups is the last number
+    instances.forEach((classInst: any) => {
+      const { classificationLevel, needForEntireTaskOrderDuration, selectedPeriods } = classInst;
+      let dowTaskNumber: string;
+      let portabilityTaskNumber: string;
+      let levelNumber: number;
+      let groupNumber: number;
+      let taskPeriods: string[] = [];
+      let uniqueGroupingIndex = -1;
+
+      if (classificationLevel.classification === Classification.TS) {
+        levelNumber = classificationLevelsOrder.indexOf(classificationLevel.classification) + 1;
+        groupNumber = cloudSupportGroupsOrder.indexOf(groupName) + 1;
+        dowTaskNumber = groupName === cloudSupportGroupsOrder[5]
+          ? `4.3.${levelNumber}`
+          : `4.3.${levelNumber}.${groupNumber}`
+      } else {
+        levelNumber = classificationLevelsOrder.indexOf(classificationLevel.impactLevel) + 1;
+        groupNumber = cloudSupportGroupsOrder.indexOf(groupName) + 1;
+        dowTaskNumber = groupName === cloudSupportGroupsOrder[5]
+          ? `4.3.${levelNumber}`
+          : `4.3.${levelNumber}.${groupNumber}`
+      }
+
+      if (needForEntireTaskOrderDuration) {
+        entireDurationTasks.push(dowTaskNumber)
+      }
+
+      if (selectedPeriods && selectedPeriods.length >= 1) {
+        taskPeriods = selectedPeriods
+          .sort((a: IPeriod, b: IPeriod) => a.optionOrder - b.optionOrder)
+          .map((period: IPeriod) => period.periodType === PeriodType.BASE ? "B" : `OP${period.optionOrder}`)
+
+        // see if there any previous period pairings that the task number can be added to
+        const matchingPeriodPair = uniqueSelectedPeriodPairs.filter((pair, index) =>  {
+          const hasAllPeriods = hasSameSelectedPeriods(pair, taskPeriods)
+          uniqueGroupingIndex = hasAllPeriods ? index : -1;
+          return hasAllPeriods
+        });
+
+        if (matchingPeriodPair.length >= 1 && uniqueGroupingIndex > -1) {
+          uniqueSelectedPeriodPairs[uniqueGroupingIndex].dowTaskNumbers.push(dowTaskNumber)
+        }
+
+        // create a new period pairing for the task number
+        if (matchingPeriodPair.length < 1) {
+          uniqueSelectedPeriodPairs.push({
+            dowTaskNumbers: [dowTaskNumber],
+            taskPeriods: taskPeriods
+          })
+        }
+      }
+
+    })
+  });
+
+  // sort so the pairts with the most periods are first
+  const sortedUniuqueSelectedPeriodPairs = uniqueSelectedPeriodPairs
+    .sort((a: ITaskGrouping, b: ITaskGrouping) => a.taskPeriods.length - b.taskPeriods.length)
+  
+  // template helping object to create table
+  const mappedTaskNumbers: any = {
+    popPeriods,
+    entireDurationTasks,
+    taskNumberGroups: sortedUniuqueSelectedPeriodPairs,
+  };
+
+  console.log(JSON.stringify(mappedTaskNumbers))
+  return mappedTaskNumbers;
+};
+
+export const getCDRLs = (popTasks: any, contractType: any) => {
+  const { firmFixedPrice, timeAndMaterials} = contractType;
+  const cdrl = []
+  const allTasks = popTasks; 
+  console.log("ALL TaSK: ", allTasks)
+
+  const progressReport = {
+    taskNumbers: ["ANY"],
+    ffp: firmFixedPrice ? "x001, x003, x005" : "",
+    tm: timeAndMaterials ? "x017, x019, x021" : "",
+    cdrl: "A012",
+    name: "TO Monthly Progress Report",
+  };
+
+  cdrl.push(progressReport)
+
+
+  const trainingClins: string[] = [];
+  const taskNumbers: string[] = []
+  const portabilityPlanClins: string[] = [];
+  let portabilityTaskNumbers: string[] = [];
+  const edgeClins: string[] = [];
+  // const edgeTaskNumbers: string[] = [];
+  const subtaskNumbers = ["1", "2", "3", "4", "5"];
+  subtaskNumbers.forEach((number: any) =>  {
+    // tasks 4.3.x (portability plan)
+    const taskNumber = `4.3.${number}`;
+    const containsPortabilityTasks = allTasks.includes(taskNumber);
+
+    if (containsPortabilityTasks) {
+      portabilityTaskNumbers.push(taskNumber);
+      switch(number) {
+        case "1":
+        case "2":
+        case "3":
+          firmFixedPrice ? portabilityPlanClins.push("x001") : null;
+          timeAndMaterials ? portabilityPlanClins.push("x017") : null;
+          break;
+        case "4":
+          firmFixedPrice ? portabilityPlanClins.push("x003") : null;
+          timeAndMaterials ? portabilityPlanClins.push("x019") : null;
+          break;
+        case "5":
+          firmFixedPrice ? portabilityPlanClins.push("x005") : null;
+          timeAndMaterials ? portabilityPlanClins.push("x021") : null;
+          break;
+        default:
+          break;
+      }
+    }
+
+    // subtasks 4.3.x.1
+    // const subtaskNumber = `4.3.${number}.3`;
+    const trainingSubtask = `4.3.${number}.3`;
+    const containsTrainingSubtask = allTasks.includes(trainingSubtask);
+
+    if (containsTrainingSubtask) {
+      taskNumbers.push(trainingSubtask);
+      switch(number) {
+        case "1":
+        case "2":
+        case "3":
+          firmFixedPrice ? trainingClins.push("x002") : null;
+          timeAndMaterials ? trainingClins.push("x018") : null;
+          break;
+        case "4":
+          firmFixedPrice ? trainingClins.push("x004") : null;
+          timeAndMaterials ? trainingClins.push("x020") : null;
+          break;
+        case "5":
+          firmFixedPrice ? trainingClins.push("x006") : null;
+          timeAndMaterials ? trainingClins.push("x022") : null;
+          break;
+        default:
+          break;
+      }
+    }
+    // subtasks 4.2.x.9
+    const edgeSubtask = `4.2.${number}.9`;
+    const containsEdgeSubtask = allTasks.includes(edgeSubtask);
+
+    if (containsEdgeSubtask) {
+      taskNumbers.push(edgeSubtask);
+      switch(number) {
+        case "1":
+        case "2":
+        case "3":
+          firmFixedPrice ? edgeClins.push("x001") : null;
+          timeAndMaterials ? edgeClins.push("x017") : null;
+          break;
+        case "4":
+          firmFixedPrice ? edgeClins.push("x003") : null;
+          timeAndMaterials ? edgeClins.push("x019") : null;
+          break;
+        case "5":
+          firmFixedPrice ? edgeClins.push("x005") : null;
+          timeAndMaterials ? edgeClins.push("x021") : null;
+          break;
+        default:
+          break;
+      }
+    }
+
+  })
+
+
+  const training = {
+    // TODO: filter to get only subtask numbers
+    taskNumbers,
+    clins: trainingClins,
+  }
+  console.log("TRAINING Tasks: ", training)
+ if (training.taskNumbers.length >= 1) {
+    cdrl.push({ ...training, cdrl: "*A004", name: "System Administrator Training Materials" })
+    cdrl.push({ ...training, cdrl: "*A005", name: "Role-Based User Training Material" })
+ }
+  portabilityTaskNumbers = subtaskNumbers
+    .map((number: string) => `4.3.${number}.9`)
+    .filter((taskNumber) => taskNumbers.includes(taskNumber))
+  const portabilityPlan = {
+    taskNumbers: portabilityTaskNumbers,
+    clins: portabilityPlanClins,
+    cdrl: `**A006`,
+    name: "Portability Plan"
+  }
+  if (portabilityPlan.taskNumbers.length >= 1) {
+    cdrl.push({ ...portabilityPlan })
+  }
+ 
+  
+
+  // subtaskNumbers.map()
+  const edgeTaskNumbers = subtaskNumbers
+    .map((number: string) => `4.2.${number}.9`)
+    .filter((taskNumber) => taskNumbers.includes(taskNumber))
+  console.log("IOT #: ", edgeTaskNumbers)
+  console.log("all task #: ", taskNumbers)
+  const edge = {
+    taskNumbers: edgeTaskNumbers,
+    clins: edgeClins,
+    cdrl: `**A006`,
+    name: "Portability Plan"
+  }
+  if (edge.taskNumbers.length >= 1) {
+    cdrl.push({ ...edge})
+  }
+
+  // TE
+
+  return cdrl;
 }
 
 // documents and the related templates
