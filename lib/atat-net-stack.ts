c656180c7b23ab5335b5e7b1af2ab8aff884283e
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-01-05 13:41:04 -0500

    fixed the vpc flow logs format/fixed test

--- lib/atat-net-stack.ts
+++ lib/atat-net-stack.ts
@@ -56,7 +56,8 @@ export class AtatNetStack extends cdk.Stack {
     );
 
     // Capture all VPC flow logs and send to CloudWatch Logs with indefinite retention.
-    // Flow log format made to meet C5ISR log format requirement 
+    // Flow log format made to meet C5ISR log format requirement
+    /* eslint-disable no-template-curly-in-string */
     vpc.addFlowLog("AllFlowLogs", {
       logFormat: [
         ec2.LogFormat.VERSION,
@@ -79,6 +80,7 @@ export class AtatNetStack extends cdk.Stack {
         ec2.LogFormat.custom("${action}"),
         ec2.LogFormat.custom("${tcp-flags}"),
         ec2.LogFormat.custom("${log-status}"),
+        /* eslint-enable no-template-curly-in-string */
       ],
       destination: ec2.FlowLogDestination.toCloudWatchLogs(
         new logs.LogGroup(this, "ALLFlowLogs", {
