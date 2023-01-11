c2911a9dd33aad279860a2339244e7340fb064cb
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-01-10 21:14:20 -0500

    added bucket location for vpc logs

--- lib/atat-net-stack.ts
+++ lib/atat-net-stack.ts
@@ -58,7 +58,7 @@ export class AtatNetStack extends cdk.Stack {
       })
     );
 
-    const vflbucket = s3.Bucket.fromBucketAttributes(this, "ImportedBucket", {
+    const vflbucket = s3.Bucket.fromBucketAttributes(this, "flow-logs-301912046736-us-gov-west-1", {
       bucketArn: "arn:aws-us-gov:s3:::flow-logs-301912046736-us-gov-west-1",
     });
 
