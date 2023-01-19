b386a3e7354897d1a0740d19459f58471277f1ab
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-01-19 12:04:30 -0500

    creates log bucket for cissp updated retention to infinte

--- lib/atat-web-api-stack.ts
+++ lib/atat-web-api-stack.ts
@@ -201,6 +201,12 @@ export class AtatWebApiStack extends cdk.Stack {
       logGroupName: "hacc-cssp-cwl-logs",
       retention: RetentionDays.INFINITE,
     });
+    NagSuppressions.addResourceSuppressions(logGroup, [
+      {
+        id: "NIST.800.53.R4-CloudWatchLogGroupRetentionPeriod",
+        reason: "Setting retention to infinte thus no minimum retention is needed. ",
+      },
+    ]);
 
     // Ensure that no IAM users in this Stack can ever do anything
     // except for invoke the created API Gateway.
