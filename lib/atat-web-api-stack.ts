eb3152b9e871f60cc937a7e16869afb1708df1b6
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-01-23 12:32:58 -0500

    commented out the log group for testing | Added the New Resource Log bucket

--- lib/atat-web-api-stack.ts
+++ lib/atat-web-api-stack.ts
@@ -112,6 +112,32 @@ export class AtatWebApiStack extends cdk.Stack {
         reason: "Server Access Logs cannot be delivered to a bucket with Object Lock enabled",
       },
     ]);
+
+    //  Bucket ofr Object Lock Testing
+    const NewaccessLogsBucket = new s3.Bucket(this, "NewResourceAccessLogs", {
+      // Elastic Load Balancing Log Delivery requires SSE-S3 and _does not_ support
+      // SSE-KMS. This still ensures that log data is encrypted at rest.
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: true,
+    });
+    const newBucket = NewaccessLogsBucket.node.defaultChild as s3.CfnBucket;
+    newBucket.addPropertyOverride("ObjectLockEnabled", true);
+    NagSuppressions.addResourceSuppressions(NewaccessLogsBucket, [
+      {
+        id: "NIST.800.53.R4-S3BucketLoggingEnabled",
+        reason: "The ideal bucket for this to log to is itself. That creates complexity with receiving other logs",
+      },
+      {
+        id: "NIST.800.53.R4-S3BucketReplicationEnabled",
+        reason: "Cross region replication is not required for this use case",
+      },
+      {
+        id: "NIST.800.53.R4-S3BucketDefaultLockEnabled",
+        reason: "Server Access Logs cannot be delivered to a bucket with Object Lock enabled",
+  },
+    ]);
     const api = new AtatRestApi(this, "HothApi", apiProps);
     if (props.apiDomain && network) {
       const certificate = acm.Certificate.fromCertificateArn(this, "ApiCertificate", props.apiDomain.acmCertificateArn);
