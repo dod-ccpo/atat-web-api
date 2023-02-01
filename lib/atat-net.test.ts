995fdfcd2cfefe40c5824be928529cf3d3998f0c
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-02-01 10:19:16 -0500

    updated the test file for the net stack vpc flow logs

--- lib/atat-net.test.ts
+++ lib/atat-net.test.ts
@@ -13,7 +13,7 @@ describe("ATAT network creation", () => {
     const template = Template.fromStack(stack);
     // THEN
     template.hasResourceProperties("AWS::EC2::FlowLog", {
-      LogDestinationType: "s3",
+      LogDestinationType: "logs",
       ResourceType: "VPC",
       TrafficType: "ALL",
       ResourceId: stack.resolve(stack.vpc.vpcId),
