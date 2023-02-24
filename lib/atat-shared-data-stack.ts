1c39e5ad604efeaf2635dd5cc43c3f76d9511299
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-02-24 14:05:13 -0500

    grants the cloudwatch event access to the kms key encrypting the sns topic

--- lib/atat-shared-data-stack.ts
+++ lib/atat-shared-data-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as kms from "aws-cdk-lib/aws-kms";
 import * as logs from "aws-cdk-lib/aws-logs";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
 import { NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
