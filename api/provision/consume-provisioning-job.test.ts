b22e1775c25a651a546d51e02655f5e0b988ac8c
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2023-03-10 01:49:50 -0500

    Improve test descriptions for clarity of intent. Explicitly state expectations for mock responses. Simplify sample message data and remove expectation of a specific number of messages or message structure. Simplify message content check.

--- api/provision/consume-provisioning-job.test.ts
+++ api/provision/consume-provisioning-job.test.ts
@@ -6,66 +6,73 @@ import { ApiSuccessResponse, ErrorStatusCode, OtherErrorResponse, SuccessStatusC
 import { generateMockMessageResponses, requestContext } from "../util/common-test-fixtures";
 import { handler } from "./consume-provisioning-job";
 
+// This is standing in for the SQS queue content.
+// The number of messages and message content are irrelevant
+// excepting that it cannot be empty.
 const messageBodies = [
   {
-    code: 200,
-    content: {
-      some: "good content",
-    },
+    foo: "foo",
   },
   {
-    code: 400,
-    content: {
-      some: "bad content",
-    },
+    bar: "bar",
   },
 ];
 const mockReceiveMessageResponse = generateMockMessageResponses(messageBodies);
+// Each entry of messageBodies is expanded to a message element
+// under the object key 'Messages' similar to the following:
+// {
+//   MessageId: 'b5353c',
+//   ReceiptHandle: 'AQC0e6b=',
+//   MD5OfBody: '53c0a94739bbb505b458e55096ea85bf',
+//   Body: '{"foo":"foo"}',
+//   Attributes: undefined,
+//   MD5OfMessageAttributes: undefined,
+//   MessageAttributes: undefined
+// },
 
 const sqsMock = mockClient(sqsClient);
 beforeEach(() => {
   sqsMock.reset();
 });
 
-describe("Consumer Provisioning Job handler valid behavior", () => {
-  it("should poll 2 messages from SQS provisioning queue with 200", async () => {
-    process.env.PROVISIONING_QUEUE_URL = "my url";
-    sqsMock.on(ReceiveMessageCommand).resolves(mockReceiveMessageResponse);
+describe("Consume Provisioning Job handler - normal SQS behavior", () => {
+  it("should poll non-empty queue and return 200 status and expected number of messages", async () => {
+    // object key Messages is based on messageBodies to simulate a non-empty queue
+    const sqsMockResponse = mockReceiveMessageResponse;
+    expect(sqsMockResponse.$metadata).toBeDefined();
+    expect(sqsMockResponse.Messages).toBeDefined();
+    expect(sqsMockResponse.Messages).toHaveLength(messageBodies.length);
+    sqsMock.on(ReceiveMessageCommand).resolves(sqsMockResponse);
 
     const response = await handler({ body: null, requestContext } as APIGatewayProxyEvent, {} as Context);
-    const responseBody = JSON.parse(response.body);
-
     expect(sqsMock.commandCalls(ReceiveMessageCommand)).toHaveLength(1);
-    expect(sqsMock.commandCalls(DeleteMessageCommand)).toHaveLength(2);
+    expect(sqsMock.commandCalls(DeleteMessageCommand)).toHaveLength(sqsMockResponse.Messages.length);
     expect(response).toBeInstanceOf(ApiSuccessResponse);
     expect(response.statusCode).toBe(SuccessStatusCode.OK);
-    expect(responseBody).toHaveLength(2);
-    expect(responseBody).toEqual(
-      mockReceiveMessageResponse.Messages.map((message) => {
-        const body = JSON.parse(message.Body);
-        return { code: body.code, content: body.content };
-      })
-    );
+    expect(JSON.parse(response.body)).toEqual(messageBodies);
   });
-  it("should poll 0 messages and return empty array with 200", async () => {
-    process.env.PROVISIONING_QUEUE_URL = "my url";
-    sqsMock.on(ReceiveMessageCommand).resolves({ ...mockReceiveMessageResponse, Messages: undefined });
+  it("should poll empty queue and return 200 status and no messages", async () => {
+    // object key Messages is overridden with undefined to simulate an empty queue
+    const sqsMockResponse = { ...mockReceiveMessageResponse, Messages: undefined };
+    expect(sqsMockResponse.$metadata).toBeDefined();
+    expect(sqsMockResponse.Messages).toBeUndefined();
+    sqsMock.on(ReceiveMessageCommand).resolves(sqsMockResponse);
 
     const response = await handler({ body: null, requestContext } as APIGatewayProxyEvent, {} as Context);
-    const responseBody = JSON.parse(response.body);
-
     expect(sqsMock.commandCalls(ReceiveMessageCommand)).toHaveLength(1);
     expect(sqsMock.commandCalls(DeleteMessageCommand)).toHaveLength(0);
     expect(response).toBeInstanceOf(ApiSuccessResponse);
     expect(response.statusCode).toBe(SuccessStatusCode.OK);
+
+    const responseBody = JSON.parse(response.body);
     expect(responseBody).toHaveLength(0);
     expect(responseBody).toEqual([]);
   });
 });
 
-describe("Consumer Provisioning Job invalid behavior", () => {
-  it("should throw an internal error if invalid response from queue", async () => {
-    process.env.PROVISIONING_QUEUE_URL = "my url";
+describe("Consume Provisioning Job handler - abnormal SQS behavior", () => {
+  it("should poll an abnormally behaving queue and return 500 status", async () => {
+    // simulate abnormal queue behavior; no object in the response, no $metadata nor Messages keys
     sqsMock.on(ReceiveMessageCommand).resolves(undefined!);
 
     const response = await handler({ body: null, requestContext } as APIGatewayProxyEvent, {} as Context);
