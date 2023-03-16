7ffdddc8efff95ae35df155e08bea4d22ed3cf7a
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-03-16 17:34:22 -0400

    testing deployment

--- lib/atat-pipeline-stack.ts
+++ lib/atat-pipeline-stack.ts
@@ -56,6 +56,7 @@ class AtatApplication extends cdk.Stage {
   }
 }
 
+// AtatPipelineStack Stack
 export class AtatPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AtatPipelineStackProps) {
     super(scope, id, props);
