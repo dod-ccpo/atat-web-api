c7f703de9abc4868638f73506d0027def6d1baca
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2023-08-10 17:15:28 -0400

    removed comment

--- lib/atat-notification-stack.ts
+++ lib/atat-notification-stack.ts
@@ -15,7 +15,6 @@ export interface AtatNotificationStackProps {
   topicEncryptionKey: kms.IKey;
 }
 
-// Event to trigger SNS topic
 export class IamChangeRule extends events.Rule {
   constructor(scope: Construct, id: string, props?: events.RuleProps) {
     super(scope, id, {
