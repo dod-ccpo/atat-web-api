43d995b09e800db50ae33e3bafef5f4af9919c42
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-08-18 12:15:48 -0400

    added sns notification for new guard duty findings

--- lib/atat-notification-stack.ts
+++ lib/atat-notification-stack.ts
@@ -62,9 +62,26 @@ export class PipelineStatus extends events.Rule {
   }
 }
 
+// Event to trigger SNS topic for GuardDuty findings
+export class GuardDutyFindingRule extends events.Rule {
+  constructor(scope: Construct, id: string, props?: events.RuleProps) {
+    super(scope, id, {
+      ...props,
+      eventPattern: {
+        source: ["aws.guardduty"],
+        detailType: ["AWS GuardDuty Findings"],
+        detail: {
+          state: ["GuardDuty Finding"],
+        },
+      },
+    });
+  }
+}
+
 export class AtatNotificationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AtatNotificationStackProps) {
     super(scope, id);
+    // SNS Topic and subscription for IAM user activity
     const topic = new sns.Topic(this, "AtatNotifications", { masterKey: props.topicEncryptionKey });
     topic.addSubscription(new subscriptions.EmailSubscription(props.notificationEmail));
     const topicTarget = new eventTargets.SnsTopic(topic);
@@ -72,11 +89,20 @@ export class AtatNotificationStack extends cdk.Stack {
     const iamChanges = new IamChangeRule(this, "IamChanges");
     iamChanges.addTarget(topicTarget);
 
+    // SNS Topic and subscription for Pipeline Status
     const topicpipeline = new sns.Topic(this, "AtatPipelineNotifications", { masterKey: props.topicEncryptionKey });
     topicpipeline.addSubscription(new subscriptions.EmailSubscription(props.notificationEmail));
     const topicpipelineTarget = new eventTargets.SnsTopic(topicpipeline);
 
     const pipelineChanges = new PipelineStatus(this, "PipelineStatus");
     pipelineChanges.addTarget(topicpipelineTarget);
+
+    // SNS Topic and subscription for GuardDuty findings
+    const guardDutyTopic = new sns.Topic(this, "AtatGuardDutyNotifications", { masterKey: props.topicEncryptionKey });
+    guardDutyTopic.addSubscription(new subscriptions.EmailSubscription(props.notificationEmail));
+    const guardDutyFindingTarget = new eventTargets.SnsTopic(guardDutyTopic);
+
+    const guardDutyFinding = new GuardDutyFindingRule(this, "GuardDutyFindings");
+    guardDutyFinding.addTarget(guardDutyFindingTarget);
   }
 }
