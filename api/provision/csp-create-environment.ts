48cb9c2cd9c4444f44887a918d0c58d8268cb0ac
Stephen Hayes <stephen.hayes@hunterstrategy.net>
2023-10-27 07:07:14 -0500

    AT-9971 types/lambda updated with three new optional fields

--- api/provision/csp-create-environment.ts
+++ api/provision/csp-create-environment.ts
@@ -38,16 +38,30 @@ async function makeRequest(client: IAtatClient, request: HothProvisionRequest): 
     deadline.getHours() + (atatApiTypes.ClassificationLevel.UNCLASSIFIED === payload.classificationLevel ? 2 : 72)
   );
 
+  const {
+    name,
+    administrators,
+    classificationLevel,
+    cloudDistinguisher,
+    accountName,
+    emailDistributionList,
+    isMigration
+  } = payload;
+
   const addEnvironmentRequest: atatApiTypes.AddEnvironmentRequest = {
     portfolioId: request.portfolioId,
     environment: {
-      name: payload.name,
-      administrators: payload.administrators,
-      classificationLevel: payload.classificationLevel,
-      cloudDistinguisher: payload.cloudDistinguisher,
+      name,
+      administrators,
+      classificationLevel,
+      cloudDistinguisher,
+      ...(accountName && { accountName }),
+      ...(emailDistributionList && { emailDistributionList }),
+      ...(isMigration !== undefined && { isMigration }),
     },
-    provisionDeadline: deadline.toISOString(),
+    ...(!isMigration && { provisionDeadline: deadline.toISOString()})
   };
+
   try {
     logger.info(`Invoking addEnvironment against CSP ${request.targetCspName}`);
     const cspResponse = await client.addEnvironment(addEnvironmentRequest);
