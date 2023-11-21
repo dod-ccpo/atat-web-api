30613312fd9958fc7aa1a8af790e380608f4f710
Jesse Peirce (Hunter Strategy) <137221161+jessepeirce-ccpo@users.noreply.github.com>
2023-11-21 09:29:35 -0500

    fixing logic on when to create custom resource for finding api gw IP addresses

--- lib/atat-web-api.test.ts
+++ lib/atat-web-api.test.ts
@@ -19,7 +19,7 @@ test("The stack creates successfully with a VPC defined", () => {
   // WHEN
   const network = new AtatNetStack(app, "TestNetStack", {
     vpcFlowLogBucket: "arn:aws:us-east-1:s3::123456789012:flow-logs-123456789012-us-east-1",
-    eventbus: "arn:aws:us-east-1:event:12345678910:test",
+    tgwEventBus: "arn:aws:us-east-1:event:12345678910:test",
   });
   const stack = new AtatWebApi.AtatWebApiStack(app, "TestStack", { environmentName: "At0000", network, albeventbusARN: "arn:test" });
   // THEN
